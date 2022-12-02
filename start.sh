4949e6b65ef8e5148f697f8eeab5c86a46f337ac
Jaroslaw Michalski <jaroslaw.m.michalski@phe.gov.uk>
2022-12-02 15:28:04 +0000

    don't migrate anything for now

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "Starting server..."
 
-aerich init -t settings.TORTOISE_ORM
+# aerich init -t settings.TORTOISE_ORM
 
-echo "AERICH initialization finished, runnig aerich upgrade now"
-aerich upgrade
+# echo "AERICH initialization finished, runnig aerich upgrade now"
+# aerich upgrade
 
-echo "AERICH upgrade is done, starting the app..."
+# echo "AERICH upgrade is done, starting the app..."
 uvicorn main:app --host 0.0.0.0 --port 80
