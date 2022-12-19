7888842827533c3e2200158d985d7281e950fe09
Ben Collier <bencollier@fastmail.com>
2022-12-19 21:33:43 +0000

    Add import route

--- wpapi/main.py
+++ wpapi/main.py
@@ -155,16 +155,13 @@ async def import_file():
 def upload(file: UploadFile = File(...)):
     try:
         contents = file.file.read()
-        with open(file.filename, 'wb') as f:
-            # f.write(contents)
-            pass
         
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+    return {"message": f"{contents}"}
 
 
 
