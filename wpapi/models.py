efa97140b3b8572bca38b1c53dae4a2e36d90a00
Ben Collier <bencollier@fastmail.com>
2022-12-20 00:28:11 +0000

    Adding file upload

--- wpapi/models.py
+++ wpapi/models.py
@@ -10,25 +10,25 @@ class MultiPathogen(Model):
     publish_date = fields.DateField()
     season = fields.CharField(max_length=9, null=True)
     influenza_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
-    rsv_pct = fields.DecimalField(max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
+    rsv_pct = fields.DecimalField(max_digits=4, decimal_places=1, null=True)
     rhinovirus_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
     parainfluenza_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
-    hmpv_pct = fields.DecimalField(max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
+    hmpv_pct = fields.DecimalField(max_digits=4, decimal_places=1, null=True)
     adenovirus_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
     sars_cov_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
     influenza_a_h3n2_n_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
     influenza_a_h1n1_pdm09_n_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
     influenza_a_not_subtyped_n_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
     influenza_b_n_pct = fields.DecimalField(
-        max_digits=3, decimal_places=1, null=True)
+        max_digits=4, decimal_places=1, null=True)
 
     # Defining ``__str__`` is also optional, but gives you pretty
     # represent of model in debugger and interpreter
