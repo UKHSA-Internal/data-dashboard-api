b25264ef03f9860b1b8a205f3019187b35f15de1
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-10 18:38:21 +0000

    Rename endpoint from `chart` -> `charts`

--- apiv3/urls.py
+++ apiv3/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     re_path(r'^upload/(?P<filename>[^/]+)$', FileUploadView.as_view()),
-    re_path(r'^chart/(?P<topic>[^/]+)$', ChartView.as_view()),
+    re_path(r'^charts/(?P<topic>[^/]+)$', ChartView.as_view()),
     path('admin/', admin.site.urls),
 ]
