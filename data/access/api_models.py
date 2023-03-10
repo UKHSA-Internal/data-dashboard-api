61e3e42ccbee9ef2f0464ebc0f8aae2f20db2275
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-10 16:31:24 +0000

    Implement custom `WeeklyTimeSeriesQuerySet` and `WeeklyTimeSeriesManager`
    The application layer should interact with the `WeeklyTimeSeriesManager` abstraction not the `WeeklyTimeSeriesQuerySet` directly.
    The manager should only be able to interact with the queryset directly.

--- data/access/api_models.py
+++ data/access/api_models.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 from django.db.models import Manager, QuerySet
 
@@ -9,19 +9,29 @@ DEFAULT_WEEKLY_TIME_SERIES_MANAGER = WeeklyTimeSeries.objects
 
 def get_weekly_disease_incidence(
     topic: str,
-    metric: str,
-    year: int = 2022,
     weekly_time_series_manager: Manager = DEFAULT_WEEKLY_TIME_SERIES_MANAGER,
-) -> List[int]:
+) -> List[Union[int, float]]:
+    """Queries the `WeeklyTimeSeries` (api model) for weekly incidences.
 
-    ordered_time_series_for_topic: QuerySet = weekly_time_series_manager.filter(
-        topic=topic,
-        metric=metric,
-        year=year,
-    ).order_by("start_date")
+    Note that this function casts the resultant queryset to a list.
+    Which will incur the penalty of a db call/ full query execution,
+    not just lazy evaluation.
 
-    flattened_metric_values: QuerySet = ordered_time_series_for_topic.values_list(
-        "metric_value", flat=True
+    Args:
+        topic: The name of the disease being queried.
+            E.g. `Influenza`
+        weekly_time_series_manager: The model manager used
+            for the `WeeklyTimeSeries`.
+            Defaults to `WeeklyTimeSeriesManager`
+
+    Returns:
+        List[Union[int, float]]: Flat list of metric values only
+
+    """
+    ordered_metric_values_for_topic: QuerySet = (
+        weekly_time_series_manager.get_metric_values_for_weekly_positivity_by_topic(
+            topic=topic
+        )
     )
 
-    return list(flattened_metric_values)
+    return list(ordered_metric_values_for_topic)
--- domain/charts/data_visualization.py
+++ domain/charts/data_visualization.py
@@ -1,34 +1,26 @@
+from typing import List, Union
+
 import plotly.graph_objects
 
 from data.access.api_models import get_weekly_disease_incidence
 from domain.charts.generation import generate_chart_figure
 
 
-def create_chart_figure_for_topic(
-    topic: str, metric: str, year: int = 2022
-) -> plotly.graph_objects.Figure:
-    weekly_disease_incidence = get_weekly_disease_incidence(
+def create_chart_figure_for_topic(topic: str) -> plotly.graph_objects.Figure:
+    weekly_disease_incidence: List[Union[int, float]] = get_weekly_disease_incidence(
         topic=topic,
-        metric=metric,
-        year=year,
     )
     return generate_chart_figure(data_points=weekly_disease_incidence)
 
 
 def write_chart_file_for_topic(
     topic: str,
-    metric: str = "weekly_positivity",
-    year: int = 2022,
-    file_format: str = "svg",
+    file_format: str = "png",
 ) -> str:
     """For a given `topic`, queries the db, creates a chart and writes the resulting file.
 
     Args:
         topic: The name of the topic. E.g. `Influenza`
-        metric: The metric to create the chart for.
-            Defaults to "weekly_positivity"
-        year: The year to generate the chart data for.
-            Defaults to 2022
         file_format: Can be any of the following:
             - 'png'
             - 'jpg' or 'jpeg'
@@ -42,7 +34,7 @@ def write_chart_file_for_topic(
 
     """
     figure: plotly.graph_objects.Figure = create_chart_figure_for_topic(
-        topic=topic, metric=metric, year=year
+        topic=topic
     )
     return write_figure(figure=figure, topic=topic, file_format=file_format)
 
