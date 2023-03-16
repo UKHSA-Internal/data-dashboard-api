296a0c2e25d137bd1bf4b0cb992d77984b15508b
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-16 12:04:05 +0000

    Push CMS codebase into `cms/` directory at root level of project

---
+++ cms/dashboard/wagtail_hooks.py
@@ -0,0 +1,10 @@
+from django.templatetags.static import static
+from django.utils.html import format_html
+from wagtail import hooks
+
+
+@hooks.register("insert_global_admin_css")
+def global_admin_css():
+    return format_html(
+        '<link rel="stylesheet" type="text/css" href="{}">', static("css/theme.css")
+    )
