6be97698e4296f8eb3a452a38f73537cfa747ad0
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-17 13:55:17 +0000

    Add related links model for common pages

--- cms/common/models.py
+++ cms/common/models.py
@@ -1,10 +1,11 @@
 from typing import List
 
 from django.db import models
-from wagtail.admin.panels import FieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, InlinePanel, ObjectList, TabbedInterface
 from wagtail.api import APIField
 from wagtail.fields import RichTextField
-from wagtail.models import Page
+from wagtail.models import Orderable, Page
 from wagtail.search import index
 
 HEADING_2: str = "h2"
@@ -35,7 +36,44 @@ class CommonPage(Page):
         FieldPanel("body"),
     ]
 
+    sidebar_content_panels = [
+        InlinePanel("related_links", heading="Related links", label="Related link"),
+    ]
+
+    # Sets which fields to expose on the API
     api_fields = [
         APIField("date_posted"),
+        APIField("body"),
+    ]
+
+    # Tabs to position at the top of the view
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Content"),
+            ObjectList(sidebar_content_panels, heading="Related Links"),
+            ObjectList(Page.promote_panels, heading="Promote"),
+        ]
+    )
+
+
+class CommonPageRelatedLink(Orderable):
+    page = ParentalKey(
+        CommonPage, on_delete=models.SET_NULL, null=True, related_name="related_links"
+    )
+    title = models.CharField(max_length=255)
+    url = models.URLField(verbose_name="URL")
+    body = RichTextField(features=[])
+
+    # Sets which panels to show on the editing view
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("url"),
+        FieldPanel("body"),
+    ]
+
+    # Sets which fields to expose on the API
+    api_fields = [
+        APIField("title"),
+        APIField("url"),
         APIField("body"),
     ]
