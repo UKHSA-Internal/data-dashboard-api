1662f8af5af51bcaef5522182a1b713e1624a5d0
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-23 16:32:02 +0000

    Load in postrges env vars. If `APIENV` is set to `DEV` use a local db otherwise use the one dictated by the postrges env variables

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+APIENV = os.environ.get("APIENV")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
