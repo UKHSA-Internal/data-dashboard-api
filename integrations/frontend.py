4b93c2c5860d865db9bc958099b7bf6e0e0a30ea
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-24 16:53:13 +0000

    Add APIClient for triggering the front end cache

--- integrations/frontend.py
+++ integrations/frontend.py
@@ -4,18 +4,18 @@ import requests
 
 
 class APIClient:
-    def __init__(self, url: str, key: str):
-        self.url = url
-        self.key = key
+    def __init__(self, base_url: str, api_key: str):
+        self.base_url = base_url
+        self.api_key = api_key
 
-    def _get_request(self, **kwargs):
+    def _get_request(self, url, **kwargs):
         params = kwargs.pop("params", {})
-        params["secret"] = self.key
-        return requests.get(**kwargs, params=params)
+        params["secret"] = self.api_key
+        return requests.get(url=url, params=params, **kwargs)
 
     @property
     def revalidate_url(self) -> str:
-        return urllib.parse.urljoin(self.url, "api/revalidate")
+        return urllib.parse.urljoin(self.base_url, "api/revalidate")
 
     def send_request_to_revalidate_page(self, slug: str) -> requests.Response:
         return self._get_request(url=self.revalidate_url, params={"slug": slug})
--- tests/unit/integrations/test_frontend.py
+++ tests/unit/integrations/test_frontend.py
@@ -1,18 +1,21 @@
+from unittest import mock
+
 from integrations import frontend
+
+MODULE_PATH = "integrations.frontend"
 
 
 class TestAPIClient:
     def test_revalidate_url_has_correct_path(self):
         """
-        Given
-        When
-        Then
+        Given a base URL
+        When the `revalidate_url` property is called from an instance of the `APIClient`
+        Then the correct URL for the `revalidate` endpoint is returned
         """
         # Given
-        fake_url = "fake-url.com"
-        fake_key = "some-fake-key"
-
-        api_client = frontend.APIClient(url=fake_url, key=fake_key)
+        fake_url = "https://fake-url.com"
+        api_key = "some-fake-key"
+        api_client = frontend.APIClient(base_url=fake_url, api_key=api_key)
 
         # When
         revalidate_url: str = api_client.revalidate_url
@@ -20,22 +23,54 @@ class TestAPIClient:
         # Then
         assert revalidate_url == f"{fake_url}/api/revalidate"
 
-    def test_get_request_passes_key_to_params_of_request(self):
+    @mock.patch(f"{MODULE_PATH}.requests")
+    def test_get_request_passes_key_to_params_of_request(
+        self, requests_spy: mock.MagicMock
+    ):
         """
-        Given
-        When
-        Then
+        Given a base URL and an API key
+        When the `_get_request` property is called from an instance of the `APIClient`
+        Then the `get` is called from the `requests` library with the correct args,
+            including the API key
         """
         # Given
-        fake_url = "fake-url.com"
-        fake_key = "some-fake-key"
-
-        api_client = frontend.APIClient(url=fake_url, key=fake_key)
+        fake_url = "https://fake-url.com"
+        api_key = "some-fake-key"
+        api_client = frontend.APIClient(base_url=fake_url, api_key=api_key)
 
         # When
-        revalidate_url: str = api_client.revalidate_url
+        response = api_client._get_request(url=fake_url)
 
         # Then
-        assert revalidate_url == f"{fake_url}/api/revalidate"
+        expected_params = {"secret": api_key}
+        requests_spy.get.assert_called_once_with(
+            url=fake_url,
+            params=expected_params,
+        )
+        assert response == requests_spy.get.return_value
 
+    @mock.patch(f"{MODULE_PATH}.requests")
+    def test_send_request_to_revalidate_page(self, requests_spy: mock.MagicMock):
+        """
+        Given a base URL and a slug, which is an identifier for a specific page
+        When the `send_request_to_revalidate_page()` is called from an instance of the `APIClient`
+        Then the `get` is called from the `requests` library with the correct args.
+        """
+        # Given
+        fake_url = "https://fake-url.com"
+        api_key = "some-fake-key"
+        slug = "influenza"
+        api_client = frontend.APIClient(base_url=fake_url, api_key=api_key)
 
+        # When
+        response = api_client.send_request_to_revalidate_page(slug=slug)
+
+        # Then
+        expected_params = {"secret": api_key, "slug": slug}
+        expected_url = f"{fake_url}/api/revalidate"
+
+        requests_spy.get.assert_called_once_with(
+            url=expected_url,
+            params=expected_params,
+        )
+        assert response == requests_spy.get.return_value
