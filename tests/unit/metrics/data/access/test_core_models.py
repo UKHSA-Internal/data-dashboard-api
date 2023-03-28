ac9eca5f362d77a0b70908446ccdac384fdba5eb
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-28 09:57:27 +0100

    Add test for `unzip_values()`

--- tests/unit/metrics/data/access/test_core_models.py
+++ tests/unit/metrics/data/access/test_core_models.py
@@ -2,7 +2,29 @@ from unittest import mock
 
 from django.db.models import QuerySet
 
+from metrics.data.access.core_models import _unzip_values
+
 # from metrics.data.access.core_models import get_weekly_disease_incidence
+
+
+def test_unzip_values():
+    """
+    Given a list of 3 * 2-item tuples
+    When `_unzip_values()` is called
+    Then the result is 2 tuples which contain 3 items each
+    """
+    # Given
+    values = [(1, 2), (3, 4), (5, 6)]
+
+    # When
+    unzipped_lists = _unzip_values(values)
+
+    # Then
+    first_index_item_unzipped_result, second_index_item_unzipped_result = unzipped_lists
+
+    assert first_index_item_unzipped_result == (1, 3, 5)
+    assert second_index_item_unzipped_result == (2, 4, 6)
+
 
 # class TestGetWeeklyDiseaseIncidence:
 #     def test_returns_filtered_queryset_casted_to_list(self):
