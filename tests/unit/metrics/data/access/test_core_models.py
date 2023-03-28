4527c63c524638295605d794d3fd4fce07853967
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-28 11:08:30 +0100

    Remove redundant test

--- tests/unit/metrics/data/access/test_core_models.py
+++ tests/unit/metrics/data/access/test_core_models.py
@@ -1,10 +1,4 @@
-from unittest import mock
-
-from django.db.models import QuerySet
-
 from metrics.data.access.core_models import unzip_values
-
-# from metrics.data.access.core_models import get_weekly_disease_incidence
 
 
 def test_unzip_values():
@@ -24,28 +18,3 @@ def test_unzip_values():
 
     assert first_index_item_unzipped_result == (1, 3, 5)
     assert second_index_item_unzipped_result == (2, 4, 6)
-
-
-# class TestGetWeeklyDiseaseIncidence:
-#     def test_returns_filtered_queryset_casted_to_list(self):
-#         """
-#         Given a `CoreTimeSeriesManager` and a mocked `QuerySet`.
-#         When `get_weekly_disease_incidence() is called.
-#         Then the returned object is the filtered `QuerySet`
-#             which has been cast to a list.
-#         """
-#         # Given
-#         mocked_core_time_series_manager = mock.Mock()
-#         mocked_queryset = mock.MagicMock(spec=QuerySet)
-#         mocked_core_time_series_manager.get_metric_values_for_weekly_positivity_by_topic.return_value = (
-#             mocked_queryset
-#         )
-#
-#         # When
-#         weekly_disease_incidence = get_weekly_disease_incidence(
-#             topic=mock.Mock(),  # stubbed
-#             core_time_series_manager=mocked_core_time_series_manager,
-#         )
-#
-#         # Then
-#         assert weekly_disease_incidence == list(mocked_queryset)
