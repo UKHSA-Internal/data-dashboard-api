10734225a4084ea89b2477fcf69248e316a06dc4
Ian.Francis <Ian.Francis@mnscorp.net>
2023-03-29 17:34:32 +0000

    Neutral colour and arrow direction plus pulling date back from Model

--- tests/unit/metrics/data/access/test_generate_dashboard.py
+++ tests/unit/metrics/data/access/test_generate_dashboard.py
@@ -1,12 +1,12 @@
 from unittest import TestCase, mock
 
 from metrics.data.access.dashboard_metadata import ApplyFormatting
-from metrics.data.access.generate_dashboard import format_val, populate_dashboard
+from metrics.data.access.generate_dashboard import format_cell, populate_dashboard
 
 
 class TestFormatVal(TestCase):
     def test_format_good_int(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="123.45",
             formatting={"number_format": ApplyFormatting.IntNoDP},
@@ -14,7 +14,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "123")
 
     def test_format_good_pct_with_brackets(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="123.45678",
             formatting={
@@ -25,7 +25,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "(123.5%)")
 
     def test_format_good_pct(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="123.45678",
             formatting={"number_format": ApplyFormatting.PctOneDP},
@@ -33,7 +33,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "123.5%")
 
     def test_format_unknown(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="123.45",
             formatting={"number_format": "unrecognised"},
@@ -41,7 +41,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "unrecognised")
 
     def test_format_good_negative_int(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="-123.45",
             formatting={"number_format": ApplyFormatting.IntNoDP},
@@ -49,7 +49,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "-123")
 
     def test_format_good_negative_pct_with_brackets(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="-123.45678",
             formatting={
@@ -60,7 +60,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "(-123.5%)")
 
     def test_format_good_negative_pct(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="-123.45678",
             formatting={"number_format": ApplyFormatting.PctOneDP},
@@ -68,23 +68,71 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "-123.5%")
 
     def test_format_get_up_arrow(self):
-        actual: str = format_val(
-            metric_name="ignore",
+        actual: str = format_cell(
+            metric_name="admission",
             metric_value="123.45678",
             formatting={"get_arrow": True},
         )
         self.assertEqual(actual, "up")
 
     def test_format_get_down_arrow(self):
-        actual: str = format_val(
-            metric_name="ignore",
+        actual: str = format_cell(
+            metric_name="admission",
             metric_value="-123.45678",
             formatting={"get_arrow": True},
         )
         self.assertEqual(actual, "down")
 
+    def test_format_get_neutral_arrow(self):
+        actual: str = format_cell(
+            metric_name="deaths",
+            metric_value="0",
+            formatting={"get_arrow": True},
+        )
+        self.assertEqual(actual, "neutral")
+
+    def test_format_get_colour_positive_bad(self):
+        actual: str = format_cell(
+            metric_name="cases",
+            metric_value="123.45678",
+            formatting={"get_colour": True},
+        )
+        self.assertEqual(actual, "red")
+
+    def test_format_get_colour_positive_good(self):
+        actual: str = format_cell(
+            metric_name="vaccination",
+            metric_value="123.45678",
+            formatting={"get_colour": True},
+        )
+        self.assertEqual(actual, "green")
+
+    def test_format_get_colour_negative_bad(self):
+        actual: str = format_cell(
+            metric_name="vaccination",
+            metric_value="-123.45678",
+            formatting={"get_colour": True},
+        )
+        self.assertEqual(actual, "red")
+
+    def test_format_get_colour_negative_good(self):
+        actual: str = format_cell(
+            metric_name="cases",
+            metric_value="-123.45678",
+            formatting={"get_colour": True},
+        )
+        self.assertEqual(actual, "green")
+
+    def test_format_get_neutral_colour(self):
+        actual: str = format_cell(
+            metric_name="admission",
+            metric_value="0",
+            formatting={"get_colour": True},
+        )
+        self.assertEqual(actual, "neutral")
+
     def test_make_absolute(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="-123.45",
             formatting={
@@ -95,7 +143,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "123")
 
     def test_format_negative_unknown(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="-123.45",
             formatting={"number_format": "unrecognised"},
@@ -103,7 +151,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "unrecognised")
 
     def test_format_bad_int(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="no_value",
             formatting={"number_format": ApplyFormatting.IntNoDP},
@@ -111,7 +159,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "no_value")
 
     def test_format_bad_pct_with_brackets(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="no_value",
             formatting={
@@ -122,7 +170,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "no_value")
 
     def test_format_bad_pct(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="no_value",
             formatting={"number_format": ApplyFormatting.PctOneDP},
@@ -130,7 +178,7 @@ class TestFormatVal(TestCase):
         self.assertEqual(actual, "no_value")
 
     def test_format_bad_unknown(self):
-        actual: str = format_val(
+        actual: str = format_cell(
             metric_name="ignore",
             metric_value="no_value",
             formatting={"number_format": "unrecognised"},
@@ -140,8 +188,8 @@ class TestFormatVal(TestCase):
 
 class TestPopulateDashboard(TestCase):
     @mock.patch(
-        "metrics.data.access.generate_dashboard.get_metric_value_from_db",
-        return_value=123.45,
+        "metrics.data.access.generate_dashboard.get_value_from_db",
+        return_value={"metric_value": 123.45},
     )
     def test_populate_dashboard_all_fine(self, mock_get_db_value):
         metadata = {
@@ -176,7 +224,7 @@ class TestPopulateDashboard(TestCase):
         self.assertEqual(actual, expected)
         mock_get_db_value.assert_called_once()
 
-    @mock.patch("metrics.data.access.generate_dashboard.get_metric_value_from_db")
+    @mock.patch("metrics.data.access.generate_dashboard.get_value_from_db")
     def test_populate_dashboard_no_filter(self, mock_get_db_value):
         metadata = {
             "coronavirus": [
@@ -193,8 +241,8 @@ class TestPopulateDashboard(TestCase):
         mock_get_db_value.assert_not_called()
 
     @mock.patch(
-        "metrics.data.access.generate_dashboard.get_metric_value_from_db",
-        return_value=123.45,
+        "metrics.data.access.generate_dashboard.get_value_from_db",
+        return_value={"metric_value": 123.45},
     )
     def test_populate_dashboard_no_formatting(self, mock_get_db_value):
         metadata = {
