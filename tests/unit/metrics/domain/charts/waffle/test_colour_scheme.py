38db919dbefa5ca35189c7ae3ce70652721cd14b
A-Ashiq <afaan.ashiq@nate.tech>
2023-03-31 15:22:51 +0100

    Add integration tests for `line_with_shaded_section`

--- tests/unit/metrics/domain/charts/waffle/test_colour_scheme.py
+++ tests/unit/metrics/domain/charts/waffle/test_colour_scheme.py
@@ -1,44 +1,99 @@
+from typing import List
+
 import pytest
 
 from metrics.domain.charts.waffle import colour_scheme
 
 
-class TestGetRgbColour:
-    @pytest.mark.parametrize(
-        "input_color, expected_string",
-        [
-            ("light_grey", "rgba(216,216,216,1)"),
-            ("light_green", "rgba(119,196,191,1)"),
-            ("middle_green", "rgba(0,156,145,1)"),
-            ("dark_green", "rgba(0,65,61,1)"),
-        ],
-    )
-    def test_returns_correct_string(self, input_color: str, expected_string: str):
+class TestRGBAColours:
+    @pytest.mark.parametrize("rgba_enum", colour_scheme.RGBAColours)
+    def test_stringified_returns_correct_string(
+        self, rgba_enum: colour_scheme.RGBAColours
+    ):
         """
-        Given a human-readable colour like "dark_green"
-        When `get_rgb_colour()` is called
-        Then the corresponding rgba colour representation is returned
+        Given any of the values of the `RGBAColours` enum
+        When the `stringified` property is called
+        Then the correct string is returned for that enum value.
+
+        E.g. If RGBAColours.WHITE.stringified is called,
+            then "rgba(0, 0, 0, 0)" is returned
         """
         # Given
-        human_readable_colour: str = input_color
+        rgba_colour_enum: colour_scheme.RGBAColours = rgba_enum
 
         # When
-        rgb_colour_representation: str = colour_scheme.get_rgb_colour(
-            colour=human_readable_colour
+        stringified_rgba_value: str = rgba_colour_enum.stringified
+
+        # Then
+        assert stringified_rgba_value == f"rgba{rgba_colour_enum.value}"
+
+
+class TestBuildColorScale:
+    def test_identifier_three_produces_darkest_scale(self):
+        """
+        Given an identifier of 3
+        When `build_color_scale()` is called
+        Then a list of nested lists referencing a dark colour scale is returned
+        """
+        # Given
+        identifier = 3
+
+        # When
+        colour_scale: List[List] = colour_scheme.build_color_scale(
+            identifier=identifier
         )
 
         # Then
-        assert rgb_colour_representation == expected_string
+        expected_colour_scale = [
+            [0, colour_scheme.RGBAColours.GREY.stringified],
+            [0.5, colour_scheme.RGBAColours.DARK_GREEN.stringified],
+            [0.9, colour_scheme.RGBAColours.DARK_GREEN.stringified],
+            [1, colour_scheme.RGBAColours.DARK_GREEN.stringified],
+        ]
+        assert colour_scale == expected_colour_scale
 
-    def test_raises_error_for_unsupported_colour(self):
+    def test_identifier_two_produces_middle_scale(self):
         """
-        Given a human-readable colour which is unsupported
-        When `get_rgb_colour()` is called
-        Then a `KeyError` is raised
+        Given an identifier of 2
+        When `build_color_scale()` is called
+        Then a list of nested lists referencing a middle colour scale is returned
         """
         # Given
-        unsupported_colour = "non_existent_colour"
+        identifier = 2
 
-        # When / Then
-        with pytest.raises(KeyError):
-            colour_scheme.get_rgb_colour(colour=unsupported_colour)
+        # When
+        colour_scale: List[List] = colour_scheme.build_color_scale(
+            identifier=identifier
+        )
+
+        # Then
+        expected_colour_scale = [
+            [0, colour_scheme.RGBAColours.GREY.stringified],
+            [0.5, colour_scheme.RGBAColours.MIDDLE_GREEN.stringified],
+            [0.9, colour_scheme.RGBAColours.MIDDLE_GREEN.stringified],
+            [1, colour_scheme.RGBAColours.MIDDLE_GREEN.stringified],
+        ]
+        assert colour_scale == expected_colour_scale
+
+    def test_identifier_one_produces_lightest_scale(self):
+        """
+        Given an identifier of 2
+        When `build_color_scale()` is called
+        Then a list of nested lists referencing a light colour scale is returned
+        """
+        # Given
+        identifier = 1
+
+        # When
+        colour_scale: List[List] = colour_scheme.build_color_scale(
+            identifier=identifier
+        )
+
+        # Then
+        expected_colour_scale = [
+            [0, colour_scheme.RGBAColours.GREY.stringified],
+            [0.5, colour_scheme.RGBAColours.GREY.stringified],
+            [0.9, colour_scheme.RGBAColours.LIGHT_GREEN.stringified],
+            [1, colour_scheme.RGBAColours.LIGHT_GREEN.stringified],
+        ]
+        assert colour_scale == expected_colour_scale
