23607d41b2d2e71ab88134396df001e7295e1057
A-Ashiq <afaan.ashiq@nate.tech>
2023-04-06 13:27:24 +0100

    Inject `TopicManager` and `MetricManager` into `ChartsRequestSerializer`

--- metrics/api/serializers.py
+++ metrics/api/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Manager
 from rest_framework import serializers
 
 from metrics.data.models.api_models import APITimeSeries
@@ -40,13 +41,24 @@ class ChartsQuerySerializer(serializers.Serializer):
 
 class ChartsRequestSerializer(serializers.Serializer):
     topic = serializers.ChoiceField(
-        choices=Topic.objects.all().values_list("name", flat=True), default="COVID-19"
+        choices=[], required=True
     )
     metric = serializers.ChoiceField(
-        choices=Metric.objects.get_all_names(), required=True
+        choices=[], required=True
     )
     chart_type = serializers.ChoiceField(choices=ChartTypes.choices(), required=True)
     date_from = serializers.DateField(required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields["topic"].choices = self.topic_manager.get_all_names()
+        self.fields["metric"].choices = self.metric_manager.get_all_names()
+
+    @property
+    def topic_manager(self) -> Manager:
+        return self.context.get("topic_manager", Topic.objects)
+
+    @property
+    def metric_manager(self) -> Manager:
+        return self.context.get("metric_manager", Metric.objects)
