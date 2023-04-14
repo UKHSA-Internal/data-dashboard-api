ec68c0604bdba840547df556aa1663b26b4a1380
A-Ashiq <afaan.ashiq@nate.tech>
2023-04-14 10:07:01 +0100

    Add hook to register icons

--- cms/dashboard/wagtail_hooks.py
+++ cms/dashboard/wagtail_hooks.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.templatetags.static import static
 from django.utils.html import format_html
 from wagtail import hooks
@@ -8,3 +10,33 @@ def global_admin_css():
     return format_html(
         '<link rel="stylesheet" type="text/css" href="{}">', static("css/theme.css")
     )
+
+
+@hooks.register("register_icons")
+def register_icons(icons: List[str]):
+    """Registers additional svg icons used for the custom content blocks
+
+    Args:
+        icons: List of native svg filename icons
+
+    Returns:
+        List[str]: The list of native
+            and additional custom svg icon filenames
+
+    """
+    return icons + [
+        "icons/chart_card.svg",
+        "icons/standalone_chart.svg",
+        "icons/headline_number.svg",
+        "icons/number.svg",
+        "icons/trend_down.svg",
+        "icons/text.svg",
+    ]
+
+
+@hooks.register("construct_main_menu")
+def hide_images_and_documents_menu_item(request, menu_items):
+    menu_items[:] = [
+        item for item in menu_items if item.name not in ["images", "documents"]
+    ]
+    return menu_items
