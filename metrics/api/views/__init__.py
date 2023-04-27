8658a18ddf1baf97af005f6147e3118e17c6ed8e
Ian.Francis <Ian.Francis@mnscorp.net>
2023-04-27 11:01:56 +0000

    Temporary tabular data endpoint

--- metrics/api/views/__init__.py
+++ metrics/api/views/__init__.py
@@ -2,4 +2,5 @@ from .charts import ChartsView, ChartView
 from .file_upload import FileUploadView
 from .headlines import HeadlinesView
 from .health import HealthView
+from .tabular import TabularDataView
 from .trends import TrendsView
