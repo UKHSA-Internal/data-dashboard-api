e1af090fb359e01b9365570d739734515e347f51
A-Ashiq <afaan.ashiq@nate.tech>
2023-04-28 15:31:45 +0100

    Complete docstring on `MetricsAPIInterface` `

--- cms/metrics_interface/interface.py
+++ cms/metrics_interface/interface.py
@@ -17,7 +17,7 @@ class MetricsAPIInterface:
     Note that this is enforced via the architectural constraints of this codebase.
 
     It is intended that the CMS calls for the data that it needs via model managers
-    from the Metrics API via this abstraction only.
+    from the Metrics API through this abstraction only.
 
     Parameters:
     -----------
@@ -27,6 +27,16 @@ class MetricsAPIInterface:
     metric_manager : `MetricManager`
         The model manager for the `Metric` model belonging to the Metrics API
         Defaults to the concrete `MetricManager` via `Metric.objects`
+    stratum_manager : `StratumManager`
+        The model manager for the `Stratum` model belonging to the Metrics API
+        Defaults to the concrete `StratumManager` via `Stratum.objects`
+    geography_manager : `GeographyManager`
+        The model manager for the `Geography` model belonging to the Metrics API
+        Defaults to the concrete `GeographyManager` via `Geography.objects`
+    geography_type_manager : `GeographyTypeManager`
+        The model manager for the `GeographyType` model belonging to the Metrics API
+        Defaults to the concrete `GeographyTypeManager` via `GeographyType.objects`
+
     """
 
     def __init__(
@@ -138,185 +148,3 @@ class MetricsAPIInterface:
 
         """
         return self.geography_type_manager.get_all_names()
-
-
-LIST_OF_TWO_STRING_TUPLES = List[Tuple[str, str]]
-
-
-def _build_two_item_tuple_choices(choices: List[str]) -> LIST_OF_TWO_STRING_TUPLES:
-    return [(choice, choice) for choice in choices]
-
-
-def get_all_unique_metric_names() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `metric` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Metric` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of unique metric names.
-        Examples:
-            [("new_cases_daily", "new_cases_daily"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(
-        metrics_interface.get_all_unique_metric_names()
-    )
-
-
-def get_chart_types() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `chart_type` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new chart type is added.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of chart_types.
-        Examples:
-            [("line_with_shaded_section", "line_with_shaded_section"), ...]
-
-    """
-    return MetricsAPIInterface.get_chart_types()
-
-
-def get_all_topic_names() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `topic` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Topic` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of topic names.
-        Examples:
-            [("COVID-19", "COVID-19"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(metrics_interface.get_all_topic_names())
-
-
-def get_all_unique_change_type_metric_names() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `metric` fields of trend number CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Metric` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of change type metric names.
-        Examples:
-            [("new_cases_7days_change", "new_cases_7days_change"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(
-        metrics_interface.get_all_unique_change_type_metric_names()
-    )
-
-
-def get_all_unique_percent_change_type_names() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `percentage_metric` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Metric` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of change percent type metric names.
-        Examples:
-            [("new_cases_7days_change_percentage", "new_cases_7days_change_percentage"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(
-        metrics_interface.get_all_unique_percent_change_type_names()
-    )
-
-
-def get_all_stratums() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `stratum` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Topic` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of topic names.
-        Examples:
-            [("0_4", "0_4"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(metrics_interface.get_all_stratum_names())
-
-
-def get_all_geographies() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `geography` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Topic` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of topic names.
-        Examples:
-            [("England", "England"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(metrics_interface.get_all_geography_names())
-
-
-def get_all_geography_types() -> LIST_OF_TWO_STRING_TUPLES:
-    """Callable for the `choices` on the `geography_type` fields of the CMS blocks.
-
-    Notes:
-        This callable wraps the `MetricsAPIInterface`
-        and is passed to a migration for the CMS blocks.
-        This means that we don't need to create a new migration
-        whenever a new `Topic` is added to that table.
-        Instead, the 1-off migration is pointed at this callable.
-        So Wagtail will pull the choices by invoking this function.
-
-    Returns:
-        A list of 2-item tuples of topic names.
-        Examples:
-            [("Nation", "Nation"), ...]
-
-    """
-    metrics_interface = MetricsAPIInterface()
-    return _build_two_item_tuple_choices(
-        metrics_interface.get_all_geography_type_names()
-    )
