eae62a54e1898b91c6625b6b3ac1095e95885554
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-05 13:29:13 +0100

    Wrap tests for `ChartsInterface` within test class

--- tests/unit/metrics/interfaces/charts/test_access.py
+++ tests/unit/metrics/interfaces/charts/test_access.py
@@ -8,231 +8,166 @@ from metrics.interfaces.charts.access import ChartsInterface, make_datetime_from
 MODULE_PATH = "metrics.interfaces.charts.access"
 
 
-@mock.patch.object(ChartsInterface, "generate_simple_line_chart")
-def test_generate_chart_figure_delegates_call_for_simple_line_chart(
-    spy_generate_simple_line_chart_method: mock.MagicMock,
-):
-    """
-    Given a requirement for a `simple_line_graph` chart
-    When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
-    Then the call is delegated to the `generate_simple_line_chart()` method
-    """
-    # Given
-    chart_type: str = ChartTypes.simple_line.value
-    mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
-    mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
-
-    charts_interface = ChartsInterface(
-        chart_plots=mocked_chart_plots,
-        core_time_series_manager=mock.Mock(),
-    )
-
-    # When
-    generated_chart_figure = charts_interface.generate_chart_figure()
-
-    # Then
-    spy_generate_simple_line_chart_method.assert_called_once()
-    assert generated_chart_figure == spy_generate_simple_line_chart_method.return_value
-
-
-@mock.patch.object(ChartsInterface, "generate_line_with_shaded_section_chart")
-def test_generate_chart_figure_delegates_call_for_line_with_shaded_section_chart(
-    spy_generate_line_with_shaded_section_chart_method: mock.MagicMock,
-):
-    """
-    Given a requirement for a `line_with_shaded_section` chart
-    When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
-    Then the call is delegated to the `generate_line_with_shaded_section_chart()` method
-    """
-    # Given
-    chart_type: str = ChartTypes.line_with_shaded_section.value
-    mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
-    mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
-
-    charts_interface = ChartsInterface(
-        chart_plots=mocked_chart_plots,
-        core_time_series_manager=mock.Mock(),
-    )
-
-    # When
-    generated_chart_figure = charts_interface.generate_chart_figure()
-
-    # Then
-    spy_generate_line_with_shaded_section_chart_method.assert_called_once()
-    assert (
-        generated_chart_figure
-        == spy_generate_line_with_shaded_section_chart_method.return_value
-    )
-
-
-@mock.patch.object(ChartsInterface, "generate_bar_chart")
-def test_generate_chart_figure_delegates_call_for_waffle_chart(
-    spy_generate_bar_chart_method: mock.MagicMock,
-):
-    """
-    Given a requirement for a `generate_bar_chart` chart
-    When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
-    Then the call is delegated to the `generate_bar_chart()` method
-    """
-    # Given
-    chart_type: str = ChartTypes.bar.value
-    mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
-    mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
-
-    charts_interface = ChartsInterface(
-        chart_plots=mocked_chart_plots,
-        core_time_series_manager=mock.Mock(),
-    )
-
-    # When
-    generated_chart_figure = charts_interface.generate_chart_figure()
-
-    # Then
-    spy_generate_bar_chart_method.assert_called_once()
-    assert generated_chart_figure == spy_generate_bar_chart_method.return_value
-
-
-@mock.patch.object(ChartsInterface, "generate_line_multi_coloured")
-def test_generate_chart_figure_delegates_call_for_line_multi_coloured(
-    spy_generate_line_multi_coloured_method: mock.MagicMock,
-):
-    """
-    Given a requirement for a `line_multi_coloured` chart
-    When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
-    Then the call is delegated to the `generate_chart_figure()` method
-    """
-    # Given
-    chart_type: str = ChartTypes.line_multi_coloured.value
-    mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
-    mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
-
-    charts_interface = ChartsInterface(
-        chart_plots=mocked_chart_plots,
-        core_time_series_manager=mock.Mock(),
-    )
-
-    # When
-    generated_chart_figure = charts_interface.generate_chart_figure()
-
-    # Then
-    spy_generate_line_multi_coloured_method.assert_called_once()
-    assert (
-        generated_chart_figure == spy_generate_line_multi_coloured_method.return_value
-    )
-
-
-def test_get_timeseries_calls_core_time_series_manager_with_correct_args():
-    """
-    Given a `CoreTimeSeriesManager`
-    When `get_timeseries()` is called from an instance of `ChartsInterface`
-    Then the correct method is called from `CoreTimeSeriesManager` to retrieve the timeseries
-    """
-    # Given
-    spy_core_time_series_manager = mock.Mock()
-    mocked_topic = mock.Mock()
-    mocked_metric = mock.Mock()
-    mocked_date_from = mock.Mock()
-    mocked_geography = mock.Mock()
-    mocked_geography_type = mock.Mock()
-    mocked_stratum = mock.Mock()
-
-    charts_interface = ChartsInterface(
-        chart_plots=mock.MagicMock(),
-        core_time_series_manager=spy_core_time_series_manager,
-    )
-
-    # When
-    timeseries = charts_interface.get_timeseries(
-        topic=mocked_topic,
-        metric=mocked_metric,
-        date_from=mocked_date_from,
-        geography=mocked_geography,
-        geography_type=mocked_geography_type,
-        stratum=mocked_stratum,
-    )
-
-    # Then
-    assert (
-        timeseries
-        == spy_core_time_series_manager.filter_for_dates_and_values.return_value
-    )
-    spy_core_time_series_manager.filter_for_dates_and_values.assert_called_once_with(
-        topic=mocked_topic,
-        metric=mocked_metric,
-        date_from=mocked_date_from,
-        geography=mocked_geography,
-        geography_type=mocked_geography_type,
-        stratum=mocked_stratum,
-    )
-
-
-class TestMakeDatetimeFromString:
-    def test_returns_correct_value(self):
-        """
-        Given a valid date string in the format `%Y-%m-%d`
-        When `make_datetime_from_string()` is called
-        Then a `datetime.datetime` object is returned for the given date
-        """
-        # Given
-        year = "2023"
-        month = "01"
-        day = "01"
-        date_from = f"{year}-{month}-{day}"
-
-        # When
-        parsed_date_from = make_datetime_from_string(date_from=date_from)
-
-        # Then
-        assert type(parsed_date_from) == datetime.datetime
-        assert parsed_date_from.year == int(year)
-        assert parsed_date_from.month == int(month)
-        assert parsed_date_from.day == int(day)
-
-    @mock.patch(f"{MODULE_PATH}.get_date_n_months_ago_from_timestamp")
-    def test_delegates_call_to_get_default_of_one_year_if_none_provided(
+class TestChartsInterface:
+    @mock.patch.object(ChartsInterface, "generate_simple_line_chart")
+    def test_generate_chart_figure_delegates_call_for_simple_line_chart(
         self,
-        spy_get_date_n_months_ago_from_timestamp: mock.MagicMock,
+        spy_generate_simple_line_chart_method: mock.MagicMock,
     ):
         """
-        Given an input `date_from` of None
-        When `make_datetime_from_string()` is called
-        Then `get_date_n_months_ago_from_timestamp()` is called to make a datestamp of 1 year prior to the current date
+        Given a requirement for a `simple_line_graph` chart
+        When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
+        Then the call is delegated to the `generate_simple_line_chart()` method
         """
         # Given
-        date_from = None
+        chart_type: str = ChartTypes.simple_line.value
+        mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
+        mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
+
+        charts_interface = ChartsInterface(
+            chart_plots=mocked_chart_plots,
+            core_time_series_manager=mock.Mock(),
+        )
 
         # When
-        parsed_date_from = make_datetime_from_string(date_from=date_from)
+        generated_chart_figure = charts_interface.generate_chart_figure()
 
         # Then
-        spy_get_date_n_months_ago_from_timestamp.assert_called_once_with(
-            datetime_stamp=datetime.date.today(),
-            number_of_months=12,
+        spy_generate_simple_line_chart_method.assert_called_once()
+        assert (
+            generated_chart_figure == spy_generate_simple_line_chart_method.return_value
         )
-        assert parsed_date_from == spy_get_date_n_months_ago_from_timestamp.return_value
 
-    @mock.patch(f"{MODULE_PATH}.get_date_n_months_ago_from_timestamp")
-    def test_delegates_call_to_get_default_of_one_year_if_empty_string_provided(
+    @mock.patch.object(ChartsInterface, "generate_line_with_shaded_section_chart")
+    def test_generate_chart_figure_delegates_call_for_line_with_shaded_section_chart(
         self,
-        spy_get_date_n_months_ago_from_timestamp: mock.MagicMock,
+        spy_generate_line_with_shaded_section_chart_method: mock.MagicMock,
     ):
         """
-        Given an input `date_from` of an empty string
-        When `make_datetime_from_string()` is called
-        Then `get_date_n_months_ago_from_timestamp()` is called to make a datestamp of 1 year prior to the current date
+        Given a requirement for a `line_with_shaded_section` chart
+        When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
+        Then the call is delegated to the `generate_line_with_shaded_section_chart()` method
         """
         # Given
-        date_from = ""
+        chart_type: str = ChartTypes.line_with_shaded_section.value
+        mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
+        mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
+
+        charts_interface = ChartsInterface(
+            chart_plots=mocked_chart_plots,
+            core_time_series_manager=mock.Mock(),
+        )
 
         # When
-        parsed_date_from = make_datetime_from_string(date_from=date_from)
+        generated_chart_figure = charts_interface.generate_chart_figure()
 
         # Then
-        spy_get_date_n_months_ago_from_timestamp.assert_called_once_with(
-            datetime_stamp=datetime.date.today(),
-            number_of_months=12,
+        spy_generate_line_with_shaded_section_chart_method.assert_called_once()
+        assert (
+            generated_chart_figure
+            == spy_generate_line_with_shaded_section_chart_method.return_value
         )
-        assert parsed_date_from == spy_get_date_n_months_ago_from_timestamp.return_value
+
+    @mock.patch.object(ChartsInterface, "generate_bar_chart")
+    def test_generate_chart_figure_delegates_call_for_waffle_chart(
+        self,
+        spy_generate_bar_chart_method: mock.MagicMock,
+    ):
+        """
+        Given a requirement for a `generate_bar_chart` chart
+        When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
+        Then the call is delegated to the `generate_bar_chart()` method
+        """
+        # Given
+        chart_type: str = ChartTypes.bar.value
+        mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
+        mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
+
+        charts_interface = ChartsInterface(
+            chart_plots=mocked_chart_plots,
+            core_time_series_manager=mock.Mock(),
+        )
+
+        # When
+        generated_chart_figure = charts_interface.generate_chart_figure()
+
+        # Then
+        spy_generate_bar_chart_method.assert_called_once()
+        assert generated_chart_figure == spy_generate_bar_chart_method.return_value
+
+    @mock.patch.object(ChartsInterface, "generate_line_multi_coloured")
+    def test_generate_chart_figure_delegates_call_for_line_multi_coloured(
+        self,
+        spy_generate_line_multi_coloured_method: mock.MagicMock,
+    ):
+        """
+        Given a requirement for a `line_multi_coloured` chart
+        When `generate_chart_figure()` is called from an instance of the `ChartsInterface`
+        Then the call is delegated to the `generate_chart_figure()` method
+        """
+        # Given
+        chart_type: str = ChartTypes.line_multi_coloured.value
+        mocked_chart_plot_params = mock.Mock(chart_type=chart_type)
+        mocked_chart_plots = mock.Mock(plots=[mocked_chart_plot_params])
+
+        charts_interface = ChartsInterface(
+            chart_plots=mocked_chart_plots,
+            core_time_series_manager=mock.Mock(),
+        )
+
+        # When
+        generated_chart_figure = charts_interface.generate_chart_figure()
+
+        # Then
+        spy_generate_line_multi_coloured_method.assert_called_once()
+        assert (
+            generated_chart_figure
+            == spy_generate_line_multi_coloured_method.return_value
+        )
+
+    def test_get_timeseries_calls_core_time_series_manager_with_correct_args(self):
+        """
+        Given a `CoreTimeSeriesManager`
+        When `get_timeseries()` is called from an instance of `ChartsInterface`
+        Then the correct method is called from `CoreTimeSeriesManager` to retrieve the timeseries
+        """
+        # Given
+        spy_core_time_series_manager = mock.Mock()
+        mocked_topic = mock.Mock()
+        mocked_metric = mock.Mock()
+        mocked_date_from = mock.Mock()
+        mocked_geography = mock.Mock()
+        mocked_geography_type = mock.Mock()
+        mocked_stratum = mock.Mock()
+
+        charts_interface = ChartsInterface(
+            chart_plots=mock.MagicMock(),
+            core_time_series_manager=spy_core_time_series_manager,
+        )
+
+        # When
+        timeseries = charts_interface.get_timeseries(
+            topic=mocked_topic,
+            metric=mocked_metric,
+            date_from=mocked_date_from,
+            geography=mocked_geography,
+            geography_type=mocked_geography_type,
+            stratum=mocked_stratum,
+        )
+
+        # Then
+        assert (
+            timeseries
+            == spy_core_time_series_manager.filter_for_dates_and_values.return_value
+        )
+        spy_core_time_series_manager.filter_for_dates_and_values.assert_called_once_with(
+            topic=mocked_topic,
+            metric=mocked_metric,
+            date_from=mocked_date_from,
+            geography=mocked_geography,
+            geography_type=mocked_geography_type,
+            stratum=mocked_stratum,
+        )
 
     @mock.patch(f"{MODULE_PATH}.calculations.get_rolling_period_slice_for_metric")
     @mock.patch.object(ChartsInterface, "calculate_change_in_metric_value")
@@ -322,3 +257,72 @@ class TestMakeDatetimeFromString:
             **fake_chart_plot_parameters.to_dict_for_query(),
             date_from=expected_date_from,
         )
+
+
+class TestMakeDatetimeFromString:
+    def test_returns_correct_value(self):
+        """
+        Given a valid date string in the format `%Y-%m-%d`
+        When `make_datetime_from_string()` is called
+        Then a `datetime.datetime` object is returned for the given date
+        """
+        # Given
+        year = "2023"
+        month = "01"
+        day = "01"
+        date_from = f"{year}-{month}-{day}"
+
+        # When
+        parsed_date_from = make_datetime_from_string(date_from=date_from)
+
+        # Then
+        assert type(parsed_date_from) == datetime.datetime
+        assert parsed_date_from.year == int(year)
+        assert parsed_date_from.month == int(month)
+        assert parsed_date_from.day == int(day)
+
+    @mock.patch(f"{MODULE_PATH}.get_date_n_months_ago_from_timestamp")
+    def test_delegates_call_to_get_default_of_one_year_if_none_provided(
+        self,
+        spy_get_date_n_months_ago_from_timestamp: mock.MagicMock,
+    ):
+        """
+        Given an input `date_from` of None
+        When `make_datetime_from_string()` is called
+        Then `get_date_n_months_ago_from_timestamp()` is called to make a datestamp of 1 year prior to the current date
+        """
+        # Given
+        date_from = None
+
+        # When
+        parsed_date_from = make_datetime_from_string(date_from=date_from)
+
+        # Then
+        spy_get_date_n_months_ago_from_timestamp.assert_called_once_with(
+            datetime_stamp=datetime.date.today(),
+            number_of_months=12,
+        )
+        assert parsed_date_from == spy_get_date_n_months_ago_from_timestamp.return_value
+
+    @mock.patch(f"{MODULE_PATH}.get_date_n_months_ago_from_timestamp")
+    def test_delegates_call_to_get_default_of_one_year_if_empty_string_provided(
+        self,
+        spy_get_date_n_months_ago_from_timestamp: mock.MagicMock,
+    ):
+        """
+        Given an input `date_from` of an empty string
+        When `make_datetime_from_string()` is called
+        Then `get_date_n_months_ago_from_timestamp()` is called to make a datestamp of 1 year prior to the current date
+        """
+        # Given
+        date_from = ""
+
+        # When
+        parsed_date_from = make_datetime_from_string(date_from=date_from)
+
+        # Then
+        spy_get_date_n_months_ago_from_timestamp.assert_called_once_with(
+            datetime_stamp=datetime.date.today(),
+            number_of_months=12,
+        )
+        assert parsed_date_from == spy_get_date_n_months_ago_from_timestamp.return_value
