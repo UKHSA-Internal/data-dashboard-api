beef9adca568d1b45cf6cf7f9b631f419f342e25
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-09 12:30:11 +0100

    Improve type hinting in tests

--- tests/unit/cms/home/test_api.py
+++ tests/unit/cms/home/test_api.py
@@ -21,8 +21,13 @@ class TestBlankHomePage:
         api_fields: List[APIField] = blank_page.api_fields
 
         # Then
-        expected_api_field_names = ["body", "related_links", "last_published_at"]
-        api_field_names = [api_field.name for api_field in api_fields]
+        expected_api_field_names: Set[str] = {
+            "page_description",
+            "body",
+            "related_links",
+            "last_published_at",
+        }
+        api_field_names: Set[str] = {api_field.name for api_field in api_fields}
         assert api_field_names == expected_api_field_names
 
     def test_has_correct_content_panels(self):
@@ -38,8 +43,12 @@ class TestBlankHomePage:
         content_panels: List[FieldPanel] = blank_page.content_panels
 
         # Then
-        expected_content_panel_names: Set[str] = {"title", "page_description", "body"}
-        content_panel_names = set([p.field_name for p in content_panels])
+        expected_content_panel_names: Set[str] = {
+            "title",
+            "page_description",
+            "body",
+        }
+        content_panel_names: Set[str] = {p.field_name for p in content_panels}
         assert content_panel_names == expected_content_panel_names
 
     def test_has_correct_sidebar_panels(self):
@@ -55,8 +64,10 @@ class TestBlankHomePage:
         sidebar_content_panels: List[InlinePanel] = blank_page.sidebar_content_panels
 
         # Then
-        expected_sidebar_content_panel_names: Set[str] = {"related_links"}
-        sidebar_content_panel_names = set(
-            [p.relation_name for p in sidebar_content_panels]
-        )
+        expected_sidebar_content_panel_names: Set[str] = {
+            "related_links",
+        }
+        sidebar_content_panel_names: Set[str] = {
+            p.relation_name for p in sidebar_content_panels
+        }
         assert sidebar_content_panel_names == expected_sidebar_content_panel_names
