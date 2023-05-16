c04c36374d6a38c0593eaec94e85966297fe103c
Ian.Francis <Ian.Francis@mnscorp.net>
2023-05-16 17:01:37 +0000

    View changes plus tests

--- metrics/api/views/__init__.py
+++ metrics/api/views/__init__.py
@@ -3,5 +3,5 @@ from .downloads import DownloadsView
 from .file_upload import FileUploadView
 from .headlines import HeadlinesView
 from .health import HealthView
-from .tabular import TabularView
+from .tabular import TabularView, TabularView_v1
 from .trends import TrendsView
