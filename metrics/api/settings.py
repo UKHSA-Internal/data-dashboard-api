cfcb6ed09ce89c559bf455b4ce2cd1e46d6e7615
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-22 15:31:47 +0100

    Implement bootstrap 3.3.7 for browsable API in place of default theme provided by Django Rest Framework

--- metrics/api/settings.py
+++ metrics/api/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = "metrics.api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
