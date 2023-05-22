d3f0bb1545f979d0badde566bbef26fdc44c4cb2
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-22 16:33:18 +0100

    Fix typo and refactor test

--- tests/integration/metrics/public_api/views/test_theme_list.py
+++ tests/integration/metrics/public_api/views/test_theme_list.py
@@ -9,6 +9,14 @@ from metrics.data.models.api_models import APITimeSeries
 
 
 class TestThemeListView:
+    @property
+    def path(self) -> str:
+        return "/api/public/timeseries/themes/"
+
+    @property
+    def test_server_base_name(self) -> str:
+        return "http://testserver"
+
     @staticmethod
     def _setup_api_time_series(
         **kwargs,
@@ -21,21 +29,17 @@ class TestThemeListView:
             **kwargs,
         )
 
-    @property
-    def path(self) -> str:
-        return "/api/public/timeseries/themes/"
-
     @pytest.mark.django_db
     def test_returns_correct_response(self, client: APIClient):
         """
         Given a valid request
         When the `GET /api/public/timeseries/themes/` endpoint is hit
         Then the response is an HTTP 200 OK
-        And the response contains a list of theme name and links
+        And the response contains a list of theme names and links
         """
         # Given
-        theme = "infectious_disease"
-        self._setup_api_time_series(theme=theme)
+        theme_value = "infectious_disease"
+        self._setup_api_time_series(theme=theme_value)
 
         # When
         response: Response = client.get(path=self.path, format="json")
@@ -45,6 +49,6 @@ class TestThemeListView:
 
         response_data = response.data
         # Check that each object in the response contains a name and link
-        assert response_data[0]["name"] == theme
-        server_base_name = "http://testserver"
-        assert response_data[0]["link"] == f"{server_base_name}{self.path}{theme}"
+        assert response_data[0]["name"] == theme_value
+        expected_theme_link = f"{self.test_server_base_name}{self.path}{theme_value}"
+        assert response_data[0]["link"] == expected_theme_link
