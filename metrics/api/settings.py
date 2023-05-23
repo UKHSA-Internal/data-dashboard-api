f7f1b9d481ea2c3d407b4b7fc3ec271c4e0a0798
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-23 16:37:28 +0100

    Re-add `wagtail.documents` and `wagtail.images` to `INSTALLED_APPS`

--- metrics/api/settings.py
+++ metrics/api/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     "wagtail.sites",
     "wagtail.users",
     "wagtail.snippets",
+    "wagtail.documents",
+    "wagtail.images",
     "wagtail.search",
     "wagtail.admin",
     "wagtail",
