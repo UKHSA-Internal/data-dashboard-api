a676f470d0f75b5ecb779e4ae3ebc907e31b3572
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-23 16:44:35 +0100

    Run `collectstatic` management command in `entrypoint.sh` before starting server so Django has access to static files in non-local environments

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ function set_up_django() {
     echo "Migrating tables"
     python manage.py migrate --noinput
 
+    echo "Collecting static files"
+    python manage.py collectstatic
+
     echo "Starting server"
     python manage.py runserver 0.0.0.0:80
 }
