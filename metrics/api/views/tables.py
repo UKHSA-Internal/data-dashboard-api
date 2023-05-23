bb311123e04fd1ad151944a495fba07e3dd34c4c
Ian.Francis <Ian.Francis@mnscorp.net>
2023-05-23 07:15:13 +0000

    View changes

---
+++ metrics/api/views/tables.py
@@ -0,0 +1,98 @@
+from http import HTTPStatus
+from typing import Dict, List
+
+from drf_spectacular.utils import extend_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_api_key.permissions import HasAPIKey
+
+from metrics.api.serializers.tables import TablesResponseSerializer, TablesSerializer
+from metrics.data.access.core_models import (
+    get_month_end_timeseries_metric_values_from_date,
+)
+from metrics.interfaces.tables import access, validation
+
+
+class OldTabularView(APIView):
+    permission_classes = [HasAPIKey]
+
+    @extend_schema(deprecated=True)
+    def get(self, request, *args, **kwargs):
+        """This endpoint can be used to generate a summary of the chart data but in tabular format
+        There are 2 mandatory URL parameters:
+
+        - `topic` - relates to a type of disease (eg COVID-19, Influenza)
+
+        - `metric` - refers to the type of metric (eg, new_cases_daily, cases_age_sex)
+
+        """
+        topic: str = kwargs["topic"]
+        metric_name: str = kwargs["metric"]
+
+        result: List[Dict[str, str]] = get_month_end_timeseries_metric_values_from_date(
+            metric_name=metric_name, topic=topic
+        )
+
+        return Response(result)
+
+
+class TablesView(APIView):
+    permission_classes = [HasAPIKey]
+
+    @extend_schema(
+        request=TablesSerializer,
+        responses={HTTPStatus.OK.value: TablesResponseSerializer},
+    )
+    def post(self, request, *args, **kwargs):
+        """This endpoint can be used to generate chart data in tabular format
+
+        Multiple plots can be added as an array of objects from the request body.
+        This payload takes the following set of parameters for each plot:
+
+        | Parameter name   | Description                                                                | Example                  | Mandatory |
+        |------------------|----------------------------------------------------------------------------|--------------------------|-----------|
+        | `topic`          | The name of the disease/threat                                             | COVID-19                 | Yes       |
+        | `metric`         | The name of the metric being queried for                                   | new_cases_daily          | Yes       |
+        | `stratum`        | The smallest subgroup a metric can be broken down into                     | 0_4                      | No        |
+        | `geography`      | The geography constraints to apply any data filtering to                   | London                   | No        |
+        | `geography_type` | The type of geographical categorisation to apply any data filtering to     | Nation                   | No        |
+        | `date_from`      | The date from which to start the data slice from. In the format YYYY-MM-DD | 2023-01-01               | No        |
+        | `date_to`        | The date to end the data slice to. In the format YYYY-MM-DD                | 2023-05-01               | No        |
+        | `label`          | The label to assign on the legend for this individual plot                 | 15 to 44 years old       | No        |
+
+        ---
+
+        # Main errors
+
+        There are certain combination of `topic / metric` which do not make sense.
+
+        This is primarily because a set of `metric` values are not available for every `topic`.
+
+        As well as this, certain `metric` names reference data of a certain profile.
+
+        ---
+
+        ## Selected metric not available for topic
+
+        In these cases, this endpoint will return an HTTP 400 BAD REQUEST.
+        For example, if a metric like `new_cases_daily` (which is only used for `COVID-19`) is being asked for with a topic of `Influenza`.
+
+        Then an HTTP 400 BAD REQUEST is returned with the following error message:
+            `Influenza` does not have a corresponding metric of `COVID-19`
+        """
+        request_serializer = TablesSerializer(data=request.data)
+        request_serializer.is_valid(raise_exception=True)
+
+        table_plot_models = request_serializer.to_models()
+
+        try:
+            tabular_data_response: str = access.generate_tabular_output(
+                table_plots=table_plot_models
+            )
+
+        except validation.MetricDoesNotSupportTopicError as error:
+            return Response(
+                status=HTTPStatus.BAD_REQUEST, data={"error_message": str(error)}
+            )
+
+        return Response(tabular_data_response)
--- tests/integration/metrics/api/views/test_tabular.py
+++ tests/integration/metrics/api/views/test_tabular.py
@@ -8,7 +8,7 @@ from rest_framework.test import APIClient
 from metrics.data.models.core_models import CoreTimeSeries, Metric, Topic
 
 
-class TestTabularView:
+class TestTablesView:
     @staticmethod
     def _setup_time_series(
         topic_name: str, metric_name: str, metric_value: float
@@ -26,7 +26,7 @@ class TestTabularView:
 
     @property
     def path(self) -> str:
-        return "/tabular/v2/"
+        return "/tables/v2/"
 
     metric_name = "vaccinations_percentage_uptake_spring22"
     topic_name = "COVID-19"
@@ -38,11 +38,11 @@ class TestTabularView:
         """
         Given a valid payload to create a chart
         And an authenticated APIClient
-        When the `POST /tabular/v2` endpoint is hit i.e. without the trailing `/`
+        When the `POST /tables/v2` endpoint is hit i.e. without the trailing `/`
         Then the response is still a valid HTTP 200 OK
         """
         # Given
-        path_without_trailing_forward_slash: str = "/tabular/v2"
+        path_without_trailing_forward_slash: str = "/tables/v2"
         self._setup_time_series(
             metric_name=self.metric_name, metric_value=13, topic_name=self.topic_name
         )
@@ -71,7 +71,7 @@ class TestTabularView:
         """
         Given a valid payload to create a chart
         And an authenticated APIClient
-        When the `POST /tabular/v2/` endpoint is hit
+        When the `POST /tables/v2/` endpoint is hit
         Then the response is not an HTTP 401 UNAUTHORIZED
         """
         # Given
@@ -98,14 +98,14 @@ class TestTabularView:
         assert response.status_code != HTTPStatus.UNAUTHORIZED
         assert response.status_code == HTTPStatus.OK
 
-        # Check that the headers on the response indicate an `svg` image being returned
+        # Check that the headers on the response indicate a json-type reponse is being returned
         assert response.headers["Content-Type"] == "application/json"
 
     @pytest.mark.django_db
     def test_post_request_without_api_key_is_unauthorized(self):
         """
         Given an APIClient which is not authenticated
-        When the `GET /tabular/v2/` endpoint is hit
+        When the `GET /tables/v2/` endpoint is hit
         Then an HTTP 401 UNAUTHORIZED response is returned
         """
         # Given
@@ -124,12 +124,14 @@ class TestTabularView:
         """
         Given a valid payload to create a chart
         And an authenticated APIClient
-        When the `POST /tabular/v2/` endpoint is hit with a single plot
+        When the `POST /tables/v2/` endpoint is hit with a single plot
         Then the response is of the correct format
         """
         # Given
         self._setup_time_series(
-            metric_name=self.metric_name, metric_value=13, topic_name=self.topic_name
+            metric_name=self.metric_name,
+            metric_value=123,
+            topic_name=self.topic_name,
         )
         valid_payload = {
             "file_format": "svg",
@@ -142,7 +144,17 @@ class TestTabularView:
             ],
         }
 
-        expected_response = [{"date": "2023-01-01", "Plot1": "13.0"}]
+        expected_response = [
+            {
+                "date": "2023-01-01",
+                "values": [
+                    {
+                        "label": "Plot1",
+                        "value": "123.0",
+                    },
+                ],
+            }
+        ]
 
         # When
         response: Response = authenticated_api_client.post(
@@ -159,16 +171,20 @@ class TestTabularView:
         """
         Given a valid payload to create a chart
         And an authenticated APIClient
-        When the `POST /tabular/v2/` endpoint is hit with multiple plots
+        When the `POST /tables/v2/` endpoint is hit with multiple plots
         Then the response is of the correct format
         """
         # Given
         self._setup_time_series(
-            metric_name=self.metric_name, metric_value=123, topic_name=self.topic_name
+            metric_name=self.metric_name,
+            metric_value=123,
+            topic_name=self.topic_name,
         )
         # Add another
         self._setup_time_series(
-            metric_name=self.metric_name, metric_value=123, topic_name=self.topic_name
+            metric_name=self.metric_name,
+            metric_value=123,
+            topic_name=self.topic_name,
         )
         valid_payload = {
             "file_format": "svg",
@@ -188,7 +204,13 @@ class TestTabularView:
         }
 
         expected_response = [
-            {"date": "2023-01-01", "values": {"Plot1": "123.0", "plot_label": "123.0"}}
+            {
+                "date": "2023-01-01",
+                "values": [
+                    {"label": "Plot1", "value": "123.0"},
+                    {"label": "plot_label", "value": "123.0"},
+                ],
+            }
         ]
 
         # When
