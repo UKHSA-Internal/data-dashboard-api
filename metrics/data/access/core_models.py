f4c12cf8ac1f2079681c50bd51c3a29d543f42be
A-Ashiq <afaan.ashiq@nate.tech>
2023-05-25 11:49:05 +0100

    Pass `PlotParameters` model from `tabular` endpoint to take advantage of `date_from` default logic

--- metrics/data/access/core_models.py
+++ metrics/data/access/core_models.py
@@ -4,13 +4,11 @@ This shall only include functionality which is used to read from the database.
 
 Specifically, this file contains read database logic for the Core models only.
 """
-import datetime
 from typing import Dict, List, Tuple
 
 from django.db.models import Manager, QuerySet
 
 from metrics.data.models.core_models import CoreTimeSeries
-from metrics.domain.models import get_date_n_months_ago_from_timestamp
 
 DEFAULT_CORE_TIME_SERIES_MANAGER = CoreTimeSeries.objects
 
@@ -30,28 +28,21 @@ def unzip_values(values) -> Tuple[List, List]:
 
 
 def get_month_end_timeseries_metric_values_from_date(
-    metric_name: str,
-    topic_name: str,
+    plot_parameters,
     core_time_series_manager: Manager = DEFAULT_CORE_TIME_SERIES_MANAGER,
 ) -> List[Dict[str, str]]:
     """
     Fetch the month-end timeseries values for the given topic & metric
      Args:
-         metric_name: The required metric (eg. new_cases_daily)
-         topic_name: The required topic (eg. COVID-19)
+         plot_parameters: The required plot parameters
          core_time_series_manager: The timeseries manager. Default is the CoreTimeSeries manager
      Returns:
          A dictionary of date:metric_value pairs
     """
-    today = datetime.datetime.today()
-    n_months_ago: datetime.date = get_date_n_months_ago_from_timestamp(
-        datetime_stamp=today
-    )
-
     queryset = core_time_series_manager.by_topic_metric_for_dates_and_values(
-        topic_name=topic_name,
-        metric_name=metric_name,
-        date_from=n_months_ago,
+        topic_name=plot_parameters.topic_name,
+        metric_name=plot_parameters.metric_name,
+        date_from=plot_parameters.date_from_value,
     )
 
     months: QuerySet = queryset.dates("dt", kind="month")
