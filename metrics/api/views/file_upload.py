9cb4e901f63a473ca25641d1b65ae13ab91eb4c1
A-Ashiq <afaan.ashiq@nate.tech>
2023-06-02 09:25:20 +0100

    Remove `generate_api_time_series()` call from `upload/` endpoint

--- metrics/api/views/file_upload.py
+++ metrics/api/views/file_upload.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_api_key.permissions import HasAPIKey
 
-from metrics.data.operations.api_models import generate_api_time_series
 from metrics.data.operations.core_models import load_core_data
 
 UPLOAD_API_TAG = ["upload"]
@@ -30,5 +29,4 @@ class FileUploadView(APIView):
         Note that this endpoint is **deprecated** and should only be used for demo/testing purposes.
         """
         load_core_data(filename=request.FILES.get("file"))
-        generate_api_time_series()
         return Response(status=204)
