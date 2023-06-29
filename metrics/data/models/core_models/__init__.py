fbc8274a0bd1e4bad5b87686d3bcb3e5d3c8a449
A-Ashiq <afaan.ashiq@nate.tech>
2023-06-29 13:33:51 +0100

    Spit `core_models.py` into -> `timeseries.py` and `supporting.py`

---
+++ metrics/data/models/core_models/__init__.py
@@ -0,0 +1,10 @@
+from .supporting import (
+    Geography,
+    GeographyType,
+    Metric,
+    Stratum,
+    SubTheme,
+    Theme,
+    Topic,
+)
+from .timeseries import CoreTimeSeries
--- metrics/data/models/core_models/supporting.py
+++ metrics/data/models/core_models/supporting.py
@@ -1,25 +1,12 @@
-"""
-For any given API we will require:
- - An API View
- - Flat model generated from the core data
- - Function to generate the flat model from the core data models
-
-This file contains the core models only.
-The core models should be populated via data ingress (file uploads)
-"""
-
 from django.db import models
 from django.utils import timezone
 
-from metrics.data.enums import TimePeriod
 from metrics.data.managers.core_models.geography import GeographyManager
 from metrics.data.managers.core_models.geography_type import GeographyTypeManager
 from metrics.data.managers.core_models.metric import MetricManager
 from metrics.data.managers.core_models.stratum import StratumManager
-from metrics.data.managers.core_models.time_series import CoreTimeSeriesManager
 from metrics.data.managers.core_models.topic import TopicManager
-
-CHAR_COLUMN_MAX_CONSTRAINT: int = 50
+from metrics.data.models.core_models.headline import CHAR_COLUMN_MAX_CONSTRAINT
 
 
 class Theme(models.Model):
@@ -67,43 +54,3 @@ class Stratum(models.Model):
     name = models.CharField(max_length=CHAR_COLUMN_MAX_CONSTRAINT)
 
     objects = StratumManager()
-
-
-class CoreTimeSeries(models.Model):
-    period = models.CharField(
-        max_length=1,
-        choices=TimePeriod.choices(),
-    )
-    geography = models.ForeignKey(
-        to=Geography,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    metric = models.ForeignKey(
-        to=Metric,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    stratum = models.ForeignKey(
-        to=Stratum,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    sex = models.CharField(
-        max_length=3,
-        null=True,
-    )
-
-    year = models.PositiveSmallIntegerField()
-    epiweek = models.PositiveSmallIntegerField()
-
-    dt = models.DateField()
-    metric_value = models.DecimalField(
-        max_digits=11,
-        decimal_places=1,
-    )
-
-    objects = CoreTimeSeriesManager()
-
-    def __str__(self):
-        return f"Core Data for {self.dt}, metric '{self.metric.name}', value: {self.metric_value}"
