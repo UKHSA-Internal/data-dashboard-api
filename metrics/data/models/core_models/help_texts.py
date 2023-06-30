2b5d6032e07128330550e7a172efade9825da1d0
A-Ashiq <afaan.ashiq@nate.tech>
2023-06-30 09:31:20 +0100

    Add model field `help_texts`

---
+++ metrics/data/models/core_models/help_texts.py
@@ -0,0 +1,21 @@
+REFRESH_DATE = """
+The date which this piece of data was last updated.
+"""
+PERIOD_START = """
+Start of the period for which this figure is being provided.
+"""
+PERIOD_END = """
+End of the period for which this figure is being provided.
+"""
+
+# Common help texts
+STRATUM = """
+Smallest subgroup a metric can be broken down into (e.g. ethnicity, testing pillar) excluding age and sex.
+If a metric cannot be further broken down into strata, the value for stratum will be 'default'
+"""
+AGE = """
+Age band of patients.
+"""
+SEX = """
+Sex of patients.
+"""
