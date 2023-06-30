064a8f14fce6452d1ebbd84d1fa17adceace4bf5
A-Ashiq <afaan.ashiq@nate.tech>
2023-06-30 09:33:52 +0100

    Add `CoreHeadline` model

---
+++ metrics/data/models/core_models/headline.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from metrics.data.enums import TimePeriod
+from metrics.data.models.core_models import help_texts
+from metrics.data.models.core_models.supporting import Age, Geography, Metric, Stratum
+
+
+class CoreHeadline(models.Model):
+    metric = models.ForeignKey(
+        to=Metric,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    geography = models.ForeignKey(
+        to=Geography,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    stratum = models.ForeignKey(
+        to=Stratum,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text=help_texts.STRATUM,
+    )
+    age = models.ForeignKey(
+        to=Age,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text=help_texts.AGE,
+    )
+    sex = models.CharField(
+        max_length=3,
+        null=True,
+        help_text=help_texts.SEX,
+    )
+    metric_frequency = models.CharField(
+        max_length=1,
+        choices=TimePeriod.choices(),
+    )
+
+    refresh_date = models.DateField(help_text=help_texts.REFRESH_DATE)
+    period_start = models.DateField(help_text=help_texts.PERIOD_START)
+    period_end = models.DateField(help_text=help_texts.PERIOD_END)
+
+    metric_value = models.DecimalField(
+        max_digits=11,
+        decimal_places=1,
+    )
+
+    def __str__(self):
+        return f"Core Headline Data for {self.refresh_date}, metric '{self.metric.name}', value: {self.metric_value}"
