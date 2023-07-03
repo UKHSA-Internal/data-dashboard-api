4702cd1f2c84a827d1cac52b30b7f573141954eb
A-Ashiq <afaan.ashiq@nate.tech>
2023-07-03 15:54:55 +0100

    Add type hints

--- cms/dashboard/viewsets.py
+++ cms/dashboard/viewsets.py
@@ -1,5 +1,9 @@
+from typing import List
+
 from django.urls import path
+from django.urls.resolvers import RoutePattern
 from drf_spectacular.utils import extend_schema
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_api_key.permissions import HasAPIKey
 from wagtail.api.v2.views import PagesAPIViewSet
@@ -17,16 +21,19 @@ class CMSDraftPagesViewSet(PagesAPIViewSet):
     base_serializer_class = CMSDraftPagesSerializer
     permission_classes = [HasAPIKey]
 
-    def detail_view(self, request, pk):
+    def detail_view(self, request: Request, pk: int):
         instance = self.get_object()
         instance = instance.get_latest_revision_as_object()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
     @classmethod
-    def get_urlpatterns(cls):
-        """
-        This returns a list of URL patterns for the endpoint
+    def get_urlpatterns(cls) -> List[RoutePattern]:
+        """This returns a list of URL patterns for the viewset.
+
+        Notes:
+            Only the detail `/{id}` path is included.
+
         """
         return [
             path("<int:pk>/", cls.as_view({"get": "detail_view"}), name="detail"),
