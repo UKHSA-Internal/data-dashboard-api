eadcb871e4be95c7a03fef3b6550b270a9b985bc
A-Ashiq <afaan.ashiq@nate.tech>
2023-07-05 10:36:44 +0100

    Add API key in front of `/api/suggestions/v1/` endpoint

--- feedback/views/suggestions.py
+++ feedback/views/suggestions.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from drf_spectacular.utils import extend_schema
 from rest_framework.request import Request
 from rest_framework.views import APIView
+from rest_framework_api_key.permissions import HasAPIKey
 
 from feedback import send_email
 from feedback.serializers.questions import SuggestionsSerializer
@@ -12,7 +13,7 @@ SUGGESTIONS_API_TAG = "suggestions"
 
 
 class SuggestionsView(APIView):
-    permission_classes = []
+    permission_classes = [HasAPIKey]
 
     @extend_schema(tags=[SUGGESTIONS_API_TAG], request=SuggestionsSerializer)
     def post(self, request: Request, *args, **kwargs) -> HttpResponse:
