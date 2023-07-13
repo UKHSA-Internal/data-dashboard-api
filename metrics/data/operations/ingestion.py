fcf978afb39e4e69e390eb94faba6fec7cf633cb
A-Ashiq <afaan.ashiq@nate.tech>
2023-07-13 16:20:05 +0100

    Add `create_core_timeseries()`

--- metrics/data/operations/ingestion.py
+++ metrics/data/operations/ingestion.py
@@ -1,9 +1,11 @@
 from django.db.models.manager import Manager
 
-from metrics.data.models.core_models import CoreHeadline
+from metrics.data.models.core_models import CoreHeadline, CoreTimeSeries
 
 DEFAULT_CORE_HEADLINE_MANAGER = CoreHeadline.objects
 DEFAULT_BATCH_SIZE = 100
+
+DEFAULT_CORE_TIMESERIES_MANAGER = CoreTimeSeries.objects
 
 
 def create_core_headlines(
@@ -45,6 +47,53 @@ def create_core_headlines(
 
     core_headline_manager.bulk_create(
         objs=core_headline_model_instances,
+        ignore_conflicts=True,
+        batch_size=batch_size,
+    )
+
+
+def create_core_timeseries(
+    timeseries_dtos: list["TimeSeriesDTO"],
+    core_timeseries_manager: Manager = DEFAULT_CORE_TIMESERIES_MANAGER,
+    batch_size: int = DEFAULT_BATCH_SIZE,
+) -> None:
+    """Creates `CoreTimeSeries` records from a list of TimeSeriesDTOs
+
+    Args:
+        timeseries_dtos: A list of TimeSeriesDTOs
+            which have been enriched with all the correct fields
+            and ready to be translated into database records
+        batch_size: Controls the number of objects created
+            in a single write query to the database.
+            Defaults to 100.
+        core_timeseries_manager: The model manager for `CoreTimeSeries`
+            Defaults to the concrete `CoreTimeSeriesManager`
+            via `CoreTimeSeries.objects`
+
+    Returns:
+        None
+
+    """
+    core_timeseries_model_instances: list[CoreHeadline] = [
+        core_timeseries_manager.model(
+            metric_id=int(timeseries_dto.metric),
+            geography_id=int(timeseries_dto.geography),
+            stratum_id=int(timeseries_dto.stratum),
+            age_id=int(timeseries_dto.age),
+            sex=timeseries_dto.sex,
+            metric_frequency=timeseries_dto.metric_frequency,
+            year=timeseries_dto.metric_frequency,
+            month=timeseries_dto.month,
+            epiweek=timeseries_dto.epiweek,
+            date=timeseries_dto.date,
+            refresh_date=timeseries_dto.refresh_date,
+            metric_value=timeseries_dto.metric_value,
+        )
+        for timeseries_dto in timeseries_dtos
+    ]
+
+    core_timeseries_manager.bulk_create(
+        objs=core_timeseries_model_instances,
         ignore_conflicts=True,
         batch_size=batch_size,
     )
