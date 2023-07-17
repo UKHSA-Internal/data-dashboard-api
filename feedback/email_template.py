50dc0971462d0b82e80fdfe2e4b4e154e92854f4
A-Ashiq <afaan.ashiq@nate.tech>
2023-07-17 14:20:25 +0100

    Set functions for enriching suggestions and email body as private

--- feedback/email_template.py
+++ feedback/email_template.py
@@ -1,6 +1,25 @@
 from enum import Enum
 
 
+def build_body_for_email(suggestions: dict[str, str]) -> str:
+    """Builds the suggestions email body as a string to be sent to the email server
+
+    Args:
+        suggestions: A dict containing:
+            keys - Shortform questions
+            values - Answers provided by the user
+
+    Returns:
+        A continuous string which represents
+        the body of the feedback email
+
+    """
+    enriched_suggestions = _enrich_suggestions_with_long_form_questions(
+        suggestions=suggestions
+    )
+    return _build_body_from_suggestions(suggestions=enriched_suggestions)
+
+
 class FeedbackQuestion(Enum):
     reason = "What was your reason for visiting the dashboard today?"
     improve_experience = "How could we improve your experience with the dashboard?"
@@ -8,7 +27,7 @@ class FeedbackQuestion(Enum):
     did_you_find_everything = "Did you find everything you were looking for?"
 
 
-def enrich_suggestions_with_long_form_questions(
+def _enrich_suggestions_with_long_form_questions(
     suggestions: dict[str, str]
 ) -> dict[str, str]:
     """Enriches the question keys in the given `suggestions` with the long form versions
@@ -31,3 +50,22 @@ def enrich_suggestions_with_long_form_questions(
         question.value: suggestions[question.name]
         for question in list(FeedbackQuestion)
     }
+
+
+def _build_body_from_suggestions(suggestions: dict[str, str]) -> str:
+    """Builds the suggestions email body as a string to be sent to the email server
+
+    Args:
+        suggestions: A dict containing:
+            keys - Longform questions
+            values - Answers provided by the user
+
+    Returns:
+        A continuous string which represents
+        the body of the feedback email
+
+    """
+    email_body = ""
+    for question, answer in suggestions.items():
+        email_body += f"Question: {question}\nAnswer: {answer}\n\n"
+    return email_body
