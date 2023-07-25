24173fb9e34a8757365dd57791c00d0647589552
Phillip Stanley <phillip.stanley1@nhs.net>
2023-07-25 17:04:01 +0100

    update formatting for test geography

--- tests/unit/metrics/data/models/core_models/supporting.py
+++ tests/unit/metrics/data/models/core_models/supporting.py
@@ -6,11 +6,10 @@ from tests.fakes.models.metrics.geography import FakeGeography
 
 
 class TestGeography:
-    geography_code = "E450000012f"
+    geography_code = "E45000001"
 
     def test_geography_code_exists(self):
-        """Tests that the geography_code field is available on geography model.
-
+        """
         Given I have a valid geography code.
         When I initialise a new instance of the geography model passing the geography_code.
         Then the value will be assigned to the model's geography_code field.
@@ -24,12 +23,11 @@ class TestGeography:
         # Then
         assert geography_model.geography_code == geography_code
 
-    def test_geography_code_max_legnth(self):
-        """Tests the max length property of the geography_code field.
-
+    def test_geography_code_max_length(self):
+        """
         Given I have a geography_code and a max_length value
         When I initialise a new instance of the geography model passing in the geography code.
-        Then The instance should have meta data of a max_length matching the max_length_constraint
+        Then the instance should have meta data of a max_length matching the max_length_constraint
         """
         # Given
         geography_code = self.geography_code
@@ -37,11 +35,11 @@ class TestGeography:
 
         # When
         geography_model = FakeGeography(geography_code=geography_code)
-        geography_code_field = [
-            item
-            for item in geography_model._meta.concrete_fields
-            if item.attname == "geography_code"
-        ]
+        geography_code_field = next(
+            field
+            for field in geography_model._meta.concrete_fields
+            if field.attname == "geography_code"
+        )
 
         # Then
-        assert geography_code_field[0].max_length == max_length_constraint
+        assert geography_code_field.max_length == max_length_constraint
