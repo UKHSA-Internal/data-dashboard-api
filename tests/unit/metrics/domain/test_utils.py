13de05fb15173f07fec295aca5acace78776b138
A-Ashiq <afaan.ashiq@nate.tech>
2023-08-08 17:38:42 +0100

    Allow for Beta-type metric names to be captured by `_check_for_substring_match()`

--- tests/unit/metrics/domain/test_utils.py
+++ tests/unit/metrics/domain/test_utils.py
@@ -1,6 +1,10 @@
 import pytest
 
-from metrics.domain.utils import ChartAxisFields, get_axis_name
+from metrics.domain.utils import (
+    ChartAxisFields,
+    _check_for_substring_match,
+    get_axis_name,
+)
 
 
 class TestGetAxisName:
@@ -117,3 +121,69 @@ class TestGetAxisName:
 
         # Then
         assert axis_value == ChartAxisFields.metric.value
+
+
+class TestCheckForSubstringMatch:
+    @pytest.mark.parametrize(
+        "metric_name",
+        [
+            "influenza_headline_ICUHDUadmissionrateLatest",
+            "influenza_healthcare_ICUHDUadmissionrateByWeek",
+            "COVID-19_headline_positivity_latest",
+            "COVID-19_headline_7DayAdmissions",
+            "RSV_testing_positivityByWeek",
+            "COVID-19_cases_countRollingMean",
+            "parainfluenza_testing_positivityByWeek",
+            "COVID-19_headline_newcases_7daychange",
+            "rhinovirus_headline_positivityLatest",
+            "RSV_healthcare_hospadmissionrateByWeek",
+            "COVID-19_headline_ONSdeaths_7daypercentchange",
+            "COVID-19_cases_casesByDay",
+            "COVID-19_healthcare_OccupiedBedsByDay",
+            "parainfluenza_headline_positivityLatest",
+            "COVID-19_testing_7daypositivity",
+            "COVID-19_headline_newcases_7daytotals",
+            "COVID-19_headline_7DayAdmissionsPercentChange",
+            "RSV_headline_positivityLatest",
+            "COVID-19_healthcare_AdmissionsByDay",
+            "COVID-19_deaths_ONSByDay",
+            "COVID-19_deaths_ONSRollingMean",
+            "COVID-19_healthcare_AdmissionsRollingMean",
+            "adenovirus_headline_positivityLatest",
+            "influenza_headline_positivityLatest",
+            "hMPV_testing_positivityByWeek",
+            "rhinovirus_testing_positivityByWeek",
+            "hMPV_headline_positivityLatest",
+            "influenza_headline_ICUHDUadmissionrateChange",
+            "COVID-19_headline_ONSdeaths_7daytotals",
+            "COVID-19_headline_newcases_7daypercentchange",
+            "COVID-19_deaths_ONSByWeek",
+            "COVID-19_headline_ONSdeaths_7daychange",
+            "RSV_headline_hospadmissionrateLatest",
+            "COVID-19_headline_7DayAdmissionsChange",
+            "COVID-19_cases_rateRollingMean",
+        ],
+    )
+    def test_returns_for_correct_strings(self, metric_name: str):
+        """
+        Given a metric name and a collection of substrings
+            of which 1 substring is contained within the metric name
+        When `_check_for_substring_match()` is called
+        Then True is returned
+        """
+        # Given
+        substrings: tuple[str, ...] = (
+            "cases",
+            "deaths",
+            "healthcare",
+            "admission",
+            "positivity",
+        )
+
+        # When
+        substrings_are_matching: bool = _check_for_substring_match(
+            string_to_check=metric_name, substrings=substrings
+        )
+
+        # Then
+        assert substrings_are_matching
