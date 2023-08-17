6d6181bddcd4ad78126064b3cb15498d07881144
A-Ashiq <afaan.ashiq@nate.tech>
2023-08-17 09:40:14 +0100

    Update `FakeGeographyManager.get_all_names()` to return deduplicated names

--- tests/fakes/managers/geography_manager.py
+++ tests/fakes/managers/geography_manager.py
@@ -12,4 +12,4 @@ class FakeGeographyManager(GeographyManager):
         super().__init__(**kwargs)
 
     def get_all_names(self) -> list[str]:
-        return [geography.name for geography in self.geographies]
+        return list(set([geography.name for geography in self.geographies]))
