bc5457b933df177a16454778c5c25b1a337371e6
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-08 17:36:25 +0100

    Add custom model manager for `TopicManager`

---
+++ cms/topic/managers.py
@@ -0,0 +1,40 @@
+"""
+This file contains the custom QuerySet and Manager classes associated with the `TopicPage` model.
+
+Note that the application layer should only call into the `Manager` class.
+The application should not interact directly with the `QuerySet` class.
+"""
+from django.db import models
+from wagtail.models import BasePageManager
+from wagtail.query import PageQuerySet
+
+
+class TopicPageQuerySet(PageQuerySet):
+    """Custom queryset which can be used by the `TopicPageManager`"""
+
+    def get_live_pages(self) -> models.QuerySet:
+        """Gets the all currently live pages.
+
+        Returns:
+            QuerySet: A queryset of the live pages:
+                Examples:
+                    `<TopicPageQuerySet [<TopicPage: COVID-19>, <TopicPage: Influenza>, ...]>`
+        """
+        return self.filter(live=True)
+
+
+class TopicPageManager(BasePageManager):
+    """Custom model manager class for the `TopicPage` model."""
+
+    def get_queryset(self) -> TopicPageQuerySet:
+        return TopicPageQuerySet(model=self.model, using=self.db)
+
+    def get_live_pages(self) -> models.QuerySet:
+        """Gets the all currently live pages.
+
+        Returns:
+            QuerySet: A queryset of the live pages:
+                Examples:
+                    `<TopicPageQuerySet [<TopicPage: COVID-19>, <TopicPage: Influenza>, ...]>`
+        """
+        return self.get_queryset().get_live_pages()
