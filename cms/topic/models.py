b1c59cb34427b6ee9e887a0dc22307d2efe738ae
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-08 17:41:00 +0100

    Pass `TopicPageManager` to `TopicPage.objects`

--- cms/topic/models.py
+++ cms/topic/models.py
@@ -9,7 +9,7 @@ from wagtail.search import index
 from cms.common.models import AVAILABLE_RICH_TEXT_FEATURES
 from cms.dynamic_content import help_texts
 from cms.dynamic_content.access import ALLOWABLE_BODY_CONTENT
-from metrics.data.managers.core_models.topic import TopicManager
+from cms.topic.managers import TopicPageManager
 
 
 class TopicPage(Page):
@@ -57,7 +57,7 @@ class TopicPage(Page):
         ]
     )
 
-    objects = TopicManager()
+    objects = TopicPageManager()
 
     def is_previewable(self) -> bool:
         """Returns False. Since this is a headless CMS the preview panel is not supported"""
