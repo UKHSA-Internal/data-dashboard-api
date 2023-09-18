d320987d73edb917655ab958969a507860093d14
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-18 19:20:29 +0100

    Add `construct_urlpatterns_for_feedback()`

---
+++ tests/unit/feedback/api/test_urls.py
@@ -0,0 +1,26 @@
+from django.urls import URLPattern
+
+from feedback.api.urls import construct_urlpatterns_for_feedback
+
+
+class TestConstructUrlPatternsForFeedback:
+    def test_returns_correct_url_patterns(self):
+        """
+        Given a url prefix
+        When `construct_urlpatterns_for_feedback()` is called
+        Then the list of the 1 expected url pattern is returned
+        """
+        # Given
+        prefix = "fake-api/"
+
+        # When
+        constructed_urlpatterns: list[URLPattern] = construct_urlpatterns_for_feedback(
+            prefix=prefix
+        )
+
+        # Then
+        assert len(constructed_urlpatterns) == 1
+        assert (
+            constructed_urlpatterns[0].pattern.regex.pattern
+            == f"^{prefix}suggestions/v1"
+        )
