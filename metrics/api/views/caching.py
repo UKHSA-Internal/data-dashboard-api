96baae9add56018af55ee108abe240c4d20ee578
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-19 10:04:59 +0100

    Remove API key from `caching` endpoint

--- metrics/api/views/caching.py
+++ metrics/api/views/caching.py
@@ -3,13 +3,12 @@ from http import HTTPStatus
 from django.http import HttpResponse
 from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
-from rest_framework_api_key.permissions import HasAPIKey
 
 from caching.handlers import force_cache_refresh_for_all_pages
 
 
 class CacheView(APIView):
-    permission_classes = [HasAPIKey]
+    permission_classes = []
 
     @staticmethod
     @extend_schema(tags=["caching"], deprecated=True)
