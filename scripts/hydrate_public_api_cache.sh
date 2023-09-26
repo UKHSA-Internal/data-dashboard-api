6e9f49ebf7d425098abe5c7350ad3412c6d61df5
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-26 18:25:44 +0100

    Remove call to `collectstatic` management command for hydrating public API cache

--- scripts/hydrate_public_api_cache.sh
+++ scripts/hydrate_public_api_cache.sh
@@ -4,20 +4,8 @@ function hydrate_public_api_cache() {
     python manage.py hydrate_public_api_cache
 }
 
-function collect_static_files() {
-    # This script is intended to be ran within a job
-    # Which means the entrypoint of the container is overridden with a call to this script
-    # As such, the static files need to be collected within the job process
-    # Before the `hydrate_public_api_cache` can be called
-    python manage.py collectstatic --noinput
-}
-
 function run_script() {
     echo "Running script to fill cache for public API"
-
-    echo "Collecting static files"
-    collect_static_files
-    echo "Finished collecting static files"
 
     echo "Running command to fill cache for public API"
     hydrate_public_api_cache
