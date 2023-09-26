69d78b236928db84dab2a17fc6b0edd25eef5aab
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-26 18:26:01 +0100

    Remove extra log statement

--- scripts/hydrate_public_api_cache.sh
+++ scripts/hydrate_public_api_cache.sh
@@ -6,8 +6,6 @@ function hydrate_public_api_cache() {
 
 function run_script() {
     echo "Running script to fill cache for public API"
-
-    echo "Running command to fill cache for public API"
     hydrate_public_api_cache
     echo "Completed filling cache for public API"
 }
