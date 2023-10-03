31e10aac2d06eaa2e1faeacd5b00239175a3c49c
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-03 17:28:27 +0100

    Add docstring to `_get_frontend_base_url()`

--- caching/frontend/handlers.py
+++ caching/frontend/handlers.py
@@ -5,6 +5,15 @@ from caching.public_api.crawler import get_cdn_auth_key
 
 
 def _get_frontend_base_url() -> str:
+    """Returns the value of the "FRONTEND_URL" environment variable
+
+    Returns:
+        The value of the "FRONTEND_URL"
+
+    Raises:
+        `KeyError`: If the "FRONTEND_URL" has not been set
+
+    """
     try:
         return os.environ["FRONTEND_URL"]
     except KeyError as error:
