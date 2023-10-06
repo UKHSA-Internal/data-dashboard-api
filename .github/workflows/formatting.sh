47b93cc529fae943ce5b60ba8a3fcbc569517f3b
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-06 16:18:29 +0100

    Add `ruff` github action to `quality-checks` CI build

--- .github/workflows/formatting.sh
+++ .github/workflows/formatting.sh
@@ -2,15 +2,14 @@
 # Exit immediately on non-zero status
 set -xe
 
-# Run ruff and black (in that order) to check that there are no changes
-ruff . --preview fix
+# Run black to check that there are no changes
 black .
 
 # Only verify for .py files
 changed_files=$(git diff --name-only | grep "\.py" || true)
 
 if [ -n "${changed_files}"  ]; then
-    echo "Some files appear to be un-ruffed or unblacked. Please rectify by running ruff & then black over them."
+    echo "Some files appear to be unblacked. Please rectify by running black over them."
     echo "${changed_files}"
     exit 1
 else
