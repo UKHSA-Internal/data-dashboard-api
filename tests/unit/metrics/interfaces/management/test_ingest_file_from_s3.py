0e30a06f3e29f4ca841ca05d5f40809053b79c67
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-09 20:47:02 +0100

    Implement `ingest_file_from_s3` management command

---
+++ tests/unit/metrics/interfaces/management/test_ingest_file_from_s3.py
@@ -0,0 +1,25 @@
+from unittest import mock
+
+from django.core.management import call_command
+
+MODULE_PATH = "metrics.interfaces.management.commands.ingest_file_from_s3"
+
+
+class TestUploadFilesFromS3:
+    @mock.patch(f"{MODULE_PATH}.download_file_ingest_and_teardown")
+    def test_delegates_call(self, spy_download_file_ingest_and_teardown: mock.MagicMock):
+        """
+        Given an instance of the app
+        When a call is made to the
+            custom management command `ingest_file_from_s3`
+        Then the call is delegated to the
+            `download_file_ingest_and_teardown()` function
+        """
+        # Given
+        file_key = "abc.json"
+
+        # When
+        call_command("ingest_file_from_s3", file_key=file_key)
+
+        # Then
+        spy_download_file_ingest_and_teardown.assert_called_once_with(key=file_key)
