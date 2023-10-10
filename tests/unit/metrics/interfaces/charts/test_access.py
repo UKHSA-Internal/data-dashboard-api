d1a2aa36c25cac37e75c95bd3b7ff057972739cb
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-10 19:47:35 +0100

    Move long-running unit tests (100+ ms) to integration tests directory

--- tests/unit/metrics/interfaces/charts/test_access.py
+++ tests/unit/metrics/interfaces/charts/test_access.py
@@ -676,34 +676,3 @@ class TestMiscMethods:
         # When / Then
         with pytest.raises(InvalidFileFormatError):
             charts_interface.encode_figure(figure=figure)
-
-    @mock.patch("scour.scour.scourString")
-    def test_scour_is_called(self, mocked_scourstring: mock.MagicMock):
-        """
-        Given a Plotly Figure
-        When `create_optimized_svg()` is called from an instance of the `ChartsInterface`
-        And the format is svg
-        Then `scour.scourString` from the scour library is called
-        """
-
-        # Given
-        file_format = "svg"
-        mocked_chart_plot_params = mock.Mock(chart_type=ChartTypes.simple_line.value)
-        mocked_chart_plots = mock.Mock(
-            file_format=file_format,
-            plots=[mocked_chart_plot_params],
-        )
-
-        charts_interface = ChartsInterface(
-            chart_plots=mocked_chart_plots,
-            core_time_series_manager=mock.Mock(),
-        )
-
-        figure = plotly.graph_objs.Figure()
-
-        # When
-        figure_image = charts_interface.create_optimized_svg(figure=figure)
-
-        # Then
-        mocked_scourstring.assert_called_once()
-        assert mocked_scourstring.return_value == figure_image
