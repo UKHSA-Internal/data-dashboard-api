e6bb7700b7019ce732b12d93f14f2229e08596b8
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-11 18:29:04 +0100

    Add `BadgeColours` enum conforming to GDS tags

---
+++ cms/whats_new/colour_scheme.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class BadgeColours(models.Choices):
+    GREY = "GREY"
+    GREEN = "GREEN"
+    TURQUOISE = "TURQUOISE"
+    BLUE = "BLUE"
+    PURPLE = "PURPLE"
+    PINK = "PINK"
+    RED = "RED"
+    ORANGE = "ORANGE"
+    YELLOW = "YELLOW"
