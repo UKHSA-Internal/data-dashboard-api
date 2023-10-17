fecd0db004d59b7aeb48c0161cd642b0c8ad9a0f
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-17 20:04:38 +0100

    Remove unused `get_axis_name()`

--- metrics/domain/utils.py
+++ metrics/domain/utils.py
@@ -72,22 +72,5 @@ class ChartAxisFields(Enum):
             return cls.get_default_y_axis().value
 
 
-def get_axis_name(field_name: str):
-    """Convert the given field_name into the "display" version
-    If no conversion is required then just return the supplied argument unaltered
-
-    Args:
-        field_name: The fieldname to convert to the display version if required
-
-    Returns:
-        The converted or unaltered fieldname
-    """
-    return (
-        ChartAxisFields(field_name).name
-        if field_name in ChartAxisFields.values()
-        else field_name
-    )
-
-
 DEFAULT_X_AXIS = ChartAxisFields.get_default_x_axis().name
 DEFAULT_Y_AXIS = ChartAxisFields.get_default_y_axis().name
--- tests/unit/metrics/domain/test_utils.py
+++ tests/unit/metrics/domain/test_utils.py
@@ -2,32 +2,11 @@ import pytest
 
 from metrics.domain.utils import (
     ChartAxisFields,
-    _check_for_substring_match,
-    get_axis_name,
+    _check_for_substring_match, ChartTypes
 )
 
 
-class TestGetAxisName:
-    test_values = [(enums.value, enums.name) for enums in ChartAxisFields]
-    test_values.append(("not_a_field", "not_a_field"))
-
-    @pytest.mark.parametrize("field_name, expected_result", test_values)
-    def test_get_axis_field_name(self, field_name: str, expected_result: str):
-        """
-        Given a field name (eg. stratum__name)
-        When `get_axis_field_name()` is called
-        Then the expected output will be returned
-        """
-
-        # Given
-        input_field_name: str = field_name
-
-        # When
-        actual_result: str = get_axis_name(field_name=input_field_name)
-
-        # Then
-        assert actual_result == expected_result
-
+class TestChartAxisFields:
     @pytest.mark.parametrize(
         "valid_name",
         [
