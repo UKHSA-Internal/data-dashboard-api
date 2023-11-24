b0df302e392be7a5a3b9e99b4bb1ff235323aa04
A-Ashiq <afaan.ashiq@nate.tech>
2023-11-24 17:57:34 +0000

    Add `GeographyTypeFactory` with convenience post factory generation for creating associated `Geography` objects on the fly

---
+++ tests/factories/metrics/geography_type.py
@@ -0,0 +1,23 @@
+import factory
+
+from metrics.data.models.core_models import Geography, GeographyType
+
+
+class GeographyTypeFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for creating `GeographyType` instances for tests
+    """
+
+    class Meta:
+        model = GeographyType
+
+    @factory.post_generation
+    def with_geographies(self, create, extracted, geography_names, **kwargs):
+        if create:
+            geographies = [
+                Geography.objects.create(name=geography_name)
+                for geography_name in geography_names
+            ]
+            for geography in geographies:
+                geography.geography_type = self
+                geography.save(update_fields=["geography_type"])
