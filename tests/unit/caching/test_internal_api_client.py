43fb8b71ed4ba3a2a23597d7b07b0bb3a706e715
A-Ashiq <afaan.ashiq@nate.tech>
2023-11-29 16:30:59 +0000

    Add geographies list and detail endpoint calls to `InternalAPIClient`

--- tests/unit/caching/test_internal_api_client.py
+++ tests/unit/caching/test_internal_api_client.py
@@ -23,6 +23,7 @@ class TestInternalAPIClient:
                 ("charts_endpoint_path", "/api/charts/v3/"),
                 ("tables_endpoint_path", "/api/tables/v4/"),
                 ("downloads_endpoint_path", "/api/downloads/v2/"),
+                ("geographies_endpoint_path", "/api/geographies/v1/types/"),
             ]
         ),
     )
@@ -301,6 +302,52 @@ class TestInternalAPIClient:
         mocked_client.post.assert_called_once_with(
             path=internal_api_client.downloads_endpoint_path,
             data=mocked_request_data,
+            headers=internal_api_client.build_headers(),
+            format="json",
+        )
+
+    def test_hit_geographies_list_endpoint_delegates_call_correctly(self):
+        """
+        Given a client and mocked request data
+        When `hit_geographies_list_endpoint()` is called
+            from an instance of the `InternalAPIClient`
+        Then the call is delegated to the `client` object
+        """
+        # Given
+        mocked_client = mock.Mock()
+        internal_api_client = InternalAPIClient(client=mocked_client)
+
+        # When
+        response = internal_api_client.hit_geographies_list_endpoint()
+
+        # Then
+        assert response == internal_api_client._client.get.return_value
+        mocked_client.get.assert_called_once_with(
+            path=internal_api_client.geographies_endpoint_path,
+            headers=internal_api_client.build_headers(),
+            format="json",
+        )
+
+    def test_hit_geographies_detail_endpoint_delegates_call_correctly(self):
+        """
+        Given a client and mocked request data
+        When `hit_geographies_detail_endpoint()` is called
+            from an instance of the `InternalAPIClient`
+        Then the call is delegated to the `client` object
+        """
+        # Given
+        mocked_client = mock.Mock()
+        internal_api_client = InternalAPIClient(client=mocked_client)
+        fake_id = 123
+
+        # When
+        response = internal_api_client.hit_geographies_detail_endpoint(geography_type_id=fake_id)
+
+        # Then
+        assert response == internal_api_client._client.get.return_value
+        expected_path = f"{internal_api_client.geographies_endpoint_path}{fake_id}"
+        mocked_client.get.assert_called_once_with(
+            path=expected_path,
             headers=internal_api_client.build_headers(),
             format="json",
         )
