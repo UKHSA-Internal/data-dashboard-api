be6fb44e01d03ee14f940d3fa5094f2b7e745b37
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-14 12:03:25 +0000

    Rename functions

--- cms/metrics_documentation/migrations/0003_metricsdocumentationdatamigration.py
+++ cms/metrics_documentation/migrations/0003_metricsdocumentationdatamigration.py
@@ -1,19 +1,17 @@
-import datetime
-import json
-
 from cms.metrics_documentation.models import (
     MetricsDocumentationChildEntry,
     MetricsDocumentationParentPage,
 )
-from cms.metrics_documentation.utils import get_metrics_definitions
 from cms.home.models import HomePage
-from django.db import migrations, models
+from django.db import migrations
 from metrics.api.settings import ROOT_LEVEL_BASE_DIR
 
-from cms.metrics_documentation.data_migration.operations import create_metrics_documentation_parent_page, create_metrics_documentation_child_entries
+from cms.metrics_documentation.data_migration.operations import (
+    create_metrics_documentation_parent_page,
+    create_metrics_documentation_child_entries,
+)
+
 FILE_PATH = f"{ROOT_LEVEL_BASE_DIR}/cms/dashboard/templates/cms_starting_pages/"
-
-
 
 
 class RootPageDoesNotExist:
@@ -22,7 +20,7 @@ class RootPageDoesNotExist:
         super().__init__(message)
 
 
-def forward_migration_parent_entry(apps, schema_editor) -> None:
+def forward_migration_metrics_documentation_parent_page(apps, schema_editor) -> None:
     """Creates parent page for data migration if one doesn't exist.
 
     Args:
@@ -34,7 +32,6 @@ def forward_migration_parent_entry(apps, schema_editor) -> None:
     """
     try:
         create_metrics_documentation_parent_page()
-        #HomePage.objects.get(slug="ukhsa_dashboard_root")
     except HomePage.DoesNotExist:
         print("no home page to act as root")
         return
@@ -43,17 +40,7 @@ def forward_migration_parent_entry(apps, schema_editor) -> None:
     # call build metrics_documentation_parent_entry()
 
 
-def forward_metric_documentation_parent(apps, schema_editor) -> None:
-    """moved to operations
-    """
-    # move this out to new create function - do this for child as well
-    # this function, try/except for calling the new function
-    # give an error
-
-        # if root_page == none raise error. custom error.
-
-
-def forward_metric_documentation_child_entries(apps, schema_editor) -> None:
+def forward_migration_metrics_documentation_child_entries(apps, schema_editor) -> None:
     """Creates child entries for data migration.
 
     Args:
@@ -65,13 +52,12 @@ def forward_metric_documentation_child_entries(apps, schema_editor) -> None:
     """
     try:
         create_metrics_documentation_child_entries()
-        #HomePage.objects.get(slug="ukhsa_dashboard_root")
     except MetricsDocumentationParentPage.DoesNotExist:
         print("no parent page to act as root")
         return
 
 
-def reverse_metric_documentation_child_entries(apps, schema_editor) -> None:
+def reverse_migration_metrics_documentation_child_entries(apps, schema_editor) -> None:
     """Reverses the child entries migration by removing all entries.
 
     Args:
@@ -89,10 +75,10 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(
-            code=forward_migration_parent_entry,
+            code=forward_migration_metrics_documentation_parent_page,
         ),
         migrations.RunPython(
-            code=forward_metric_documentation_child_entries,
-            reverse_code=reverse_metric_documentation_child_entries,
+            code=forward_migration_metrics_documentation_child_entries,
+            reverse_code=reverse_migration_metrics_documentation_child_entries,
         ),
     ]
