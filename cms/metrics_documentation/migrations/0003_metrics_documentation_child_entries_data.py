dd3df5f7dc8343ac9e29f3092ce8a629842117f8
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-14 19:07:39 +0000

    Add dummy reverse callable for migration

--- cms/metrics_documentation/migrations/0003_metrics_documentation_child_entries_data.py
+++ cms/metrics_documentation/migrations/0003_metrics_documentation_child_entries_data.py
@@ -3,12 +3,17 @@ from django.db import migrations
 from cms.metrics_documentation.data_migration import handlers
 
 
+def pass_over(*args, **kwargs):
+    ...
+
+
 class Migration(migrations.Migration):
     dependencies = [("metrics_documentation", "0002_metricsdocumentationchildentry")]
 
     operations = [
         migrations.RunPython(
             code=handlers.forward_migration_metrics_documentation_parent_page,
+            reverse_code=pass_over,
         ),
         migrations.RunPython(
             code=handlers.forward_migration_metrics_documentation_child_entries,
