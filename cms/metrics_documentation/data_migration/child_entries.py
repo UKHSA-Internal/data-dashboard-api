3e37002324cc41eef75015a3d84a56e49d1423ad
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-18 16:14:20 +0000

    Call with kwarg

--- cms/metrics_documentation/data_migration/child_entries.py
+++ cms/metrics_documentation/data_migration/child_entries.py
@@ -75,6 +75,6 @@ def get_metrics_definitions() -> list[dict[str | list[dict]]]:
     """
     work_sheet = _load_source_data_as_worksheet()
     return [
-        build_entry_from_row_data(row)
+        build_entry_from_row_data(row=row)
         for row in work_sheet.iter_rows(min_row=2, values_only=True)
     ]
