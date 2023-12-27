d1b64f939d7e082d13043e158e66a3e092b5f2fa
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-27 16:59:05 +0000

    Implement `Theme`, `SubTheme`, & `Topic` on `MetricFactory`

--- tests/fakes/factories/metrics/metric_factory.py
+++ tests/fakes/factories/metrics/metric_factory.py
@@ -2,6 +2,8 @@ import factory
 
 from tests.fakes.models.metrics.metric import FakeMetric
 from tests.fakes.models.metrics.metric_group import FakeMetricGroup
+from tests.fakes.models.metrics.sub_theme import FakeSubTheme
+from tests.fakes.models.metrics.theme import FakeTheme
 from tests.fakes.models.metrics.topic import FakeTopic
 
 
@@ -19,7 +21,11 @@ class FakeMetricFactory(factory.Factory):
         metric_name: str = "COVID-19_deaths_ONSByDay",
         metric_group_name: str = "deaths",
         topic_name: str = "COVID-19",
+        theme_name: str = "infectious_disease",
+        sub_theme_name: str = "respiratory",
     ) -> FakeMetric:
-        topic = FakeTopic(name=topic_name)
+        theme = FakeTheme(name=theme_name)
+        sub_theme = FakeSubTheme(name=sub_theme_name, theme=theme)
+        topic = FakeTopic(name=topic_name, sub_theme=sub_theme)
         metric_group = FakeMetricGroup(name=metric_group_name, topic=topic)
         return cls.build(name=metric_name, metric_group=metric_group, topic=topic)
