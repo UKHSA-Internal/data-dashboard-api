85ca14e17ab65e382acfc0063aa9f70ba8893df1
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-27 16:59:55 +0000

    Update test write data to csv

--- tests/unit/metrics/domain/exports/test_csv.py
+++ tests/unit/metrics/domain/exports/test_csv.py
@@ -1,10 +1,12 @@
+import datetime
 import io
 
-from metrics.domain.exports.csv import write_data_to_csv
-from tests.fakes.factories.metrics.api_time_series_factory import (
-    FakeAPITimeSeriesFactory,
+from metrics.domain.exports.csv import FIELDS, write_data_to_csv
+from tests.fakes.factories.metrics.core_time_series_factory import (
+    FakeCoreTimeSeriesFactory,
 )
-from tests.fakes.models.metrics.api_time_series import FakeAPITimeSeries
+from tests.fakes.models.metrics.core_time_series import FakeCoreTimeSeries
+from tests.fakes.models.queryset import FakeQuerySet
 
 
 def _get_line_from_stream(filestream: io.StringIO) -> list[str]:
@@ -23,6 +25,7 @@ class TestWriteDataToCSV:
         "age",
         "stratum",
         "year",
+        "epiweek",
         "date",
         "metric_value",
     ]
@@ -34,14 +37,31 @@ class TestWriteDataToCSV:
         Then the expected output will be returned
         """
         # Given
-        regular_api_time_series: FakeAPITimeSeries = (
-            FakeAPITimeSeriesFactory.build_example_covid_time_series()
+        expected_date = datetime.datetime(year=2023, month=3, day=8)
+        expected_metric_value = 2364
+        expected_topic = "COVID-19"
+        expected_metric = "COVID-19_deaths_ONSByDay"
+        expected_age = "75+"
+        expected_sex = "all"
+
+        fake_core_time_series: FakeCoreTimeSeries = (
+            FakeCoreTimeSeriesFactory.build_time_series(
+                date=datetime.datetime(year=2023, month=3, day=8),
+                metric_value=expected_metric_value,
+                topic_name=expected_topic,
+                metric_name=expected_metric,
+                age_name=expected_age,
+                sex=expected_sex,
+            )
+        )
+        queryset = FakeQuerySet(instances=[fake_core_time_series]).values_list(
+            *FIELDS.values()
         )
 
         # When
         file = io.StringIO()
         csv_file: io.StringIO = write_data_to_csv(
-            file=file, api_time_series=[regular_api_time_series]
+            file=file, core_time_series_queryset=queryset
         )
         # Go back to the beginning of stream
         csv_file.seek(0)
@@ -52,60 +72,19 @@ class TestWriteDataToCSV:
 
         # Then
         expected_csv_body = [
-            "infectious_disease",
-            "respiratory",
-            "COVID-19",
-            "Nation",
-            "England",
-            "COVID-19_deaths_ONSByDay",
-            "all",
-            "75+",
-            "default",
-            "2023",
-            "2023-03-08",
-            "2364",
+            fake_core_time_series.metric.topic.sub_theme.theme.name,
+            fake_core_time_series.metric.topic.sub_theme.name,
+            expected_topic,
+            fake_core_time_series.geography.geography_type.name,
+            fake_core_time_series.geography.name,
+            expected_metric,
+            expected_sex,
+            expected_age,
+            fake_core_time_series.stratum.name,
+            str(fake_core_time_series.year),
+            str(fake_core_time_series.epiweek),
+            str(expected_date),
+            str(expected_metric_value),
         ]
-        assert csv_header == self.expected_csv_header
-        assert csv_body == expected_csv_body
-
-    def test_fields_missing(self):
-        """
-        Given a file and a QuerySet with some expected fields missing. eg geography_type,
-        When `write_data_to_csv()` is called
-        Then the expected output will be returned
-        """
-
-        # Given
-        api_time_series_additional: FakeAPITimeSeries = (
-            FakeAPITimeSeriesFactory.build_example_api_time_series_fields_missing()
-        )
-        # When
-        file = io.StringIO()
-        csv_file: io.StringIO = write_data_to_csv(
-            file=file, api_time_series=[api_time_series_additional]
-        )
-        # Go back to the beginning of stream
-        csv_file.seek(0)
-
-        # Output should consist of two rows, a heading and the data itself
-        csv_header = _get_line_from_stream(csv_file)
-        csv_body = _get_line_from_stream(csv_file)
-
-        # Then
-        expected_csv_body = [
-            "infectious_disease",
-            "respiratory",
-            "COVID-19",
-            "",  # geography type
-            "",  # geography
-            "COVID-19_deaths_ONSByDay",
-            "",  # sex
-            "",  # age
-            "default",  # stratum
-            "",  # year
-            "2023-03-08",
-            "2364",
-        ]
-
         assert csv_header == self.expected_csv_header
         assert csv_body == expected_csv_body
