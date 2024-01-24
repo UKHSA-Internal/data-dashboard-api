8a0072889e7fdc6e843e991ffcf634199d722b27
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-24 18:15:49 +0000

    Add test for 400 bad request on headlines endpoint

--- tests/integration/metrics/api/views/test_headlines.py
+++ tests/integration/metrics/api/views/test_headlines.py
@@ -4,7 +4,7 @@ import pytest
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 
-from metrics.data.models.core_models import CoreHeadline
+from metrics.data.models.core_models import CoreHeadline, Topic
 
 
 class TestHeadlinesView:
@@ -37,3 +37,34 @@ class TestHeadlinesView:
         # Then
         assert response.status_code == HTTPStatus.OK
         assert response.data == {"value": core_headline_example.metric_value}
+
+    @pytest.mark.django_db
+    def test_get_returns_error_for_invalid_request(
+        self, core_headline_example: CoreHeadline
+    ):
+        """
+        Given the name of a `metric` as well as an incorrect `topic`
+        When the `GET /api/headlines/v3/` endpoint is hit
+        Then an HTTP 400 BAD REQUEST response is returned
+        """
+        # Given
+        client = APIClient()
+
+        # The `Topic` record needs to be available
+        # Or else the serializer will invalidate the field choice first
+        incorrect_topic_name = "Influenza"
+        Topic.objects.create(name=incorrect_topic_name)
+
+        # When
+        response: Response = client.get(
+            path=self.path,
+            data={
+                "topic": incorrect_topic_name,
+                "metric": core_headline_example.metric.name,
+                "geography": core_headline_example.geography.name,
+                "geography_type": core_headline_example.geography.geography_type.name,
+            },
+        )
+
+        # Then
+        assert response.status_code == HTTPStatus.BAD_REQUEST
