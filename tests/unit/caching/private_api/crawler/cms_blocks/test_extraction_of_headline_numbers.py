83707a1852262f6f4f6c526bb19982b5ec0f3732
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-26 16:16:40 +0000

    Move existing tests to `test_extraction_of_headline_numbers:TestCMSBlockParseExtractionOfHeadlineNumbers`

--- tests/unit/caching/private_api/crawler/cms_blocks/test_extraction_of_headline_numbers.py
+++ tests/unit/caching/private_api/crawler/cms_blocks/test_extraction_of_headline_numbers.py
@@ -1,7 +1,7 @@
 from caching.private_api.crawler.cms_blocks import CMSBlockParser
 
 
-class TestCMSBlockParserGetAllHeadlineBlocksFromSection:
+class TestCMSBlockParseExtractionOfHeadlineNumbers:
     def test_get_all_headline_blocks_from_section(
         self, example_section_with_headline_chart_and_text_cards
     ):
@@ -39,3 +39,47 @@ class TestCMSBlockParserGetAllHeadlineBlocksFromSection:
             # The other chart card contains no headline blocks
         ]
         assert headline_blocks == expected_headline_blocks
+
+    def test_get_headline_blocks_from_chart_blocks(self, example_chart_blocks):
+        """
+        Given a list of example chart blocks
+        When `get_headline_blocks_from_chart_blocks()` is called
+            from the `CMSBlockParser` class
+        Then the correct headline number blocks are extracted and returned
+        """
+        # Given
+        chart_blocks = example_chart_blocks
+
+        # When
+        headline_blocks = CMSBlockParser.get_headline_blocks_from_chart_blocks(
+            chart_blocks=chart_blocks
+        )
+
+        # Then
+        expected_headline_blocks = [
+            chart_blocks[0]["headline_number_columns"][0],
+            chart_blocks[0]["headline_number_columns"][1],
+            chart_blocks[1]["headline_number_columns"][0],
+            chart_blocks[1]["headline_number_columns"][1],
+        ]
+        assert headline_blocks == expected_headline_blocks
+
+    def test_get_headline_blocks_from_chart_blocks_returns_empty_list_for_no_chart_blocks(
+        self,
+    ):
+        """
+        Given an empty list of chart blocks
+        When `get_headline_blocks_from_chart_blocks()` is called
+            from the `CMSBlockParser` class
+        Then an empty list is returned
+        """
+        # Given
+        no_chart_blocks = []
+
+        # When
+        headline_number_blocks = CMSBlockParser.get_headline_blocks_from_chart_blocks(
+            chart_blocks=no_chart_blocks
+        )
+
+        # Then
+        assert headline_number_blocks == []
