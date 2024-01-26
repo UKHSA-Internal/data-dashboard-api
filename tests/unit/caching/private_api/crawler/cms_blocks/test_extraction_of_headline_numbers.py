c88377131bbc6c6b56ce0c122949e1e2af14bfb7
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-26 16:21:06 +0000

    Move existing tests to `test_extraction_of_headline_numbers:TestCMSBlockParseExtractionOfHeadlineNumbers`

--- tests/unit/caching/private_api/crawler/cms_blocks/test_extraction_of_headline_numbers.py
+++ tests/unit/caching/private_api/crawler/cms_blocks/test_extraction_of_headline_numbers.py
@@ -83,3 +83,75 @@ class TestCMSBlockParseExtractionOfHeadlineNumbers:
 
         # Then
         assert headline_number_blocks == []
+
+    def test_get_headline_blocks_from_headline_number_row_cards(
+        self, example_headline_number_row_cards
+    ):
+        """
+        Given a list of example headline number row cards
+        When `get_headline_blocks_from_headline_number_row_cards()` is called
+            from the `CMSBlockParser` class
+        Then the correct headline number blocks are extracted and returned
+        """
+        # Given
+        headline_number_row_cards = example_headline_number_row_cards
+
+        # When
+        headline_number_blocks = (
+            CMSBlockParser.get_headline_blocks_from_headline_number_row_cards(
+                headline_numbers_row_cards=headline_number_row_cards
+            )
+        )
+
+        # Then
+        expected_headline_number_blocks = [
+            headline_number_row_cards[0]["value"]["columns"][0]["value"]["rows"][0],
+            headline_number_row_cards[0]["value"]["columns"][0]["value"]["rows"][1],
+            headline_number_row_cards[0]["value"]["columns"][1]["value"]["rows"][0],
+        ]
+        assert headline_number_blocks == expected_headline_number_blocks
+
+    def test_get_headline_blocks_from_headline_number_row_cards_returns_empty_list_for_no_cards(
+        self,
+    ):
+        """
+        Given an empty list of headline number row cards
+        When `get_headline_blocks_from_headline_number_row_cards()` is called
+            from the `CMSBlockParser` class
+        Then an empty list is returned
+        """
+        # Given
+        no_headline_number_row_cards = []
+
+        # When
+        headline_number_blocks = (
+            CMSBlockParser.get_headline_blocks_from_headline_number_row_cards(
+                headline_numbers_row_cards=no_headline_number_row_cards
+            )
+        )
+
+        # Then
+        assert headline_number_blocks == []
+
+    def test_get_headline_blocks_from_headline_number_row_cards_returns_empty_list_for_incorrect_cards(
+        self,
+    ):
+        """
+        Given a list of incorrect headline number row cards
+        When `get_headline_blocks_from_headline_number_row_cards()` is called
+            from the `CMSBlockParser` class
+        Then an empty list is returned
+        """
+        # Given
+        incorrect_headline_number_row_cards = [
+            {"type": "headline_numbers_row_card", "value": {"incorrect_key": []}}
+        ]
+        # When
+        headline_number_blocks = (
+            CMSBlockParser.get_headline_blocks_from_headline_number_row_cards(
+                headline_numbers_row_cards=incorrect_headline_number_row_cards
+            )
+        )
+
+        # Then
+        assert headline_number_blocks == []
