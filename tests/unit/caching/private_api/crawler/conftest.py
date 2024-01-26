d3753d09174602ca0c1301e0c92eccf8ee9c82f3
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-26 16:11:59 +0000

    Move example section out as a test fixture

--- tests/unit/caching/private_api/crawler/conftest.py
+++ tests/unit/caching/private_api/crawler/conftest.py
@@ -221,6 +221,197 @@ def example_section_with_headline_card_and_chart_card():
 
 
 @pytest.fixture
+def example_section_with_headline_chart_and_text_cards():
+    return {
+        "type": "section",
+        "value": {
+            "heading": "COVID-19",
+            "content": [
+                {
+                    "type": "text_card",
+                    "value": {
+                        "body": '<p data-block-key="6du8j">Summary of COVID-19 data. For more detailed data, go to the <a id="5" linktype="page">COVID-19 page</a>.</p>'
+                    },
+                    "id": "6a399089-6e24-4010-a484-a12745d38872",
+                },
+                {
+                    "type": "headline_numbers_row_card",
+                    "value": {
+                        "columns": [
+                            {
+                                "type": "column",
+                                "value": {
+                                    "title": "Cases",
+                                    "rows": [
+                                        {
+                                            "type": "headline_number",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_headline_cases_7DayTotals",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "body": "Weekly",
+                                            },
+                                            "id": "eff08341-7bfa-4a3b-b013-527e7b954ce8",
+                                        },
+                                        {
+                                            "type": "trend_number",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_headline_cases_7DayChange",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "body": "7 days",
+                                                "percentage_metric": "COVID-19_headline_cases_7DayPercentChange",
+                                            },
+                                            "id": "a57a4ad5-6b52-45a6-acfd-2fe208cb5617",
+                                        },
+                                    ],
+                                },
+                                "id": "ff081d2a-e235-4bc2-9b09-220f8fe20494",
+                            },
+                            {
+                                "type": "column",
+                                "value": {
+                                    "title": "Testing",
+                                    "rows": [
+                                        {
+                                            "type": "percentage_number",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_headline_positivity_latest",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "body": "Virus tests positivity",
+                                            },
+                                            "id": "36746bcd-1dce-4e5e-81f8-60c8b9994540",
+                                        }
+                                    ],
+                                },
+                                "id": "1e3bf214-88e4-4cf4-9b78-3ad7eabb2eaa",
+                            },
+                        ]
+                    },
+                    "id": "e285caf4-ae79-4c76-bcb7-426d6e66cb8a",
+                },
+                {
+                    "type": "chart_row_card",
+                    "value": {
+                        "columns": [
+                            {
+                                "type": "chart_with_headline_and_trend_card",
+                                "value": {
+                                    "title": "Cases",
+                                    "body": "Positive COVID-19 cases reported in England (7-day rolling average)",
+                                    "x_axis": "",
+                                    "y_axis": "",
+                                    "chart": [
+                                        {
+                                            "type": "plot",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_cases_countRollingMean",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "chart_type": "line_with_shaded_section",
+                                                "date_from": None,
+                                                "date_to": None,
+                                                "label": "",
+                                                "line_colour": "",
+                                                "line_type": "",
+                                            },
+                                            "id": "b0ead98b-4102-48f6-b94e-ff7bcffe1dc4",
+                                        }
+                                    ],
+                                    "headline_number_columns": [
+                                        {
+                                            "type": "headline_number",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_headline_cases_7DayTotals",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "body": "7 days",
+                                            },
+                                            "id": "95b24a05-a015-42ed-b258-51c7ccaedbcd",
+                                        },
+                                        {
+                                            "type": "trend_number",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_headline_cases_7DayChange",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "body": "",
+                                                "percentage_metric": "COVID-19_headline_cases_7DayPercentChange",
+                                            },
+                                            "id": "8c42a86e-f675-41d0-a65a-633c20ac98e3",
+                                        },
+                                    ],
+                                },
+                                "id": "d9b86415-9734-46be-952a-56182f0c40be",
+                            },
+                            {
+                                "type": "chart_with_headline_and_trend_card",
+                                "value": {
+                                    "title": "Deaths",
+                                    "body": "Deaths with COVID-19 on the death certificate in England (7-day rolling average)",
+                                    "x_axis": "",
+                                    "y_axis": "",
+                                    "chart": [
+                                        {
+                                            "type": "plot",
+                                            "value": {
+                                                "topic": "COVID-19",
+                                                "metric": "COVID-19_deaths_ONSRollingMean",
+                                                "geography": "England",
+                                                "geography_type": "Nation",
+                                                "sex": "all",
+                                                "age": "all",
+                                                "stratum": "default",
+                                                "chart_type": "line_with_shaded_section",
+                                                "date_from": None,
+                                                "date_to": None,
+                                                "label": "",
+                                                "line_colour": "",
+                                                "line_type": "",
+                                            },
+                                            "id": "d3b521d8-a6bb-4960-9db9-864c3d362976",
+                                        }
+                                    ],
+                                    "headline_number_columns": [],
+                                },
+                                "id": "c18703a1-9b01-417f-8fd8-3e4db35865e5",
+                            },
+                        ]
+                    },
+                    "id": "a5acbd6c-f9b7-4d36-86f4-005c2d46debc",
+                },
+            ],
+        },
+        "id": "1f53f495-e8d1-45a3-bd34-5d27878c20fc",
+    }
+
+
+@pytest.fixture
 def example_chart_row_cards() -> list[dict[str, str | list[dict]]]:
     return [
         {
