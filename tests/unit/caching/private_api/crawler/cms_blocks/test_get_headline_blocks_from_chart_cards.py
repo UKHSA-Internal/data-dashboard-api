95efa603bb021efae78c58f6dad437c786186475
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-26 16:14:44 +0000

    Move example chart blocks out as a test fixture

--- tests/unit/caching/private_api/crawler/cms_blocks/test_get_headline_blocks_from_chart_cards.py
+++ tests/unit/caching/private_api/crawler/cms_blocks/test_get_headline_blocks_from_chart_cards.py
@@ -1,127 +1,8 @@
 from caching.private_api.crawler.cms_blocks import CMSBlockParser
 
-EXAMPLE_CHART_CARDS = [
-    {
-        "title": "Cases",
-        "body": "Positive COVID-19 cases reported in England (7-day rolling average)",
-        "x_axis": "",
-        "y_axis": "",
-        "chart": [
-            {
-                "type": "plot",
-                "value": {
-                    "topic": "COVID-19",
-                    "metric": "COVID-19_cases_countRollingMean",
-                    "geography": "England",
-                    "geography_type": "Nation",
-                    "sex": "all",
-                    "age": "all",
-                    "stratum": "default",
-                    "chart_type": "line_with_shaded_section",
-                    "date_from": None,
-                    "date_to": None,
-                    "label": "",
-                    "line_colour": "",
-                    "line_type": "",
-                },
-                "id": "b0ead98b-4102-48f6-b94e-ff7bcffe1dc4",
-            }
-        ],
-        "headline_number_columns": [
-            {
-                "type": "headline_number",
-                "value": {
-                    "topic": "COVID-19",
-                    "metric": "COVID-19_headline_cases_7DayTotals",
-                    "geography": "England",
-                    "geography_type": "Nation",
-                    "sex": "all",
-                    "age": "all",
-                    "stratum": "default",
-                    "body": "7 days",
-                },
-                "id": "95b24a05-a015-42ed-b258-51c7ccaedbcd",
-            },
-            {
-                "type": "trend_number",
-                "value": {
-                    "topic": "COVID-19",
-                    "metric": "COVID-19_headline_cases_7DayChange",
-                    "geography": "England",
-                    "geography_type": "Nation",
-                    "sex": "all",
-                    "age": "all",
-                    "stratum": "default",
-                    "body": "",
-                    "percentage_metric": "COVID-19_headline_cases_7DayPercentChange",
-                },
-                "id": "8c42a86e-f675-41d0-a65a-633c20ac98e3",
-            },
-        ],
-    },
-    {
-        "title": "Deaths",
-        "body": "Deaths with COVID-19 on the death certificate in England (7-day rolling average)",
-        "x_axis": "",
-        "y_axis": "",
-        "chart": [
-            {
-                "type": "plot",
-                "value": {
-                    "topic": "COVID-19",
-                    "metric": "COVID-19_deaths_ONSRollingMean",
-                    "geography": "England",
-                    "geography_type": "Nation",
-                    "sex": "all",
-                    "age": "all",
-                    "stratum": "default",
-                    "chart_type": "line_with_shaded_section",
-                    "date_from": None,
-                    "date_to": None,
-                    "label": "",
-                    "line_colour": "",
-                    "line_type": "",
-                },
-                "id": "d3b521d8-a6bb-4960-9db9-864c3d362976",
-            }
-        ],
-        "headline_number_columns": [
-            {
-                "type": "headline_number",
-                "value": {
-                    "topic": "COVID-19",
-                    "metric": "COVID-19_headline_ONSdeaths_7DayTotals",
-                    "geography": "England",
-                    "geography_type": "Nation",
-                    "sex": "all",
-                    "age": "all",
-                    "stratum": "default",
-                    "body": "7 days",
-                },
-                "id": "10c92d4c-bdb1-4bcc-a8a5-d0063dcee095",
-            },
-            {
-                "type": "trend_number",
-                "value": {
-                    "topic": "COVID-19",
-                    "metric": "COVID-19_headline_ONSdeaths_7DayChange",
-                    "geography": "England",
-                    "geography_type": "Nation",
-                    "sex": "all",
-                    "age": "all",
-                    "stratum": "default",
-                    "body": "",
-                    "percentage_metric": "COVID-19_headline_ONSdeaths_7DayPercentChange",
-                },
-                "id": "41ce6c59-99fe-486a-8225-341a306cc395",
-            },
-        ],
-    },
-]
-
 
 class TestGetHeadlineBlocksFromChartCards:
-    def test_returns_correct_headline_blocks(self):
+    def test_get_headline_blocks_from_chart_blocks(self, example_chart_blocks):
         """
         Given a list of example chart blocks
         When `get_headline_blocks_from_chart_blocks()` is called
@@ -129,7 +10,7 @@ class TestGetHeadlineBlocksFromChartCards:
         Then the correct headline number blocks are extracted and returned
         """
         # Given
-        chart_blocks = EXAMPLE_CHART_CARDS
+        chart_blocks = example_chart_blocks
 
         # When
         headline_blocks = CMSBlockParser.get_headline_blocks_from_chart_blocks(
@@ -145,7 +26,7 @@ class TestGetHeadlineBlocksFromChartCards:
         ]
         assert headline_blocks == expected_headline_blocks
 
-    def test_returns_empty_list_for_no_chart_blocks(self):
+    def test_get_headline_blocks_from_chart_blocks_returns_empty_list_for_no_chart_blocks(self):
         """
         Given an empty list of chart blocks
         When `get_headline_blocks_from_chart_blocks()` is called
