75e40ca13d0277e5b799caaa7c8c9dee9d57ad8a
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-29 19:34:12 +0000

    Add `GeographiesSerializer`

--- metrics/api/serializers/geographies.py
+++ metrics/api/serializers/geographies.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
+
 from rest_framework import serializers
 
-from metrics.data.models.core_models import Geography, GeographyType
+from metrics.data.managers.core_models.time_series import CoreTimeSeriesQuerySet
+from metrics.data.models.core_models import CoreTimeSeries, Geography, GeographyType
 
 
 class GeographySerializer(serializers.ModelSerializer):
@@ -21,3 +24,87 @@ class GeographyTypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = GeographyType
         fields = ["id", "name"]
+
+
+GEOGRAPHY_TYPE_RESULT = dict[str, list[dict[str, str]]]
+
+
+class GeographiesSerializer(serializers.Serializer):
+    topic = serializers.CharField()
+
+    @property
+    def core_time_series_manager(self):
+        """
+        Fetch the core_time_series_manager from the context if available.
+        If not get the Manager which has been declared on the `CoreTimeSeries` model.
+        """
+        return self.context.get("core_time_series_manager", CoreTimeSeries.objects)
+
+    def get_results(self) -> list[GEOGRAPHY_TYPE_RESULT]:
+        """Finds available geography types as list of dicts, where each dict represents a geography_type
+
+        Examples:
+          >>>> [
+                {"geography_type": "Nation", "geographies": [{"name": "England"}]},
+                {
+                    "geography_type": "Lower Tier Local Authority",
+                    "geographies": [{"name": "Birmingham"}, {"name": "Leeds"}],
+                },
+            ]
+
+        Returns:
+            List of dicts representing each geography_type with
+            aggregated geographies
+
+        """
+        topic: str = self.validated_data["topic"]
+        queryset: CoreTimeSeriesQuerySet = (
+            self.core_time_series_manager.get_available_geographies(topic=topic)
+        )
+        return _serialize_queryset(queryset=queryset)
+
+
+def _serialize_queryset(
+    queryset: CoreTimeSeriesQuerySet,
+) -> list[GEOGRAPHY_TYPE_RESULT]:
+    """Converts the `queryset` to a list of dicts, where each dict represents a geography_type
+
+    Args:
+        queryset: The resulting `CoreTimeSeriesQuerySet`
+            which has been sliced for the available geographies
+            and their corresponding geography types
+
+    Examples:
+        A given `queryset` of:
+          >>> <CoreTimeSeriesQuerySet [
+                Row(geography__name='England', geography__geography_type__name='Nation')
+                Row(geography__name='Birmingham', geography__geography_type__name='Lower Tier Local Authority'),
+                Row(geography__name='Leeds', geography__geography_type__name='Lower Tier Local Authority'),
+                ]
+            >
+
+        Would return:
+          >>>> [
+                {"geography_type": "Nation", "geographies": [{"name": "England"}]},
+                {
+                    "geography_type": "Lower Tier Local Authority",
+                    "geographies": [{"name": "Birmingham"}, {"name": "Leeds"}],
+                },
+            ]
+
+    Returns:
+        List of dicts representing each geography_type with
+        aggregated geographies.
+
+    """
+    merged_geographies = defaultdict(list)
+
+    for geography_combination in queryset:
+        geography: str = geography_combination.geography__name
+        geography_type: str = geography_combination.geography__geography_type__name
+        merged_geographies[geography_type].append({"name": geography})
+
+    return [
+        {"geography_type": geography_type, "geographies": geographies}
+        for geography_type, geographies in merged_geographies.items()
+    ]
--- tests/unit/metrics/api/serializers/test_geographies.py
+++ tests/unit/metrics/api/serializers/test_geographies.py
@@ -1,11 +1,20 @@
+import datetime
+from unittest import mock
+
 from metrics.api.serializers.geographies import (
+    GeographiesSerializer,
     GeographySerializer,
     GeographyTypesSerializer,
+    _serialize_queryset,
+)
+from tests.fakes.factories.metrics.core_time_series_factory import (
+    FakeCoreTimeSeriesFactory,
 )
 from tests.fakes.factories.metrics.geography_factory import FakeGeographyFactory
 from tests.fakes.factories.metrics.geography_type_factory import (
     FakeGeographyTypeFactory,
 )
+from tests.fakes.managers.time_series_manager import FakeCoreTimeSeriesManager
 
 
 class TestGeographySerializer:
@@ -44,3 +53,114 @@ class TestGeographyTypesSerializer:
         serialized_data = serializer.data
         assert serialized_data["name"] == fake_geography_type.name
         assert serialized_data["id"] == fake_geography_type.id
+
+
+class TestGeographiesSerializer:
+    def test_get_results(self):
+        """
+        Given a `topic` and a number of `CoreTimeSeries` records
+        When `get_results()` is called
+            from an instance of the `GeographiesSerializer`
+        Then the returned results contain the correct geographies
+        """
+        # Given
+        ltla = "Lower Tier Local Authority"
+        nation = "Nation"
+        date_stamp = datetime.datetime(year=2024, month=1, day=1)
+
+        bexley = FakeCoreTimeSeriesFactory.build_time_series(
+            metric_name="COVID-19_cases_countRollingMean",
+            topic_name="COVID-19",
+            date=date_stamp,
+            geography_type_name=ltla,
+            geography_name="Bexley",
+        )
+        hackney = FakeCoreTimeSeriesFactory.build_time_series(
+            metric_name="COVID-19_cases_countRollingMean",
+            topic_name="COVID-19",
+            date=date_stamp,
+            geography_type_name=ltla,
+            geography_name="Hackney",
+        )
+        england = FakeCoreTimeSeriesFactory.build_time_series(
+            metric_name="COVID-19_cases_countRollingMean",
+            topic_name="COVID-19",
+            date=date_stamp,
+            geography_type_name=nation,
+            geography_name="England",
+        )
+        irrelavant_leeds_geography = FakeCoreTimeSeriesFactory.build_time_series(
+            metric_name="influenza_healthcare_ICUHDUadmissionRateByWeek",
+            topic_name="Influenza",
+            date=date_stamp,
+            geography_type_name=ltla,
+            geography_name="Leeds",
+        )
+        fake_core_time_series_manager = FakeCoreTimeSeriesManager(
+            time_series=[bexley, hackney, england, irrelavant_leeds_geography]
+        )
+        serializer = GeographiesSerializer(
+            context={"core_time_series_manager": fake_core_time_series_manager},
+            data={"topic": "COVID-19"},
+        )
+
+        # When
+        serializer.is_valid(raise_exception=True)
+        results = serializer.get_results()
+
+        # Then
+        expected_results = [
+            {
+                "geography_type": bexley.geography.geography_type.name,
+                "geographies": [
+                    {"name": bexley.geography.name},
+                    {"name": hackney.geography.name},
+                ],
+            },
+            {
+                "geography_type": england.geography.geography_type.name,
+                "geographies": [{"name": england.geography.name}],
+            },
+        ]
+        assert results == expected_results
+
+
+class TestSerializeQuerySet:
+    def test_returns_correct_result(self):
+        """
+        Given a fake `QuerySet` containing geography results
+        When `_serialize_queryset()` is called
+        Then the correct results are returned
+        """
+        # Given
+        ltla = "Lower Tier Local Authority"
+        nation = "Nation"
+        hackney = mock.Mock(
+            geography__name="Hackney", geography__geography_type__name=ltla
+        )
+        bexley = mock.Mock(
+            geography__name="Bexley", geography__geography_type__name=ltla
+        )
+        england = mock.Mock(
+            geography__name="England", geography__geography_type__name=nation
+        )
+        fake_queryset = [bexley, hackney, england]
+
+        # When
+        serialized_results = _serialize_queryset(queryset=fake_queryset)
+
+        # Then
+        expected_results = [
+            {
+                "geography_type": ltla,
+                "geographies": [
+                    {"name": bexley.geography__name},
+                    {"name": hackney.geography__name},
+                ],
+            },
+            {
+                "geography_type": nation,
+                "geographies": [{"name": england.geography__name}],
+            },
+        ]
+        assert serialized_results == expected_results
