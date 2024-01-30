38744be267c08f658d4ac568cad68249ac198ec5
A-Ashiq <afaan.ashiq@nate.tech>
2024-01-30 13:01:15 +0000

    Implement `hit_list_endpoint_for_topic` on `GeographiesAPICrawler` to fetch available geographies for given topic

--- caching/private_api/crawler/geographies_crawler.py
+++ caching/private_api/crawler/geographies_crawler.py
@@ -1,7 +1,11 @@
-from collections import OrderedDict
+import logging
 from dataclasses import dataclass
 
+from rest_framework.response import Response
+
 from caching.internal_api_client import InternalAPIClient
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -34,12 +38,8 @@ class GeographiesAPICrawler:
     def __init__(self, internal_api_client: InternalAPIClient):
         self._internal_api_client = internal_api_client
 
-    def process_geographies_api(self) -> list[GeographyTypeData]:
-        """Crawls the list and retrieve/detail endpoints associated with the geographies API
-
-        Notes:
-            This traverses the retrieve/detail endpoint for
-            every available geography type
+    def hit_list_endpoint_for_topic(self, topic: str) -> list[GeographyTypeData]:
+        """Hits the endpoint for the given `topic` to fetch the associated available geographies
 
         Returns:
             List of enriched `GeographyTypeData` models
@@ -51,52 +51,35 @@ class GeographiesAPICrawler:
                     GeographyData(name="Lower Tier Local Authority", geography_names=["Birmingham", ...]),
                 ]
 
+        Raises:
+            `KeyError`: If the response schema is not
+                in the expected structure
+
         """
-        response = self._internal_api_client.hit_geographies_list_endpoint()
-        return self.hit_detail_endpoint_for_each_geography_type(
+        response: Response = self._internal_api_client.hit_geographies_list_endpoint(
+            topic=topic
+        )
+        geography_type_data_models = self._convert_to_geography_type_models(
             response_data=response.data
         )
 
-    def hit_detail_endpoint_for_each_geography_type(
-        self, response_data: list[OrderedDict]
+        logger.info("Completed processing of geographies API for `%s` page", topic)
+        return geography_type_data_models
+
+    @staticmethod
+    def _convert_to_geography_type_models(
+        response_data: dict,
     ) -> list[GeographyTypeData]:
-        """Traverses the retrieve/detail endpoint associated with each ID from the given `response_data`
+        geography_type_data_models = []
 
-        Returns:
-            List of enriched `GeographyTypeData` models
-            which hold the geography type name
-            and its associated geography names
-            E.g.
-            >>> [
-                    GeographyData(name="Nation", geography_names=["England", ...]),
-                    GeographyData(name="Lower Tier Local Authority", geography_names=["Birmingham", ...]),
-                ]
-
-        Raises:
-            `KeyError`: If an object in the `response_data`
-                does not contain an "id" key
-
-        """
-        data = []
         for geography_type_data in response_data:
-            geography_names: list[
-                str
-            ] = self.get_associated_geography_names_for_geography_type(
-                geography_type_id=geography_type_data["id"]
+            geography_type_name: str = geography_type_data["geography_type"]
+            geographies: list[dict[str, str]] = geography_type_data["geographies"]
+
+            geography_type = GeographyTypeData(
+                name=geography_type_name,
+                geography_names=[geography["name"] for geography in geographies],
             )
 
-            geography_type_data = GeographyTypeData(
-                name=geography_type_data["name"], geography_names=geography_names
-            )
-
-        Raises:
-            `KeyError`: If no geographies are returned
-
-        """
-        response = self._internal_api_client.hit_geographies_detail_endpoint(
-            geography_type_id=geography_type_id
-        )
-        response_data = response.data
-        return [
-            geography_data["name"] for geography_data in response_data["geographies"]
-        ]
+            geography_type_data_models.append(geography_type)
+        return geography_type_data_models
