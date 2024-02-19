9cb3f0dab937289e919d85c48a1e07694b7be1bf
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-02-19 15:35:51 +0000

    update AppConfig name

--- cms/composite/apps.py
+++ cms/composite/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommonCtaConfig(AppConfig):
+class CompositeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cms.composite"
