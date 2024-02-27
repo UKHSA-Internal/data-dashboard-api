fe28d083043db898f6d7ba30986292b4c46f46cc
A-Ashiq <afaan.ashiq@nate.tech>
2024-02-27 14:56:10 +0000

    Expire db connections after 1h for ingestion workloads

---
+++ metrics/api/settings/ingestion.py
@@ -0,0 +1,13 @@
+import config
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config.POSTGRES_DB,
+        "USER": config.POSTGRES_USER,
+        "PASSWORD": config.POSTGRES_PASSWORD,
+        "HOST": config.POSTGRES_HOST,
+        "PORT": config.POSTGRES_PORT,
+        "CONN_MAX_AGE": 60 * 60 * 1,
+    }
+}
