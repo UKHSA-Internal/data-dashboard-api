0e6ffa3872c87fb2bf436cea713c2ed4a846924d
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-05 14:50:44 +0000

    Implement summaries of data for each plot on a chart

--- metrics/domain/models/plots_text.py
+++ metrics/domain/models/plots_text.py
@@ -1,5 +1,14 @@
+import datetime
+from collections.abc import Iterator
+from decimal import Decimal
+from typing import Any
+
+from metrics.domain.charts.colour_scheme import RGBAChartLineColours
+from metrics.domain.charts.line_multi_coloured.properties import ChartLineTypes
 from metrics.domain.models import PlotData, PlotParameters
 from metrics.domain.utils import ChartTypes
+
+READABLE_DATE_FORMAT = "%d %B %Y"
 
 
 class PlotsText:
@@ -44,6 +53,8 @@ class PlotsText:
 
         return text
 
+    # Introduction
+
     @classmethod
     def _introduce_no_plots(cls) -> str:
         return "There is no data being shown for this chart."
@@ -55,12 +66,224 @@ class PlotsText:
     def _introduce_multiple_plots(self) -> str:
         return f"There are {len(self.plots_data)} plots on this chart. "
 
+    # Axes
+
     def _describe_axes(self) -> str:
-        plot_parameters = self.plots_data[0].parameters
+        plot_parameters: PlotParameters = self.plots_data[0].parameters
         return (
             f"The horizontal X-axis is labelled '{plot_parameters.x_axis}'. "
             f"Whilst the vertical Y-axis is labelled '{plot_parameters.y_axis}'. "
         )
+
+    # Description for single plot chart
+
+    def _describe_only_plot(self) -> str:
+        """Builds a description of a plot. Assumes there is only 1 plot on the chart.
+
+        Notes:
+            This will include content about the colour of the plot.
+            If the plot was given a `label`,
+            then commentary will be made regarding this.
+            And if the plot is date-based i.e. has date along the x-axis
+            then the description will include content about the data itself.
+
+        Returns:
+            String representation of the plot.
+            Describing its parameters and its data if valid.
+
+        """
+        plot_data: PlotData = self.plots_data[0]
+        plot_parameters: PlotParameters = plot_data.parameters
+
+        description: str = self._describe_plot_type(plot_parameters=plot_parameters)
+
+        if plot_parameters.label:
+            description += f"The plot has a label of '{plot_parameters.label}'. "
+
+        description += self._describe_plot_parameters(plot_parameters=plot_parameters)
+
+        if self._plot_is_date_based(plot_data=plot_data):
+            description += self._describe_date_based_plot_data(plot_data=plot_data)
+
+        return description
+
+    # Description for multiple plot chart
+
+    def _describe_all_plots(self) -> str:
+        """Builds a description of each plot. Assumes there are multiple plots on the chart.
+
+        Notes:
+            This will include content about the colour of each plot.
+            If plots were given a `label`,
+            then commentary will be made regarding this.
+            And if any plots are date-based i.e. have date along the x-axis
+            then the description will include content about the data itself.
+
+        Returns:
+            String representation of each plot in the chart.
+            This will describe the parameters of each plot
+            along with the data if the plots are date-based.
+
+        """
+        description = ""
+        for index, plot_data in enumerate(self.plots_data, 1):
+            description += self._describe_next_plot_in_multiple_plot_group(
+                index=index, plot_data=plot_data
+            )
+        return description
+
+    def _describe_next_plot_in_multiple_plot_group(
+        self, index: int, plot_data: PlotData
+    ) -> str:
+        """Builds a description of a plot. Assumes there are multiple plots on the chart.
+
+        Notes:
+            This will include content about the colour of the plot.
+            If the plot was given a `label`,
+            then commentary will be made regarding this.
+            And if the plot is date-based i.e. has date along the x-axis
+            then the description will include content about the data itself.
+
+        Args:
+            index: The plot number i.e. if the current plot
+                was the 2nd plot, the index would be 2.
+            plot_data:
+                An enrich `PlotData` model containing
+                all the parameters and associated data
+                which is used to create the plot.
+
+        Returns:
+            String representation of the plot.
+            Describing its parameters and its data if valid.
+
+        """
+        plot_parameters = plot_data.parameters
+        description = f"This is plot number {index} on this chart. "
+        description += self._describe_plot_type(plot_parameters=plot_parameters)
+
+        if plot_parameters.label:
+            description += f"The plot has a label of '{plot_parameters.label}'. "
+
+        description += self._describe_plot_parameters(plot_parameters=plot_parameters)
+
+        if self._plot_is_date_based(plot_data=plot_data):
+            description += self._describe_date_based_plot_data(plot_data=plot_data)
+
+        return description
+
+    # Data description
+
+    def _describe_date_based_plot_data(
+        self, plot_data: PlotData, number_of_parts: int = 6
+    ) -> str:
+        """Builds a description of the data for the given plot. Assumes the plot uses `date` along the x-axis
+
+        Notes:
+            The data is split into N parts as dictated by the
+            `number_of_parts` parameter.
+            The commentary is created about each individual
+            section of data, relating to the overall rise/fall
+            between the start and end of each section.
+
+        Args:
+            plot_data:
+                An enrich `PlotData` model containing
+                all the parameters and associated data
+                which is used to create the plot.
+            number_of_parts: The number of sections
+                to split the data into.
+                Whereby each section then has commentary
+                created for it.
+
+        Returns:
+            String representation of the plot.
+            Describing its data and the rise/fall
+            between each section in the plot
+
+        """
+        overall_start_date: str = self._stringify_date(
+            date_obj=plot_data.x_axis_values[0]
+        )
+        overall_end_date: str = self._stringify_date(
+            date_obj=plot_data.x_axis_values[-1]
+        )
+        description = (
+            f"This plot shows data from {overall_start_date} to {overall_end_date}. "
+        )
+
+        date_parts: Iterator[list[datetime.date]] = split_into_n_parts(
+            data=plot_data.x_axis_values, n=number_of_parts
+        )
+        metric_value_parts: Iterator[list[Decimal]] = split_into_n_parts(
+            data=plot_data.y_axis_values, n=number_of_parts
+        )
+
+        for index, metric_value_part in enumerate(metric_value_parts, 1):
+            date_part: list[datetime.date] = next(date_parts)
+            try:
+                section_description: str = self._build_description_for_section_of_data(
+                    metric_value_part=metric_value_part,
+                    date_part=date_part,
+                    index=index,
+                    number_of_parts=number_of_parts,
+                )
+            except IndexError:
+                continue
+
+            description += section_description
+
+        return description
+
+    def _build_description_for_section_of_data(
+        self,
+        metric_value_part: list[Decimal],
+        date_part: list[datetime.date],
+        index: int,
+        number_of_parts: int,
+    ) -> str:
+        """Builds a description of the individual part with the metric and date values
+
+        Args:
+            metric_value_part: List of `Decimal` values
+                representing the data points within the data section
+            date_part: List of `date` objects which represent the
+                data points within the data section
+            index: The current part number
+                i.e. if the current part was the 2nd section of data,
+                then the index would be 2
+            number_of_parts: The number of sections
+                which the data is being split into.
+
+        Returns:
+            A generator of `n` items, whereby
+            each item is a sublist of the original `data`
+
+        """
+        start_value: float = self._stringify_metric_value(
+            metric_value=metric_value_part[0]
+        )
+        end_value: float = self._stringify_metric_value(
+            metric_value=metric_value_part[-1]
+        )
+        start_date: str = self._stringify_date(date_obj=date_part[0])
+        end_date: str = self._stringify_date(date_obj=date_part[-1])
+
+        if end_value > start_value:
+            part_description = f"It rose from {start_value} on {start_date} to {end_value} on {end_date}. "
+        elif end_value == start_value:
+            part_description = (
+                f"The date fluctuates between {start_value} on {start_date}, "
+                f"ending with the same value on {end_date}. "
+            )
+        else:
+            part_description = f"It fell from {start_value} on {start_date} to {end_value} on {end_date}. "
+
+        if index == number_of_parts:
+            part_description = f"And finally. {part_description}"
+
+        return part_description
+
+    # Utilities
 
     @classmethod
     def _stringify_chart_type(cls, plot_parameters: PlotData) -> str:
@@ -73,23 +296,14 @@ class PlotsText:
             case ChartTypes.line_with_shaded_section.value:
                 return "line"
 
-    def _describe_only_plot(self) -> str:
-        plot_parameters: PlotParameters = self.plots_data[0].parameters
+    def _describe_plot_type(self, plot_parameters: PlotParameters) -> str:
+        line_type: str = self._get_line_type_or_default(plot_parameters=plot_parameters)
+        line_colour: str = self._get_line_colour_or_default(
+            plot_parameters=plot_parameters
+        )
         plot_type: str = self._stringify_chart_type(plot_parameters=plot_parameters)
-        description = ""
 
-        if plot_parameters.line_type and plot_parameters.line_colour:
-            description += (
-                f"The only plot on this chart "
-                f"is a {plot_parameters.line_colour.lower()} {plot_parameters.line_type.lower()} {plot_type} plot. "
-            )
-
-        if plot_parameters.label:
-            description += f"The plot has a label of '{plot_parameters.label}'. "
-
-        description += self._describe_plot_parameters(plot_parameters=plot_parameters)
-
-        return description
+        return f"This is a {line_colour} {line_type} {plot_type} plot. "
 
     @classmethod
     def _stringify_sex(cls, plot: PlotParameters) -> str:
@@ -109,28 +323,58 @@ class PlotsText:
             f"along with the age banding of '{plot_parameters.age}' for the gender group of {sex_grouping}. "
         )
 
-    def _describe_all_plots(self) -> str:
-        description = ""
-        for index, plot_data in enumerate(self.plots_data, 1):
-            plot_parameters = plot_data.parameters
-            description += self._describe_next_plot_in_multiple_plot_group(
-                index=index, plot_parameters=plot_parameters
-            )
-        return description
+    @classmethod
+    def _get_line_colour_or_default(cls, plot_parameters: PlotParameters) -> str:
+        if plot_parameters.line_colour:
+            return plot_parameters.line_colour.lower()
 
-    def _describe_next_plot_in_multiple_plot_group(
-        self, index: int, plot_parameters: PlotParameters
-    ) -> str:
-        plot_type: str = self._stringify_chart_type(plot_parameters=plot_parameters)
+        if plot_parameters.chart_type == ChartTypes.bar.value:
+            return RGBAChartLineColours.BLUE.name.lower()
 
-        description = (
-            f"Plot number {index} on this chart "
-            f"is a {plot_parameters.line_colour.lower()} {plot_parameters.line_type.lower()} {plot_type} plot. "
-        )
+        return RGBAChartLineColours.BLACK.name.lower()
 
-        if plot_parameters.label:
-            description += f"The plot has a label of '{plot_parameters.label}'. "
+    @classmethod
+    def _get_line_type_or_default(cls, plot_parameters: PlotParameters) -> str:
+        if plot_parameters.line_type:
+            return plot_parameters.line_type.lower()
 
-        description += self._describe_plot_parameters(plot_parameters=plot_parameters)
+        return ChartLineTypes.SOLID.value.lower()
 
-        return description
+    @classmethod
+    def _stringify_date(cls, date_obj: datetime.date) -> str:
+        return date_obj.strftime(format=READABLE_DATE_FORMAT)
+
+    @classmethod
+    def _stringify_metric_value(cls, metric_value: Decimal) -> float:
+        try:
+            return float(metric_value.normalize())
+        except AttributeError:
+            return float(metric_value)
+
+    @classmethod
+    def _plot_is_date_based(cls, plot_data: PlotData) -> bool:
+        return type(plot_data.x_axis_values[0]) is datetime.date
+
+
+def split_into_n_parts(data: list[Any], n: int) -> Iterator[list[Any]]:
+    """Splits the given `data` into `n` number of parts
+
+    Args:
+        data: The overarching list to be broken up
+        n: The number of parts to split the list into
+
+    Returns:
+        A generator of `n` items, whereby
+        each item is a sublist of the original `data`
+
+    """
+    quotient, remainder = divmod(len(data), n)
+
+    return (
+        data[
+            part_number * quotient
+            + min(part_number, remainder) : (part_number + 1) * quotient
+            + min(part_number + 1, remainder)
+        ]
+        for part_number in range(n)
+    )
--- tests/unit/metrics/domain/models/test_plots_text.py
+++ tests/unit/metrics/domain/models/test_plots_text.py
@@ -63,13 +63,16 @@ class TestPlotsText:
         # Then
         expected_text_about_parameters = (
             f"There is only 1 plot on this chart. "
-            f"The horizontal X-axis is labelled 'metric'. "
-            f"Whilst the vertical Y-axis is labelled 'date'. "
-            f"The only plot on this chart is a blue solid line plot. "
+            f"The horizontal X-axis is labelled '{fake_plot_data.parameters.x_axis}'. "
+            f"Whilst the vertical Y-axis is labelled '{fake_plot_data.parameters.y_axis}'. "
+            f"This is a {fake_plot_data.parameters.line_colour.lower()} "
+            f"{fake_plot_data.parameters.line_type.lower()} line plot. "
             f"The plot has a label of '{label}'. "
-            f"This plot shows data for COVID-19. "
-            f"Specifically the metric 'COVID-19_deaths_ONSByDay' for the London area, "
-            f"along with the age banding of 'all' for the gender group of all. "
+            f"This plot shows data for {fake_plot_data.parameters.topic}. "
+            f"Specifically the metric '{fake_plot_data.parameters.metric}' "
+            f"for the {fake_plot_data.parameters.geography} area, "
+            f"along with the age banding of '{fake_plot_data.parameters.age}' "
+            f"for the gender group of {fake_plot_data.parameters.sex}. "
         )
         assert expected_text_about_parameters in text
 
@@ -77,11 +80,11 @@ class TestPlotsText:
         self, fake_plot_data: PlotData
     ):
         """
-        Given a list of 1 enriched `PlotData` model
+        Given a list of 3 enriched `PlotData` models
         When `construct_text()` is called
             from an instance of `PlotsText`
         Then the returned text declares the parameters
-            which were used for the plot
+            which were used for each plot
         """
         # Given
         second_plot_data = fake_plot_data.model_copy(deep=True)
@@ -110,20 +113,23 @@ class TestPlotsText:
             "The horizontal X-axis is labelled 'metric'. Whilst the vertical Y-axis is labelled 'date'. "
         )
         expected_plot_1_params_description = (
-            "Plot number 1 on this chart is a blue solid line plot. "
+            "This is plot number 1 on this chart. "
+            "This is a blue solid line plot. "
             "This plot shows data for COVID-19. "
             "Specifically the metric 'COVID-19_deaths_ONSByDay' for the London area, "
             "along with the age banding of 'all' for the gender group of all. "
         )
         expected_plot_2_params_description = (
-            "Plot number 2 on this chart is a blue dash line plot. "
+            "This is plot number 2 on this chart. "
+            "This is a blue dash line plot. "
             "This plot shows data for Influenza. "
             "Specifically the metric 'influenza_testing_positivityByWeek' "
             "for the London area, "
             "along with the age banding of '0_4' for the gender group of females. "
         )
         expected_plot_3_params_description = (
-            "Plot number 3 on this chart is a blue solid bar plot. "
+            "This is plot number 3 on this chart. "
+            "This is a blue solid bar plot. "
             "The plot has a label of 'Influenza testing for Males'. "
             "This plot shows data for COVID-19. "
             "Specifically the metric 'influenza_testing_positivityByWeek' "
@@ -135,3 +141,80 @@ class TestPlotsText:
         assert expected_plot_1_params_description in text
         assert expected_plot_2_params_description in text
         assert expected_plot_3_params_description in text
+
+    def test_returns_correct_text_about_data_for_one_plot(
+        self, fake_plot_data: PlotData
+    ):
+        """
+        Given a list of 1 enriched `PlotData` model
+        When `construct_text()` is called
+            from an instance of `PlotsText`
+        Then the returned text provides commentary
+            about the data being shown by the plot
+        """
+        # Given
+        plots_text = PlotsText(plots_data=[fake_plot_data])
+
+        # When
+        text: str = plots_text.construct_text()
+
+        # Then
+        expected_text_about_plot_data = (
+            "This plot shows data from 01 January 2023 to 12 January 2023. "
+            "It rose from 1.0 on 01 January 2023 to 2.0 on 02 January 2023. "
+            "It rose from 4.0 on 03 January 2023 to 5.0 on 04 January 2023. "
+            "It fell from 5.0 on 05 January 2023 to 2.0 on 06 January 2023. "
+            "It rose from 1.0 on 07 January 2023 to 8.0 on 08 January 2023. "
+            "It rose from 9.0 on 09 January 2023 to 10.0 on 10 January 2023. "
+            "And finally. "
+            "It rose from 2.0 on 11 January 2023 to 3.0 on 12 January 2023. "
+        )
+        assert expected_text_about_plot_data in text
+
+    def test_returns_correct_text_about_data_for_multiple_plots(
+        self, fake_plot_data: PlotData
+    ):
+        """
+        Given a list of 2 enriched `PlotData` models
+        When `construct_text()` is called
+            from an instance of `PlotsText`
+        Then the returned text declares the parameters
+            which were used for each plot
+        """
+        # Given
+        second_plot_data = fake_plot_data.model_copy(deep=True)
+        second_plot_data.parameters.topic = "Influenza"
+        second_plot_data.parameters.metric = "influenza_testing_positivityByWeek"
+        second_plot_data.parameters.sex = "f"
+        second_plot_data.parameters.age = "0_4"
+
+        plots_text = PlotsText(plots_data=[fake_plot_data, second_plot_data])
+
+        # When
+        text: str = plots_text.construct_text()
+
+        # Then
+        expected_plot_1_data_summary = (
+            "This plot shows data from 01 January 2023 to 12 January 2023. "
+            "It rose from 1.0 on 01 January 2023 to 2.0 on 02 January 2023. "
+            "It rose from 4.0 on 03 January 2023 to 5.0 on 04 January 2023. "
+            "It fell from 5.0 on 05 January 2023 to 2.0 on 06 January 2023. "
+            "It rose from 1.0 on 07 January 2023 to 8.0 on 08 January 2023. "
+            "It rose from 9.0 on 09 January 2023 to 10.0 on 10 January 2023. "
+            "And finally. "
+            "It rose from 2.0 on 11 January 2023 to 3.0 on 12 January 2023."
+        )
+
+        expected_plot_2_data_summary = (
+            "This plot shows data from 01 January 2023 to 12 January 2023. "
+            "It rose from 1.0 on 01 January 2023 to 2.0 on 02 January 2023. "
+            "It rose from 4.0 on 03 January 2023 to 5.0 on 04 January 2023. "
+            "It fell from 5.0 on 05 January 2023 to 2.0 on 06 January 2023. "
+            "It rose from 1.0 on 07 January 2023 to 8.0 on 08 January 2023. "
+            "It rose from 9.0 on 09 January 2023 to 10.0 on 10 January 2023. "
+            "And finally. "
+            "It rose from 2.0 on 11 January 2023 to 3.0 on 12 January 2023."
+        )
+
+        assert expected_plot_1_data_summary in text
+        assert expected_plot_2_data_summary in text
