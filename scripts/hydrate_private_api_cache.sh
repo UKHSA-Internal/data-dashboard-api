122c2626ce58ca763f5ba8f1de9cea87981b090e
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-19 19:54:44 +0000

    Use `uhd` CLI tooling from within hydrate private API shell script

--- scripts/hydrate_private_api_cache.sh
+++ scripts/hydrate_private_api_cache.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-function hydrate_private_api_cache() {
-    python manage.py hydrate_private_api_cache
-}
+function run_script() {
+    echo "Running script to fill cache for private API"
 
-function collect_static_files() {
     # This script is intended to be ran within a job
     # Which means the entrypoint of the container is overridden with a call to this script
     # As such, the static files need to be collected within the job process
     # Before the `hydrate_private_api_cache` can be called
-    python manage.py collectstatic --noinput
-}
-
-function run_script() {
-    echo "Running script to fill cache for private API"
-
-    echo "Collecting static files"
-    collect_static_files
-    echo "Finished collecting static files"
-
-    echo "Running command to fill cache for private API"
-    hydrate_private_api_cache
+    uhd server setup-static-files
+    uhd cache flush-redis
     echo "Completed filling cache for private API"
 }
 
