40e4adbea10168907c7510eab31bfc86d837ba26
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-19 21:16:42 +0000

    Enforce use of key word arguments for callers in `caching/` module

--- caching/private_api/crawler/headless_cms_api.py
+++ caching/private_api/crawler/headless_cms_api.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class HeadlessCMSAPICrawler:
     """Crawls the headless CMS `pages` API for all available pages"""
 
-    def __init__(self, internal_api_client: InternalAPIClient):
+    def __init__(self, *, internal_api_client: InternalAPIClient):
         self._internal_api_client = internal_api_client
 
     def process_list_pages_for_headless_cms_api(self) -> None:
@@ -27,7 +27,7 @@ class HeadlessCMSAPICrawler:
         self._internal_api_client.hit_pages_list_endpoint_for_all_page_types()
 
     def process_detail_pages_for_headless_cms_api(
-        self, pages: list[HomePage, TopicPage]
+        self, *, pages: list[HomePage, TopicPage]
     ) -> None:
         """Makes a request to the headless CMS API detail `pages/` endpoint for each of the given `pages`
 
@@ -39,7 +39,7 @@ class HeadlessCMSAPICrawler:
             self.process_individual_page_for_headless_cms_api(page=page)
 
     def process_individual_page_for_headless_cms_api(
-        self, page: HomePage | TopicPage
+        self, *, page: HomePage | TopicPage
     ) -> None:
         """Makes a request to the headless CMS API detail `pages/` endpoint for the given `page`
 
