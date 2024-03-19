66a02fdcf94dd4ec7c37246b029f2d877b5be189
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-19 19:54:15 +0000

    Use `uhd` CLI tooling from within entry point shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 function set_up_django() {
-    echo "Migrating tables"
-    python manage.py migrate --noinput
+    source uhd.sh
 
-    echo "Collecting static files"
-    python manage.py collectstatic --noinput
+    uhd django migrate
+    uhd server setup-static-files
 
-    echo "Starting server"
-    gunicorn metrics.api.wsgi:application \
-      --workers=3 \
-      --threads=3 \
-      --worker-class=gthread \
-      --timeout=120 \
-      --bind=0.0.0.0:80
+    local port=80
+    uhd server run-production $port
 }
 
 set_up_django
