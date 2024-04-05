ba219d326b515fceb9fd02f2c382df53695c641e
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-05 15:16:34 +0100

    Add `GlobalBannerManager` and `GlobalBannerQueryset`

---
+++ cms/snippets/managers/global_banner.py
@@ -0,0 +1,35 @@
+from typing import Self
+
+from django.db import models
+from wagtail.query import PageQuerySet
+
+
+class GlobalBannerQuerySet(PageQuerySet):
+    """Custom queryset which can be used by the `GlobalBannerManager"""
+
+    def get_active_banners(self) -> Self:
+        """Gets the all currently active `GlobalBanner`.
+
+        Returns:
+            QuerySet: A queryset of the active banners:
+                Examples:
+                    `<GlobalBannerQuerySet [<GlobalBanner: Active information-level global banner>]>`
+        """
+        return self.filter(is_active=True)
+
+
+class GlobalBannerManager(models.Manager):
+    """Custom model manager class for the `GlobalBanner` model"""
+
+    def get_queryset(self) -> GlobalBannerQuerySet:
+        return GlobalBannerQuerySet(model=self.model, using=self.db)
+
+    def has_active_banner(self) -> bool:
+        """Checks if there is already a `GlobalBanner` which is active
+
+        Returns:
+            True if there is a `GlobalBanner` which has `is_active` set to True.
+            False otherwise.
+
+        """
+        return self.get_queryset().get_active_banners().exists()
