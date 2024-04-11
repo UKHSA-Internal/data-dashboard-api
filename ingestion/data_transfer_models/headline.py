38e4ea1b63fee00994339e00e0a41958f36653e6
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-11 16:48:49 +0100

    Cast `embargo` on `InboundHeadlineSpecificFields` as timezone aware datetime object

--- ingestion/data_transfer_models/headline.py
+++ ingestion/data_transfer_models/headline.py
@@ -1,7 +1,9 @@
 import datetime
 
 from pydantic import BaseModel
+from pydantic.functional_validators import field_validator
 
+from ingestion.data_transfer_models import validation
 from ingestion.data_transfer_models.base import IncomingBaseDataModel
 from ingestion.utils import type_hints
 
@@ -13,6 +15,25 @@ class InboundHeadlineSpecificFields(BaseModel):
     period_end: datetime.date
     embargo: datetime.datetime | None
     metric_value: float
+
+    @field_validator("embargo")
+    @classmethod
+    def cast_embargo_to_uk_timezone(
+        cls, embargo: datetime.datetime
+    ) -> datetime.datetime:
+        """Casts the inbound `embargo` to the London timezone
+
+        Args:
+            embargo: The inbound refresh date
+                datetime object
+
+        Returns:
+            A `datetime` object which has the timezone
+            info set to the declared `TIMEZONE` as per
+            the main django settings
+
+        """
+        return validation.cast_date_to_uk_timezone(date_value=embargo)
 
 
 class HeadlineDTO(IncomingBaseDataModel):
