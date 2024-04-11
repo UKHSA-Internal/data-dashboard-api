8f7905400f111ebe29977968b89eb8f35f031123
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-11 10:26:32 +0100

    Add tests for `geography_code` validation when `geography_type` is "Government Office Region"

--- tests/unit/ingestion/data_transfer_models/base/test_validate_geography_code.py
+++ tests/unit/ingestion/data_transfer_models/base/test_validate_geography_code.py
@@ -314,6 +314,61 @@ class TestIncomingBaseValidationForNationGeographyCode:
             IncomingBaseDataModel(**payload)
 
 
+class TestIncomingBaseValidationForGovernmentOfficeRegionGeographyCode:
+    def test_valid_geography_code_validates_successfully(
+        self, valid_payload_for_base_model: dict[str, str]
+    ):
+        """
+        Given a payload containing a valid `geography_code` value
+            for a `geography_type` of "Government Office Region"
+        When the `IncomingBaseDataModel` model is initialized
+        Then model is deemed valid
+        """
+        # Given
+        payload = valid_payload_for_base_model
+        payload["geography_type"] = enums.GeographyType.GOVERNMENT_OFFICE_REGION.value
+        payload["geography_code"] = VALID_GOVERNMENT_OFFICE_REGION_CODE
+
+        # When
+        incoming_base_validation = IncomingBaseDataModel(**payload)
+
+        # Then
+        incoming_base_validation.model_validate(
+            incoming_base_validation,
+            strict=True,
+        )
+
+    @pytest.mark.parametrize(
+        "geography_code",
+        (
+            VALID_NHS_REGION_CODE,
+            VALID_NHS_TRUST_CODE,
+            VALID_LOWER_TIER_LOCAL_AUTHORITY_CODE,
+            VALID_UPPER_TIER_LOCAL_AUTHORITY_CODE,
+            VALID_UKHSA_REGION_CODE,
+            VALID_NATION_CODE,
+        ),
+    )
+    def test_invalid_geography_code_throws_error(
+        self, geography_code: str, valid_payload_for_base_model: dict[str, str]
+    ):
+        """
+        Given a payload containing a `geography_code`
+            which is not valid for
+            the "Government Office Region" `geography_type`
+        When the `IncomingBaseDataModel` model is initialized
+        Then a `ValidationError` is raised
+        """
+        # Given
+        payload = valid_payload_for_base_model
+        payload["geography_type"] = enums.GeographyType.GOVERNMENT_OFFICE_REGION.value
+        payload["geography_code"] = geography_code
+
+        # When / Then
+        with pytest.raises(ValidationError):
+            IncomingBaseDataModel(**payload)
+
+
 class TestIncomingBaseValidationForUpperTierLocalAuthorityGeographyCode:
     @pytest.mark.parametrize(
         "geography_code",
