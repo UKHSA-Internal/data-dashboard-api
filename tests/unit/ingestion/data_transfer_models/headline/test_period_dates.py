780dcc747800b9ecfff0f998cc5ab86b408da2e6
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-12 12:11:10 +0100

    Add test for None values provided for either `period_start` or `period_end`

--- tests/unit/ingestion/data_transfer_models/headline/test_period_dates.py
+++ tests/unit/ingestion/data_transfer_models/headline/test_period_dates.py
@@ -59,11 +59,39 @@ class TestInboundHeadlineSpecificFieldsForPeriodDates:
         period_end = "2024-04-10"
         period_start = "2024-04-12"
 
-        # When
+        # When / Then
         with pytest.raises(ValidationError):
             InboundHeadlineSpecificFields(
                 period_start=period_start,
                 period_end=period_end,
+                embargo=None,
+                metric_value=123,
+            )
+
+    @pytest.mark.parametrize(
+        "period_start, period_end",
+        [
+            ("2023-01-01", None),
+            (None, "2023-01-01"),
+        ],
+    )
+    def test_deemed_invalid_if_none_provided_to_date(
+        self, period_start: str | None, period_end: str | None
+    ):
+        """
+        Given a None value provided for either period date fields
+        When the `InboundHeadlineSpecificFields` is initialized
+        Then a `ValidationError` is raised
+        """
+        # Given
+        input_period_start = period_start
+        input_period_end = period_end
+
+        # When / Then
+        with pytest.raises(ValidationError):
+            InboundHeadlineSpecificFields(
+                period_start=input_period_start,
+                period_end=input_period_end,
                 embargo=None,
                 metric_value=123,
             )
