38b5dfd293b8d5c02cb203b3b54b74036ade90ce
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-05-22 11:16:19 +0100

    CDD-1959: Heat alert endpoint

--- tests/unit/metrics/interfaces/weather_health_alerts/test_access.py
+++ tests/unit/metrics/interfaces/weather_health_alerts/test_access.py
@@ -1,4 +1,8 @@
+from unittest import mock
+
 import datetime
+
+import pytest
 
 from metrics.domain.weather_health_alerts.mapping import (
     WeatherHealthAlertTopics,
@@ -7,6 +11,8 @@ from metrics.domain.weather_health_alerts.mapping import (
 from metrics.interfaces.weather_health_alerts.access import (
     WeatherHealthAlertsInterface,
     WEATHER_HEALTH_ALERT_DETAILED_DATA,
+    get_summary_data_for_alerts,
+    get_detailed_data_for_alert,
 )
 from tests.fakes.factories.metrics.headline_factory import FakeCoreHeadlineFactory
 from tests.fakes.managers.headline_manager import FakeCoreHeadlineManager
@@ -14,6 +20,8 @@ from django.utils import timezone
 from metrics.domain.weather_health_alerts.text_lookups import HEAT_ALERT_TEXT_LOOKUP
 
 from tests.fakes.models.metrics.headline import FakeCoreHeadline
+
+MODULE_PATH = "metrics.interfaces.weather_health_alerts.access"
 
 
 class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
@@ -35,7 +43,10 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
         # Given
         topic_name = WeatherHealthAlertTopics.HEAT_ALERT.value
         metric_name = "heat-alert_headline_matrixNumber"
-        geography_code = "E07000115"
+        geography_code = "E12000001"
+        geography_name = "North East"
+        geography_type_name = "Government Office Region"
+
         fake_core_headline_manager = FakeCoreHeadlineManager(headlines=[])
         weather_health_alerts_interface = WeatherHealthAlertsInterface(
             core_headline_manager=fake_core_headline_manager
@@ -47,6 +58,8 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
                 topic_name=topic_name,
                 metric_name=metric_name,
                 geography_code=geography_code,
+                geography_name=geography_name,
+                geography_type_name=geography_type_name,
             )
         )
 
@@ -78,7 +91,9 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
         # Given
         topic_name = WeatherHealthAlertTopics.HEAT_ALERT.value
         metric_name = "heat-alert_headline_matrixNumber"
-        geography_code = "E07000115"
+        geography_code = "E12000001"
+        geography_name = "North East"
+        geography_type_name = "Government Office Region"
         two_weeks_ago = timezone.now() - datetime.timedelta(days=14)
         one_week_ago = timezone.now() - datetime.timedelta(days=7)
         red_alert_metric_value = 16
@@ -87,6 +102,8 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
             metric_name=metric_name,
             metric_value=red_alert_metric_value,
             geography_code=geography_code,
+            geography_name=geography_name,
+            geography_type_name=geography_type_name,
             period_end=one_week_ago,
             refresh_date=two_weeks_ago,
         )
@@ -104,6 +121,8 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
                 topic_name=topic_name,
                 metric_name=metric_name,
                 geography_code=geography_code,
+                geography_name=geography_name,
+                geography_type_name=geography_type_name,
             )
         )
 
@@ -134,7 +153,9 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
         # Given
         topic_name = WeatherHealthAlertTopics.HEAT_ALERT.value
         metric_name = "heat-alert_headline_matrixNumber"
-        geography_code = "E07000115"
+        geography_code = "E12000001"
+        geography_name = "North East"
+        geography_type_name = "Government Office Region"
         two_weeks_from_now = timezone.now() + datetime.timedelta(days=14)
         one_week_ago = timezone.now() - datetime.timedelta(days=7)
         red_alert_metric_value = 16
@@ -143,6 +164,8 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
             metric_name=metric_name,
             metric_value=red_alert_metric_value,
             geography_code=geography_code,
+            geography_name=geography_name,
+            geography_type_name=geography_type_name,
             period_end=two_weeks_from_now,
             refresh_date=one_week_ago,
         )
@@ -160,6 +183,8 @@ class TestWeatherHealthAlertsInterfaceBuildDetailedDataForAlert:
                 topic_name=topic_name,
                 metric_name=metric_name,
                 geography_code=geography_code,
+                geography_name=geography_name,
+                geography_type_name=geography_type_name,
             )
         )
 
@@ -195,8 +220,13 @@ class TestWeatherHealthAlertsInterfaceBuildSummaryDataForAlerts:
         topic_name = WeatherHealthAlertTopics.HEAT_ALERT.value
         metric_name = "heat-alert_headline_matrixNumber"
         first_geography_code = "E07000115"
+        first_geography_name = "Tonbridge and Malling"
         second_geography_code = "E07000120"
-        geography_codes = [first_geography_code, second_geography_code]
+        second_geography_name = "Hyndburn"
+        geography_data = [
+            (first_geography_code, first_geography_name),
+            (second_geography_code, second_geography_name),
+        ]
 
         fake_core_headline_manager = FakeCoreHeadlineManager(headlines=[])
         weather_health_alerts_interface = WeatherHealthAlertsInterface(
@@ -208,22 +238,22 @@ class TestWeatherHealthAlertsInterfaceBuildSummaryDataForAlerts:
             weather_health_alerts_interface.build_summary_data_for_alerts(
                 topic_name=topic_name,
                 metric_name=metric_name,
-                geography_codes=geography_codes,
+                geography_data=geography_data,
             )
         )
 
         # Then
-        assert len(summary_alarm_data) == len(geography_codes)
+        assert len(summary_alarm_data) == len(geography_data)
         assert (
-            summary_alarm_data[first_geography_code]["status"]
+            summary_alarm_data[0]["status"]
             == WeatherHealthAlertStatusColour.GREEN.value
         )
-        assert summary_alarm_data[first_geography_code]["refresh_date"] is None
+        assert summary_alarm_data[0]["refresh_date"] is None
         assert (
-            summary_alarm_data[second_geography_code]["status"]
+            summary_alarm_data[1]["status"]
             == WeatherHealthAlertStatusColour.GREEN.value
         )
-        assert summary_alarm_data[second_geography_code]["refresh_date"] is None
+        assert summary_alarm_data[1]["refresh_date"] is None
 
     def test_for_alerts_which_have_expired(self):
         """
@@ -245,7 +275,13 @@ class TestWeatherHealthAlertsInterfaceBuildSummaryDataForAlerts:
         topic_name = WeatherHealthAlertTopics.HEAT_ALERT.value
         metric_name = "heat-alert_headline_matrixNumber"
         first_geography_code = "E07000115"
+        first_geography_name = "Tonbridge and Malling"
         second_geography_code = "E07000120"
+        second_geography_name = "Hyndburn"
+        geography_data = [
+            (first_geography_code, first_geography_name),
+            (second_geography_code, second_geography_name),
+        ]
         two_weeks_ago = timezone.now() - datetime.timedelta(days=14)
         one_week_ago = timezone.now() - datetime.timedelta(days=7)
         red_alert_metric_value = 16
@@ -285,26 +321,26 @@ class TestWeatherHealthAlertsInterfaceBuildSummaryDataForAlerts:
             weather_health_alerts_interface.build_summary_data_for_alerts(
                 topic_name=topic_name,
                 metric_name=metric_name,
-                geography_codes=[first_geography_code, second_geography_code],
+                geography_data=geography_data,
             )
         )
 
         # Then
-        assert len(summary_alarm_data) == 2
+        assert len(summary_alarm_data) == len(geography_data)
         assert (
-            summary_alarm_data[first_geography_code]["status"]
+            summary_alarm_data[0]["status"]
             == WeatherHealthAlertStatusColour.GREEN.value
         )
         assert (
-            summary_alarm_data[first_geography_code]["refresh_date"]
+            summary_alarm_data[0]["refresh_date"]
             == fake_expired_red_alert_for_first_geography.period_end
         )
         assert (
-            summary_alarm_data[second_geography_code]["status"]
+            summary_alarm_data[1]["status"]
             == WeatherHealthAlertStatusColour.GREEN.value
         )
         assert (
-            summary_alarm_data[second_geography_code]["refresh_date"]
+            summary_alarm_data[1]["refresh_date"]
             == fake_expired_red_alert_for_second_geography.period_end
         )
 
@@ -325,7 +361,13 @@ class TestWeatherHealthAlertsInterfaceBuildSummaryDataForAlerts:
         topic_name = WeatherHealthAlertTopics.HEAT_ALERT.value
         metric_name = "heat-alert_headline_matrixNumber"
         first_geography_code = "E07000115"
+        first_geography_name = "Tonbridge and Malling"
         second_geography_code = "E07000120"
+        second_geography_name = "Hyndburn"
+        geography_data = [
+            (first_geography_code, first_geography_name),
+            (second_geography_code, second_geography_name),
+        ]
 
         two_weeks_from_now = timezone.now() + datetime.timedelta(days=14)
         one_week_ago = timezone.now() - datetime.timedelta(days=7)
@@ -368,26 +410,113 @@ class TestWeatherHealthAlertsInterfaceBuildSummaryDataForAlerts:
             weather_health_alerts_interface.build_summary_data_for_alerts(
                 topic_name=topic_name,
                 metric_name=metric_name,
-                geography_codes=[first_geography_code, second_geography_code],
+                geography_data=geography_data,
             )
         )
 
         # Then
-        assert len(detailed_alarm_data) == 2
+        assert len(detailed_alarm_data) == len(geography_data)
 
         assert (
-            detailed_alarm_data[first_geography_code]["status"]
-            == WeatherHealthAlertStatusColour.RED.value
+            detailed_alarm_data[0]["status"] == WeatherHealthAlertStatusColour.RED.value
         )
         assert (
-            detailed_alarm_data[first_geography_code]["refresh_date"]
+            detailed_alarm_data[0]["refresh_date"]
             == fake_expired_red_alert_for_first_geography.refresh_date
         )
         assert (
-            detailed_alarm_data[second_geography_code]["status"]
+            detailed_alarm_data[1]["status"]
             == WeatherHealthAlertStatusColour.AMBER.value
         )
         assert (
-            detailed_alarm_data[second_geography_code]["refresh_date"]
+            detailed_alarm_data[1]["refresh_date"]
             == fake_expired_red_alert_for_first_geography.refresh_date
         )
+
+
+class TestAccessGetSummaryDataForAlerts:
+    @pytest.mark.parametrize(
+        "geography_data, topic, metric",
+        [
+            (
+                [("E06000001", "North East"), ("E06000002", "North West")],
+                "Heat-alert",
+                "heat-alert_headline_matrixNumber",
+            )
+        ],
+    )
+    @mock.patch.object(WeatherHealthAlertsInterface, "build_summary_data_for_alerts")
+    def test_access_get_summary_data_for_alerts_makes_correct_call(
+        self,
+        spy_build_summary_data_for_alerts: mock.MagicMock,
+        geography_data: list[tuple[str, str]],
+        topic: str,
+        metric: str,
+    ):
+        """
+        Given a valid payload including geography codes, geograpny names, topic and metric names
+        When when the `get_summary_data_for_alerts()` method is called
+        Then the `WeatherHealthAlertsInterface.build_summary_data_for_alerts()` method is called.
+        """
+        # Given
+        fake_geography_data = geography_data
+        fake_topic = topic
+        fake_metric = metric
+
+        # When
+        get_summary_data_for_alerts(
+            geography_data=fake_geography_data,
+            topic_name=fake_topic,
+            metric_name=fake_metric,
+        )
+
+        # Then
+        spy_build_summary_data_for_alerts.assert_called()
+
+
+class TestAccessGetDetailedDataForAlerts:
+    @pytest.mark.parametrize(
+        "geography_code, geography_name, geography_type_name, topic, metric",
+        [
+            (
+                "E06000001",
+                "North East",
+                "Government Office Region",
+                "Heat-alert",
+                "heat-alert_headline_matrixNumber",
+            )
+        ],
+    )
+    @mock.patch.object(WeatherHealthAlertsInterface, "build_detailed_data_for_alert")
+    def test_access_get_details_data_for_alert_makes_correct_call(
+        self,
+        spy_build_detailed_data_for_alert: mock.MagicMock,
+        geography_code: str,
+        geography_name: str,
+        geography_type_name: str,
+        topic: str,
+        metric: str,
+    ):
+        """
+        Given a valid payload including geography code, geograpny name, topic and metric names
+        When when the `get_detailed_data_for_alert()` method is called
+        Then the `WeatherHealthAlertsInterface.build_detailed_data_for_alert()` method is called.
+        """
+        # Given
+        fake_geography_code = geography_code
+        fake_geography_name = geography_name
+        fake_topic = topic
+        fake_metric = metric
+        fake_geography_type_name = geography_type_name
+
+        # When
+        get_detailed_data_for_alert(
+            geography_code=fake_geography_code,
+            geography_name=fake_geography_name,
+            geography_type_name=fake_geography_type_name,
+            topic_name=fake_topic,
+            metric_name=fake_metric,
+        )
+
+        # Then
+        spy_build_detailed_data_for_alert.assert_called()
