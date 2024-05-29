56c7d1721f373fc5bd4db0bebdd94929464b59c6
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-05-29 09:27:29 +0100

    update weather alerts text

--- metrics/domain/weather_health_alerts/text_lookups/cold_alert_text.py
+++ metrics/domain/weather_health_alerts/text_lookups/cold_alert_text.py
@@ -2,100 +2,102 @@ from metrics.domain.weather_health_alerts.text_lookups import common
 
 _LEVEL_6_TEXT = """
 <p>Minor impacts are possible across the health and social care sector, 
-including:</p><ul><li>increased use of health care services by the vulnerable population</li>
+including:</p><ul><li>Increased use of health care services by the vulnerable population.</li>
 <li>increase in risk of mortality amongst vulnerable individuals. But not expected.</li></ul>
 """
 _LEVEL_7_TEXT = """
 <p>Minor impacts are probable across the health and social care sector, 
-including:</p><ul><li>increased use of healthcare services by the vulnerable population</li>
-<li>increase in risk of mortality amongst vulnerable individuals.</li></ul>
+including:</p><ul><li>Increased use of healthcare services by the vulnerable population.</li>
+<li>Increase in risk of mortality amongst vulnerable individuals.</li></ul>
 """
 _LEVEL_8_TEXT = """
 <p>Minor impacts are expected across the health and social care sector, 
-including:</p><ul><li>increased use of health care services by the vulnerable population</li>
-<li>increase in risk of mortality amongst vulnerable individuals.</li></ul>
+including:</p><ul><li>Increased use of health care services by the vulnerable population.</li>
+<li>Increase in risk of mortality amongst vulnerable individuals.</li></ul>
 """
 _LEVEL_9_TEXT = """
 <p>There is potential for significant impacts to be observed across the health and social care sector 
-due to forecast weather conditions, including:</p><ul><li>observed increase in mortality across the population, 
+due to forecast weather conditions, including:</p><ul><li>Observed increase in mortality across the population, 
 particularly in the 65+ age group or those with certain underlying health conditions, 
-but impacts may also be seen in younger age groups; increased demand for remote health care services likely</li>
-<li>impact on ability of services delivered due to effects on workforce possible</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely.</li>
+<li>Impact on ability of services delivered due to effects on workforce possible.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
 leading to increased risk of vulnerable people.</li></ul>
 """
 _LEVEL_10_TEXT = """
 <p>Significant impacts are possible across the health and social care sector due to forecast weather conditions, 
-including:</p><ul><li>observed increase in mortality across the population, 
+including:</p><ul><li>Observed increase in mortality across the population, 
 particularly in the 65+ age group or those with certain underlying health conditions, 
-but impacts may also be seen in younger age groups</li>
-<li>increased demand for remote health care services likely</li>
-<li>impact on ability of services delivered due to effects on workforce possible</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely.</li>
+<li>Impact on ability of services delivered due to effects on workforce possible</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
 leading to increased risk of vulnerable people.</li></ul>
 """
 _LEVEL_11_TEXT = """
 <p>There is potential for severe impacts to be observed across the health and social care sector 
-due to forecast weather conditions, including:</p><ul><li>increased risk of mortality across the whole population 
-with significant mortality observed in older age groups</li> 
-<li>significant increased demand on all health and social care services</li>
-<li>impact on delivery of services due to poor weather conditions and staff access</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
-leading to increased risk of vulnerable people</li>
-<li>national critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
+due to forecast weather conditions, including:</p><ul><li>Increased risk of mortality across the whole population 
+with significant mortality observed in older age groups.</li> 
+<li>Significant increased demand on all health and social care services.</li>
+<li>Impact on delivery of services due to poor weather conditions and staff access.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+leading to increased risk of vulnerable people.</li>
+<li>National critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
 """
 _LEVEL_12_TEXT = """
 <p>Significant impacts are probable across the health and social care sector due to forecast weather conditions, 
-including:</p><ul><li>observed increase in mortality across the population, 
+including:</p><ul><li>Observed increase in mortality across the population, 
 particularly in the 65+ age group or those with certain underlying health conditions, 
-but impacts may also be seen in younger age groups</li>
-<li>increased demand for remote health care services likely</li>
-<li>internal temperatures in care settings (e.g. hospitals, care homes and primary care settings) 
-may fall below recommended threshold for clinical risk assessment</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
-leading to increased risk of vulnerable people</li>
-<li>staffing issues due to external factors (e.g. travel delays)</li>
-<li>other sectors may start to observe impacts (e.g. transport and energy).</li></ul>
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely.</li>
+<li>Internal temperatures in care settings (e.g. hospitals, care homes and primary care settings) 
+may fall below recommended threshold for clinical risk assessment.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+leading to increased risk of vulnerable people.</li>
+<li>Staffing issues due to external factors (e.g. travel delays).</li>
+<li>Other sectors may start to observe impacts (e.g. transport and energy).</li></ul>
 """
 _LEVEL_13_TEXT = """
 <p>Significant impacts are expected across the health and social care sector due to forecast weather conditions, 
-including:</p><ul><li>observed increase in mortality across the population, 
+including:</p><ul><li>Observed increase in mortality across the population, 
 particularly in the 65+ age group or those with certain underlying health conditions, 
-but impacts may also be seen in younger age groups</li>
-<li>increased demand for remote health care services likely</li>
-<li>internal temperatures in care settings (e.g. hospitals, care homes and primary care settings) 
-may fall below recommended threshold for clinical risk assessment</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
-leading to increased risk of vulnerable people; staffing issues due to external factors (e.g. travel delays)</li>
-<li>other sectors starting to observe impacts (e.g. transport and energy)</li>
-<li>other sectors starting to observe impacts (e.g. transport and energy).</li></ul>
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely.</li>
+<li>Internal temperatures in care settings (e.g. hospitals, care homes and primary care settings) 
+may fall below recommended threshold for clinical risk assessment.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+leading to increased risk of vulnerable people.</li>
+<li>Staffing issues due to external factors (e.g. travel delays).</li>
+<li>Other sectors starting to observe impacts (e.g. transport and energy).</li>
+<li>Other sectors starting to observe impacts (e.g. transport and energy).</li></ul>
 """
 _LEVEL_14_TEXT = """
 <p>Severe impacts are possible across the health and social care sector due to forecast weather conditions, 
-including:</p><ul><li>increased risk of mortality across the whole population with significant mortality 
-observed in older age groups; significant increased demand on all health and social care services</li>
-<li>impact on delivery of services due to poor weather conditions and staff access</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
-leading to increased risk of vulnerable people</li>
-<li>national critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
+including:</p><ul><li>Increased risk of mortality across the whole population with significant mortality 
+observed in older age groups.</li><li>Significant increased demand on all health and social care services.</li>
+<li>Impact on delivery of services due to poor weather conditions and staff access.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+leading to increased risk of vulnerable people.</li>
+<li>National critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
 """
 _LEVEL_15_TEXT = """
 <p>Severe impacts are probable across the health and social care sector due to forecast weather conditions, 
-including:</p><ul><li>increased risk of mortality across the whole population with significant mortality 
-observed in older age groups</li><li>significant increased demand on all health and social care services</li>
-<li>impact on delivery of services due to poor weather conditions and staff access</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
-leading to increased risk of vulnerable people</li> 
-<li>national critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
+including:</p><ul><li>Increased risk of mortality across the whole population with significant mortality 
+observed in older age groups.</li><li>Significant increased demand on all health and social care services.</li>
+<li>Impact on delivery of services due to poor weather conditions and staff access.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+leading to increased risk of vulnerable people.</li> 
+<li>National critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
 """
 _LEVEL_16_TEXT = """
 <p>Severe impacts are expected across the health and social care sector due to forecast weather conditions, 
-including:</p><ul><li>increased risk of mortality across the whole population with significant mortality 
-observed in older age groups; significant increased demand on all health and social care services</li>
-<li>impact on delivery of services due to poor weather conditions and staff access</li>
-<li>maintaining indoor temperatures at recommended 18°c may become challenging for some, 
-leading to increased risk of vulnerable people</li>
-<li>national critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
+including:</p><ul><li>Increased risk of mortality across the whole population with significant mortality 
+observed in older age groups.</li><li>Significant increased demand on all health and social care services.</li>
+<li>Impact on delivery of services due to poor weather conditions and staff access.</li>
+<li>Maintaining indoor temperatures at recommended 18°c may become challenging for some, 
+leading to increased risk of vulnerable people.</li>
+<li>National critical infrastructure failures – generators, power outages, gas supplies etc.</li></ul>
 """
 
 COLD_ALERT_TEXT_LOOKUP: dict[int, str] = {
--- metrics/domain/weather_health_alerts/text_lookups/heat_alert_text.py
+++ metrics/domain/weather_health_alerts/text_lookups/heat_alert_text.py
@@ -2,109 +2,110 @@ from metrics.domain.weather_health_alerts.text_lookups import common
 
 _LEVEL_6_TEXT = """
 <p>Minor impacts are possible across the health and social care sector, 
-including:</p><ul><li>increased use of health care services by the vulnerable population</li>
-<li>increase in risk of mortality amongst vulnerable individuals 
+including:</p><ul><li>Increased use of health care services by the vulnerable population.</li>
+<li>Increase in risk of mortality amongst vulnerable individuals 
 and increased potential for indoor environments to become very warm. 
 But not expected.</li></ul>
 """
 _LEVEL_7_TEXT = """
 <p>Minor impacts are probable across the health and social care sector, 
-including:</p><ul><li>increased use of healthcare services by the vulnerable population</li>
-<li>increase in risk of mortality amongst vulnerable individuals 
+including:</p><ul><li>Increased use of healthcare services by the vulnerable population.</li>
+<li>Increase in risk of mortality amongst vulnerable individuals 
 and increased potential for indoor environments to become very warm.</li></ul>"""
 _LEVEL_8_TEXT = """
 <p>Minor impacts are expected across the health and social care sector, 
-including:</p><ul><li>increased use of health care services by the vulnerable population</li>
-<li>increase in risk of mortality amongst vulnerable individuals 
+including:</p><ul><li>Increased use of health care services by the vulnerable population.</li>
+<li>Increase in risk of mortality amongst vulnerable individuals 
 and increased potential for indoor environments to become very warm.</li></ul>
 """
 _LEVEL_9_TEXT = """
 <p>There is potential for significant impacts to be observed across the health and social care sector 
-due to the high temperatures, including:</p><ul><li>observed increase in mortality across the population likely, 
+due to the high temperatures, including:</p><ul><li>Observed increase in mortality across the population likely, 
 particularly in the 65+ age group or those with health conditions, 
-but impacts may also be seen in younger age groups</li>
-<li>increased demand for remote health care services likely
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely
 internal temperatures in care settings (hospitals and care homes) may exceed recommended threshold 
-for clinical risk assessment</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce possible 
+for clinical risk assessment.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce possible 
 and many indoor environments likely to be overheating, 
 risk to vulnerable people living independently in community as well as in care settings.</li></ul>
 """
 _LEVEL_10_TEXT = """
 <p>Significant impacts are possible across the health and social care sector due to the high temperatures, 
-including:</p><ul></li>observed increase in mortality across the population likely, 
+including:</p><ul></li>Observed increase in mortality across the population likely, 
 particularly in the 65+ age group or those with health conditions, 
-but impacts may also be seen in younger age groups</li>
-<li>increased demand for remote health care services likely</li>
-<li>internal temperatures in care settings (hospitals and care homes) may exceed recommended threshold 
-for clinical risk assessment</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce possible 
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely.</li>
+<li>Internal temperatures in care settings (hospitals and care homes) may exceed recommended threshold 
+for clinical risk assessment.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce possible 
 and many indoor environments likely to be overheating, 
 risk to vulnerable people living independently in community as well as in care settings.</li></ul>
 """
 _LEVEL_11_TEXT = """
 <p>There is potential for severe impacts to be observed across the health and social care sector 
 due to the high temperatures, 
-including:</p><ul><li>increased risk of mortality across the whole population 
-with significant mortality observed in older age groups</li>
-<li>significant increased demand on all health and social care services</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce</li>
-<li>indoor environments likely to be hot making provision of care challenging 
+including:</p><ul><li>Increased risk of mortality across the whole population 
+with significant mortality observed in older age groups.</li>
+<li>Significant increased demand on all health and social care services.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce.</li>
+<li>Indoor environments likely to be hot making provision of care challenging 
 and national critical infrastructure failures – generators, power outages etc.</li></ul>
 """
 _LEVEL_12_TEXT = """
 <p>Significant impacts are probable across the health and social care sector due to the high temperatures, 
-including:</p><ul><li>observed increase in mortality across the population likely, 
+including:</p><ul><li>Observed increase in mortality across the population likely, 
 particularly in the 65+ age group or those with health conditions, 
-but impacts may also be seen in younger age groups;</li><li>increased demand for remote health care services likely</li>
-<li>internal temperatures in care settings (hospitals and care homes) may exceed recommended threshold 
-for clinical risk assessment</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce possible 
+but impacts may also be seen in younger age groups.</li>
+<li>Increased demand for remote health care services likely.</li>
+<li>Internal temperatures in care settings (hospitals and care homes) may exceed recommended threshold 
+for clinical risk assessment.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce possible 
 and many indoor environments likely to be overheating, 
-risk to vulnerable people living independently in community as well as in care settings</li>
-<li>medicines management issues</li><li>staffing issues due to external factors (e.g. transport)</li> 
-<li>cross system demand for temporary AC capacity being exceeded possible 
+risk to vulnerable people living independently in community as well as in care settings.</li>
+<li>Medicines management issues.</li><li>Staffing issues due to external factors (e.g. transport).</li> 
+<li>Cross system demand for temporary AC capacity being exceeded possible 
 and other sectors starting to be observe impacts (e.g. travel delays).</li></ul>
 """
 _LEVEL_13_TEXT = """
 <p>Significant impacts are expected across the health and social care sector due to the high temperatures, 
 including:</p><ul><li>observed increase in mortality across the population likely, 
 particularly in the 65+ age group or those with health conditions, 
-but impacts may also be seen in younger age groups</li><li>increased demand for remote health care services likely</li>
+but impacts may also be seen in younger age groups</li><li>increased demand for remote health care services likely.</li>
 <li>internal temperatures in care settings (hospitals and care homes) may exceed recommended threshold 
-for clinical risk assessment</li>
+for clinical risk assessment.</li>
 <li>impact on ability of services to be delivered due to heat effects on workforce possible 
 and many indoor environments likely to be overheating, 
-risk to vulnerable people living independently in community as well as in care settings</li>
-<li>medicines management issues; staffing issues due to external factors (e.g. transport)</li>
+risk to vulnerable people living independently in community as well as in care settings.</li>
+<li>medicines management issues.</li><li>Staffing issues due to external factors (e.g. transport).</li>
 <li>cross system demand for temporary AC capacity being exceeded possible 
 and other sectors starting to be observe impacts (e.g. travel delays).</li></ul>
 """
 _LEVEL_14_TEXT = """
 <p>Severe impacts are possible across the health and social care sector due to the high temperatures, 
-including:</p><ul><li>increased risk of mortality across the whole population 
-with significant mortality observed in older age groups</li>
-<li>significant increased demand on all health and social care services</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce</li>
-<li>indoor environments likely to be hot making provision of care challenging 
+including:</p><ul><li>Increased risk of mortality across the whole population 
+with significant mortality observed in older age groups.</li>
+<li>Significant increased demand on all health and social care services.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce.</li>
+<li>Indoor environments likely to be hot making provision of care challenging 
 and national critical infrastructure failures – generators, power outages etc.</li></ul>
 """
 _LEVEL_15_TEXT = """
 <p>Severe impacts are probable across the health and social care sector due to the high temperatures, 
-including:</p><ul><li>increased risk of mortality across the whole population 
-with significant mortality observed in older age groups</li>
-<li>significant increased demand on all health and social care services</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce</li>
-<li>indoor environments likely to be hot making provision of care challenging 
+including:</p><ul><li>Increased risk of mortality across the whole population 
+with significant mortality observed in older age groups.</li>
+<li>Significant increased demand on all health and social care services.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce.</li>
+<li>Indoor environments likely to be hot making provision of care challenging 
 and national critical infrastructure failures – generators, power outages etc.</li></ul>
 """
 _LEVEL_16_TEXT = """
 <p>Severe impacts are expected across the health and social care sector due to the high temperatures, 
-including:</p><ul><li>increased risk of mortality across the whole population 
-with significant mortality observed in older age groups</li>
-<li>significant increased demand on all health and social care services</li>
-<li>impact on ability of services to be delivered due to heat effects on workforce</li>
-<li>indoor environments likely to be hot making provision of care challenging 
+including:</p><ul><li>Increased risk of mortality across the whole population 
+with significant mortality observed in older age groups.</li>
+<li>Significant increased demand on all health and social care services.</li>
+<li>Impact on ability of services to be delivered due to heat effects on workforce.</li>
+<li>Indoor environments likely to be hot making provision of care challenging 
 and national critical infrastructure failures – generators, power outages etc.</li></ul>
 """
 
