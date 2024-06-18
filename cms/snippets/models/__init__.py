3d4ef454258eb2b3e7b71b7e1552a861ec594226
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-06-18 11:06:00 +0100

    CDD-1977: Add WHA button snippet model

--- cms/snippets/models/__init__.py
+++ cms/snippets/models/__init__.py
@@ -1,3 +1,4 @@
 from .internal_button import InternalButton, InternalButtonTypes
 from .external_button import ExternalButton, ExternalButtonTypes, ExternalButtonIcons
+from .wha_button import WeatherAlertButton, WeatherAlertButtonTypes
 from .global_banner import GlobalBanner
