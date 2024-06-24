2017e69d7a84530483502e853283ba68743c27bc
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-24 16:34:03 +0100

    Add helper texts for dynamic content blocks used for the menu builder

---
+++ cms/snippets/models/menu_builder/help_texts.py
@@ -0,0 +1,44 @@
+MENU_LINK_HELP_TEXT: str = """
+The title to display for this menu item.
+"""
+
+MENU_LINK_BODY: str = """
+An optional body of text to display for this menu item.
+"""
+
+SECONDARY_LINKS: str = """
+Optional secondary / submenu links to attach under the primary menu link.
+"""
+
+SECONDARY_LINK: str = """
+A secondary link to attach to the list of submenu items under the primary menu link.
+"""
+
+MENU_IS_ACTIVE: str = """
+Whether to activate this menu. 
+Note that only 1 menu can be active at a time.
+To switch from 1 active menu to another, 
+you must deactivate the 1st menu and save it before activating and saving the 2nd menu.
+"""
+
+MENU_COLUMN_HEADING: str = """
+An optional heading to place at the top of this menu column.
+"""
+
+MENU_COLUMN_LINKS: str = """
+Add any number of associated links within this menu column.
+"""
+
+MENU_COLUMNS: str = """
+Add any number of column within this menu row.
+"""
+
+MENU_ROW: str = """
+Add any number of rows within this menu.
+"""
+
+MENU_INTERNAL_LABEL: str = """
+A label to associate with this particular menu design.
+Note that this label is private / internal and is not used on the dashboard.
+This is purely to help identify each of the constructed menu designs.
+"""
