6dbcbe407f395b4a6fb585a547d24e0a0d4905c0
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-04 14:27:03 +0100

    Move global_banner view into `views` package

---
+++ cms/snippets/views/global_banner.py
@@ -0,0 +1,37 @@
+from http import HTTPStatus
+
+from drf_spectacular.utils import extend_schema
+from rest_framework.response import Response
+from rest_framework.utils.serializer_helpers import ReturnDict
+from rest_framework.views import APIView
+
+from cms.snippets.serializers import (
+    GlobalBannerResponseSerializer,
+    GlobalBannerSerializer,
+)
+
+
+class GlobalBannerView(APIView):
+    permission_classes = []
+
+    @classmethod
+    @extend_schema(
+        tags=["cms"], responses={HTTPStatus.OK: GlobalBannerResponseSerializer}
+    )
+    def get(cls, request, *args, **kwargs) -> Response:
+        """
+        This endpoint returns data associated with the currently active global banner
+
+        Note that if there is no active banner then the response will look like:
+
+        ```
+        {"active_global_banner": null}
+        ```
+
+        """
+        serializer = GlobalBannerSerializer()
+        serialized_response_data: dict[str, ReturnDict[str, str] | None] = (
+            serializer.data
+        )
+
+        return Response(data=serialized_response_data, status=HTTPStatus.OK)
