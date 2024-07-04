6fbce1deff3339ae0544250127857c6b803c29a3
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-04 14:10:08 +0100

    Add `MenuSerializer` and `MenuResponseSerializer`

--- cms/snippets/serializers.py
+++ cms/snippets/serializers.py
@@ -1,8 +1,8 @@
 from django.db.models import Manager
 from rest_framework import serializers
 from rest_framework.utils.serializer_helpers import ReturnDict
-
-from cms.snippets.models import GlobalBanner
+from wagtail.models import Page
+from cms.snippets.models import GlobalBanner, Menu
 
 
 class ButtonSerializer(serializers.Serializer):
@@ -43,6 +43,18 @@ class GlobalBannerResponseSerializer(serializers.ModelSerializer):
         return super().data
 
 
+class MenuResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ["body"]
+
+    @property
+    def data(self) -> dict[str, list[dict]] | None:
+        if self.instance is None:
+            return None
+        return {"body": self.instance.body.get_prep_value()}
+
+
 class GlobalBannerSerializer(serializers.Serializer):
     @property
     def global_banner_manager(self) -> Manager:
@@ -73,3 +85,26 @@ def get_active_global_banner(
     global_banner = global_banner_manager.get_active_banner()
     serializer = GlobalBannerResponseSerializer(instance=global_banner)
     return serializer.data
+
+
+class MenuSerializer(serializers.Serializer):
+    @property
+    def menu_manager(self) -> Manager:
+        return self.context.get("menu_manager", Menu.objects)
+
+    @property
+    def data(self) -> dict[str, ReturnDict[str, str] | None]:
+        """Gets the body associated with the currently active global banner information
+
+        Args:
+            `menu_manager`: The `MenuManager`
+                used to query for records
+
+        Returns:
+            Dict representation the of the active menu.
+            If no menu is active, then None is returned
+
+        """
+        active_menu = self.menu_manager.get_active_menu()
+        serializer = MenuResponseSerializer(instance=active_menu)
+        return serializer.data
