34dfe5d9ece7e468b3b512116053d5d8bee9e77b
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-08 09:35:00 +0100

    Add `reporting_lag_period` column to core timeseries table

--- metrics/data/models/core_models/help_texts.py
+++ metrics/data/models/core_models/help_texts.py
@@ -29,3 +29,10 @@ Age band of patients.
 SEX = """
 Sex of patients.
 """
+
+# Timeseries specific fields
+REPORTING_LAG_PERIOD = """
+Whether the record falls within the current reporting lag period. 
+If true, then the value is subject to change in a subsequent retrospective update. 
+If false, then the value is considered to be final and static.
+"""
