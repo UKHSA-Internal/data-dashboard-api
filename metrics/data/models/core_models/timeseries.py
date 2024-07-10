a1d17a6d50ae8e04229621704fbf5eaa034acf9c
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-10 08:46:09 +0100

    Update name from `reporting_lag_period` -> `in_reporting_lag_period`

--- metrics/data/models/core_models/timeseries.py
+++ metrics/data/models/core_models/timeseries.py
@@ -49,8 +49,8 @@ class CoreTimeSeries(models.Model):
 
     refresh_date = models.DateTimeField(help_text=help_texts.REFRESH_DATE, null=True)
     embargo = models.DateTimeField(help_text=help_texts.EMBARGO, null=True)
-    reporting_lag_period = models.BooleanField(
-        help_text=help_texts.REPORTING_LAG_PERIOD,
+    in_reporting_lag_period = models.BooleanField(
+        help_text=help_texts.IN_REPORTING_LAG_PERIOD,
         null=True,
         blank=True,
     )
