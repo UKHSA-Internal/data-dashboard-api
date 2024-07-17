09d24d7bc35fe6ad8c455be8087fef4df3f789bf
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-17 11:42:52 +0100

    Remove duplicated kwarg

--- tests/unit/metrics/interfaces/plots/test_access.py
+++ tests/unit/metrics/interfaces/plots/test_access.py
@@ -140,12 +140,12 @@ class TestPlotsInterface:
             parameters=valid_plot_parameters,
             x_axis_values=[x.date for x in fake_core_time_series_records],
             y_axis_values=[x.metric_value for x in fake_core_time_series_records],
-            latest_date=str(max(x.date for x in fake_core_time_series_records)),
             additional_values={
                 "in_reporting_delay_period": [
                     x.in_reporting_delay_period for x in fake_core_time_series_records
                 ]
             },
+            latest_date=str(max(x.date for x in fake_core_time_series_records)),
         )
         assert plots_data == [expected_plots_data_for_valid_params]
 
