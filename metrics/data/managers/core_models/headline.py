a289776a86d1423cba7a48481591548d0e1ce8e0
A-Ashiq <afaan.ashiq@nate.tech>
2024-08-15 14:53:35 +0100

    Implement `query_for_superseded_data()` on `CoreHeadlineManager`

--- metrics/data/managers/core_models/headline.py
+++ metrics/data/managers/core_models/headline.py
@@ -194,7 +194,7 @@ class CoreHeadlineManager(models.Manager):
             topic_name: The name of the disease being queried.
                 E.g. `COVID-19`
             metric_name: The name of the metric being queried.
-                E.g. `COVID-19_deaths_ONSByDay`
+                E.g. `COVID-19_headline_7DayAdmissions`
             geography_name: The name of the geography being queried.
                 E.g. `England`
             geography_type_name: The name of the geography
@@ -231,6 +231,54 @@ class CoreHeadlineManager(models.Manager):
             )
             .first()
         )
+
+    def query_for_superseded_data(
+        self,
+        *,
+        topic_name: str,
+        metric_name: str,
+        geography_name: str,
+        geography_type_name: str,
+        geography_code: str,
+        stratum_name: str,
+        sex: str,
+        age: str,
+    ) -> CoreHeadlineQuerySet:
+        """Grabs all stale records which are not under embargo.
+
+        Args:
+           topic_name: The name of the threat being queried.
+                E.g. `COVID-19`
+           metric_name: The name of the metric being queried.
+               E.g. `COVID-19_headline_7DayAdmissions`
+           geography_name: The name of the geography being queried.
+               E.g. `England`
+           geography_type_name: The name of the geography type being queried.
+               E.g. `Nation`
+           geography_code: Code associated with the geography being queried.
+               E.g. "E45000010"
+           stratum_name: The value of the stratum to apply additional filtering to.
+               E.g. `default`, which would be used to capture all strata.
+           sex: The gender to apply additional filtering to.
+               E.g. `F`, would be used to capture Females.
+               Note that options are `M`, `F`, or `ALL`.
+           age: The age range to apply additional filtering to.
+               E.g. `0_4` would be used to capture the age of 0-4 years old
+
+        Returns:
+           The stale records in their entirety as a queryset
+
+        """
+        return self.get_queryset().get_headlines_released_from_embargo(
+            topic_name=topic_name,
+            metric_name=metric_name,
+            geography_name=geography_name,
+            geography_type_name=geography_type_name,
+            geography_code=geography_code,
+            stratum_name=stratum_name,
+            age=age,
+            sex=sex,
+        )[1:]
 
     def get_latest_headlines_for_geography_codes(
         self,
