bc89e3bad4d742c3eb96f5512940219188ec05a1
A-Ashiq <afaan.ashiq@nate.tech>
2024-09-10 15:14:28 +0100

    Update test

--- tests/unit/cms/dashboard/test_wagtail_hooks.py
+++ tests/unit/cms/dashboard/test_wagtail_hooks.py
@@ -115,18 +115,18 @@ def test_register_link_props(spy_link_entity_with_href: mock.MagicMock):
         via the `register_converter_rule()` call
     """
     # Given
-    spy_features = mock.Mock()
+    fake_rule = {"to_database_format": {"entity_decorators": {"LINK": {}}}}
+    fake_converter_rules = {
+        "contentstate": {"link": fake_rule}
+    }
+    spy_features = mock.MagicMock()
+    spy_features.converter_rules_by_converter = fake_converter_rules
 
     # When
     wagtail_hooks.register_link_props(features=spy_features)
 
     # Then
-    assert (
-        spy_features.mock_calls[0][1][2]["to_database_format"]["entity_decorators"][
-            "LINK"
-        ]
-        == spy_link_entity_with_href
-    )
+    assert fake_rule["to_database_format"]["entity_decorators"]["LINK"] == spy_link_entity_with_href
 
 
 class TestLinkEntityWithHref:
