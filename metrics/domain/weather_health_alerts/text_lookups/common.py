6cda7cffa0724a6ab25367103def93825a6584c9
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-10-23 12:06:56 +0100

    CDD-2232: Update cold alert impact text

--- metrics/domain/weather_health_alerts/text_lookups/common.py
+++ metrics/domain/weather_health_alerts/text_lookups/common.py
@@ -2,8 +2,8 @@ LEVELS_1_TO_4_TEXT = """
 <p>Little impact observed on health, healthcare services and social care provision. Business as usual.</p>
 """
 LEVEL_5_TEXT = """
-<p>There is potential for minor impacts across the health and social care sector, including:</p>
-<ul><li>Increased use of health care services by the vulnerable population.</li>
-<li>Increase in risk of mortality amongst vulnerable individuals.</li>
-<li>But these are not expected.</li></ul>
+<p>There is potential for minor impacts across health and social care services, including:</p>
+<ul><li>increased use of health care services by the vulnerable population</li>
+<li>greater risk to life of vulnerable people</li></ul>
+<p>But these are not expected.</p>
 """
