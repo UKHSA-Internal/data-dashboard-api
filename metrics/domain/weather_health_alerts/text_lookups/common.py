10eeefec216033c4aa73346d128ec53d535d274f
phill-stanley <139551113+phill-stanley@users.noreply.github.com>
2024-10-25 11:33:00 +0100

    Update metrics/domain/weather_health_alerts/text_lookups/common.py
    Co-authored-by: Afaan Ashiq <47219506+A-Ashiq@users.noreply.github.com>

--- metrics/domain/weather_health_alerts/text_lookups/common.py
+++ metrics/domain/weather_health_alerts/text_lookups/common.py
@@ -3,7 +3,7 @@ LEVELS_1_TO_4_TEXT = """
 """
 LEVEL_5_TEXT = """
 <p>There is potential for minor impacts across health and social care services, including:</p>
-<ul><li>increased use of health care services by the vulnerable population</li>
+<ul><li>increased use of health care services by vulnerable people</li>
 <li>greater risk to life of vulnerable people</li></ul>
 <p>But these are not expected.</p>
 """
