f866fa4bbc5395dfd1652da99d4d03be0147b126
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-12-09 13:17:21 +0000

    CDD-2338: Remove feedback version 1

--- feedback/email_template.py
+++ feedback/email_template.py
@@ -1,30 +1,9 @@
-from enum import Enum
-
 from django.db.models import Manager
 
 from feedback.cms_interface import CMSInterface
 
 FALLBACK_DID_YOU_FIND_EVERYTHING_ANSWER: str = "User provided no input"
 DEFAULT_FORM_PAGE_MANAGER = CMSInterface().get_form_page_manager()
-
-
-def build_body_for_email(*, suggestions: dict[str, str]) -> str:
-    """Builds the suggestions email body as a string to be sent to the email server
-
-    Args:
-        suggestions: A dict containing:
-            keys - Shortform questions
-            values - Answers provided by the user
-
-    Returns:
-        A continuous string which represents
-        the body of the feedback email
-
-    """
-    enriched_suggestions: dict[str, str] = _enrich_suggestions_with_long_form_questions(
-        suggestions=suggestions
-    )
-    return _build_body_from_suggestions(suggestions=enriched_suggestions)
 
 
 def build_body_for_email_v2(*, suggestions: dict[str, str]) -> str:
@@ -44,57 +23,6 @@ def build_body_for_email_v2(*, suggestions: dict[str, str]) -> str:
         _enrich_suggestions_with_long_form_questions_v2(suggestions=suggestions)
     )
     return _build_body_from_suggestions(suggestions=enriched_suggestions)
-
-
-class FeedbackQuestion(Enum):
-    reason = "What was your reason for visiting the dashboard today?"
-    did_you_find_everything = "Did you find everything you were looking for?"
-    improve_experience = "How could we improve your experience with the dashboard?"
-    like_to_see = "What would you like to see on the dashboard in the future?"
-
-    @classmethod
-    def string_based_questions(cls) -> list["FeedbackQuestion"]:
-        return [cls.reason, cls.improve_experience, cls.like_to_see]
-
-
-def _enrich_suggestions_with_long_form_questions(
-    *, suggestions: dict[str, str]
-) -> dict[str, str]:
-    """Enriches the question keys in the given `suggestions` with the long form versions
-
-    Examples:
-        `suggestions` is provided with the following key value pair:
-            `"reason": "some answer"`
-            Then the return key value pair would be:
-            `"What was your reason for visiting the dashboard today?": "some answer"`
-
-    Args:
-        suggestions: Dict of feedback suggestions from the user
-
-    Returns:
-        A dict of feedback suggestions with longform questions as the keys
-        and answers for the values.
-
-    """
-    long_form_suggestions = {
-        question.value: suggestions[question.name]
-        for question in FeedbackQuestion.string_based_questions()
-    }
-
-    did_you_find_everything_enum: FeedbackQuestion = (
-        FeedbackQuestion.did_you_find_everything
-    )
-    # Use the `did_you_find_everything` field from the `suggestions` input
-    # If not available, use the fallback
-    did_you_find_everything_answer = (
-        suggestions.get(did_you_find_everything_enum.name, "")
-        or FALLBACK_DID_YOU_FIND_EVERYTHING_ANSWER
-    )
-    long_form_suggestions[did_you_find_everything_enum.value] = (
-        did_you_find_everything_answer
-    )
-
-    return long_form_suggestions
 
 
 def _enrich_suggestions_with_long_form_questions_v2(
--- tests/unit/feedback/test_email_server.py
+++ tests/unit/feedback/test_email_server.py
@@ -8,8 +8,6 @@ from django.core.mail import EmailMessage
 
 from feedback.email_server import (
     DEFAULT_FEEDBACK_EMAIL_SUBJECT,
-    create_email_message,
-    send_email,
     create_email_message_v2,
     send_email_v2,
     send_email_via_ses,
@@ -18,116 +16,6 @@ from feedback.email_server import (
 
 MODULE_PATH = "feedback.email_server"
 FAKE_EMAIL_RECIPIENT_ADDRESS = "not.real@test.com"
-
-
-class TestCreateEmailMessage:
-    def test_returns_email_message(self):
-        """
-        Given a suggestions dict and a recipient email address
-        When `create_email_message()` is called
-        Then an instance of `EmailMessage` is returned
-        """
-        # Given
-        mocked_suggestions = mock.MagicMock()
-        fake_subject = "Test subject"
-
-        # When
-        email_message: EmailMessage = create_email_message(
-            suggestions=mocked_suggestions,
-            subject=fake_subject,
-            recipient_email_address=FAKE_EMAIL_RECIPIENT_ADDRESS,
-        )
-
-        # Then
-        assert type(email_message) is EmailMessage
-
-    @mock.patch(f"{MODULE_PATH}.build_body_for_email")
-    def test_sets_correct_body_on_email_message(
-        self, spy_build_body_for_email: mock.MagicMock
-    ):
-        """
-        Given a suggestions dict and a recipient email address
-        When `create_email_message()` is called
-        The body is set by delegating a call to `build_body_for_email()`
-
-        Patches:
-            `spy_build_body_for_email`: For the main assertion.
-                To check this is used for the `body`
-                of the created `EmailMessage`
-        """
-        # Given
-        mocked_suggestions = mock.Mock()
-        fake_subject = "Test subject"
-
-        # When
-        email_message: EmailMessage = create_email_message(
-            suggestions=mocked_suggestions,
-            subject=fake_subject,
-            recipient_email_address=FAKE_EMAIL_RECIPIENT_ADDRESS,
-        )
-
-        # Then
-        spy_build_body_for_email.assert_called_once_with(suggestions=mocked_suggestions)
-        assert email_message.body == spy_build_body_for_email.return_value
-
-    def test_sets_default_subject_on_email_message_when_not_provided(self):
-        """
-        Given a suggestions dict and a recipient email address
-        When `create_email_message()` is called
-        Then the default subject line is set on the email message
-        """
-        # Given
-        mocked_suggestions = mock.MagicMock()
-
-        # When
-        email_message: EmailMessage = create_email_message(
-            suggestions=mocked_suggestions,
-            recipient_email_address=FAKE_EMAIL_RECIPIENT_ADDRESS,
-        )
-
-        # Then
-        assert email_message.subject == DEFAULT_FEEDBACK_EMAIL_SUBJECT
-
-    def test_sets_specified_subject_on_email_message_when_provided(self):
-        """
-        Given a suggestions dict and a recipient email address
-        And a specified non-default subject line
-        When `create_email_message()` is called
-        Then the specified subject line is set on the email message
-        """
-        # Given
-        mocked_suggestions = mock.MagicMock()
-        fake_non_default_subject = "Specified example subject"
-
-        # When
-        email_message: EmailMessage = create_email_message(
-            suggestions=mocked_suggestions,
-            recipient_email_address=FAKE_EMAIL_RECIPIENT_ADDRESS,
-            subject=fake_non_default_subject,
-        )
-
-        # Then
-        assert email_message.subject == fake_non_default_subject
-
-    def test_sets_specified_recipient_on_email_message_when_provided(self):
-        """
-        Given a suggestions dict and a specified recipient email address
-        When `create_email_message()` is called
-        Then the specified recipient is set on the recipients of the email message
-        """
-        # Given
-        mocked_suggestions = mock.MagicMock()
-        fake_non_default_subject = "Specified example subject"
-
-        # When
-        email_message: EmailMessage = create_email_message(
-            suggestions=mocked_suggestions,
-            recipient_email_address=FAKE_EMAIL_RECIPIENT_ADDRESS,
-            subject=fake_non_default_subject,
-        )
-
-        # Then
-        assert FAKE_EMAIL_RECIPIENT_ADDRESS in email_message.recipients()
 
 
 class TestCreateEmailMessageV2:
@@ -250,73 +138,6 @@ class TestCreateEmailMessageV2:
 
         # Then
         assert FAKE_EMAIL_RECIPIENT_ADDRESS in email_message.recipients()
-
-
-class TestSendEmail:
-    @mock.patch(f"{MODULE_PATH}.create_email_message")
-    def test_delegates_call_to_create_email_message(
-        self, spy_create_email_message: mock.MagicMock
-    ):
-        """
-        Given a suggestions dict, a recipient email address and a subject line
-        When `send_email()` is called
-        Then the call is delegated to `create_email_message()`
-
-        Patches:
-            `spy_create_email_message`: For the main assertion.
-                To check the email message object is initialized
-                with the correct arguments
-        """
-        # Given
-        mocked_suggestions = mock.Mock()
-        mocked_recipient_email_address = mock.Mock()
-        mocked_subject = mock.Mock()
-
-        # When
-        send_email(
-            suggestions=mocked_suggestions,
-            recipient_email_address=mocked_recipient_email_address,
-            subject=mocked_subject,
-        )
-
-        # Then
-        spy_create_email_message.assert_called_once_with(
-            suggestions=mocked_suggestions,
-            subject=mocked_subject,
-            recipient_email_address=mocked_recipient_email_address,
-        )
-
-    @mock.patch(f"{MODULE_PATH}.create_email_message")
-    def test_calls_send_on_email_message(
-        self, spy_create_email_message: mock.MagicMock
-    ):
-        """
-        Given a suggestions dict, a recipient email address and a subject line
-        When `send_email()` is called
-        Then the `send()` is called from the returned `EmailMessage`
-
-        Patches:
-            `spy_create_email_message`: To capture and spy on
-                the returned `EmailMessage`.
-                And to check `send()` is called from the `EmailMessage`
-        """
-        # Given
-        mocked_suggestions = mock.Mock()
-        mocked_recipient_email_address = mock.Mock()
-        mocked_subject = mock.Mock()
-        fail_silently = True
-
-        # When
-        send_email(
-            suggestions=mocked_suggestions,
-            recipient_email_address=mocked_recipient_email_address,
-            subject=mocked_subject,
-            fail_silently=fail_silently,
-        )
-
-        # Then
-        email_message = spy_create_email_message.return_value
-        email_message.send.assert_called_once_with(fail_silently=fail_silently)
 
 
 class TestSendEmailV2:
