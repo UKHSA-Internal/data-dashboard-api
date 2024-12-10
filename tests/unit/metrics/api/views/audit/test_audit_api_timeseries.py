06195115304b1363b8eb466fd8a5cda422c8a0f9
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2024-12-10 15:50:05 +0000

    CDD-2326: refactor tests to use monkeypatch context

--- tests/unit/metrics/api/views/audit/test_audit_api_timeseries.py
+++ tests/unit/metrics/api/views/audit/test_audit_api_timeseries.py
@@ -12,6 +12,7 @@ class TestAuditAPITimeseriesViewSet:
     def test_authentication_is_required_when_app_mode_cms(
         self,
         spy_permissions_is_authenticated: mock.MagicMock,
+        monkeypatch,
     ):
         """
         Given the application is running with an APP_MODE of `CMS_ADMIN`
@@ -19,15 +20,15 @@ class TestAuditAPITimeseriesViewSet:
         Then rest_frameworks's `IsAuthenticated()` will be called.
         """
         # Given
-        config.APP_MODE = "CMS_ADMIN"
-        audit_api_timeseries_viewset = AuditAPITimeSeriesViewSet()
+        with monkeypatch.context() as m:
+            m.setattr(target=config, name="APP_MODE", value="CMS_ADMIN")
+            audit_api_timeseries_viewset = AuditAPITimeSeriesViewSet()
 
-        # When
-        audit_api_timeseries_viewset.get_permissions()
+            # When
+            audit_api_timeseries_viewset.get_permissions()
 
         # Then
         spy_permissions_is_authenticated.assert_called_once()
-        config.APP_MODE = ""
 
     def test_sets_no_api_key_restrictions(self):
         """
