5586d8c24629bb749f9482c30520cca38d17e82c
A-Ashiq <afaan.ashiq2@gmail.com>
2024-12-17 15:58:36 +0000

    Force user to login when browser session is closed, or after 8 hours. Allow timer to be reset on each request made for CMS admin app

--- metrics/api/settings/cms_admin.py
+++ metrics/api/settings/cms_admin.py
@@ -1,2 +1,6 @@
 # Disables the check for maximum permissible fields to be sent in request to the server
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 60 * 60 * 8
+SESSION_SAVE_EVERY_REQUEST = True
