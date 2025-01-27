4c0d7a20f9e7b9c43a94539277e793d37c8e5517
A-Ashiq <afaan.ashiq2@gmail.com>
2025-01-27 15:34:48 +0000

    Update swagger docs for public API

--- metrics/api/settings/public_api.py
+++ metrics/api/settings/public_api.py
@@ -2,7 +2,6 @@ SPECTACULAR_SETTINGS = {
     "COMPONENT_SPLIT_REQUEST": True,
     "PREPROCESSING_HOOKS": ["metrics.api.open_api.pre_processing_endpoint_filter_hook"],
     "TITLE": "UKHSA Data Dashboard API Docs",
-    "DESCRIPTION": "Docs for the API which supports the UKHSA Data Dashboard. "
-    "To access v1 documentation visit /api/swagger for v2 of the documentation visit /api/swagger/v2",
+    "DESCRIPTION": "Docs for the API which supports the UKHSA Data Dashboard. ",
     "VERSION": "v 0.1.0",
 }
