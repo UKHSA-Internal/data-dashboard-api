2b66c6a225bc05b3b638b3b75983c7af9bdefad3
Luke Towell <luke.towell@burendo.com>
2025-02-20 11:46:59 +0000

    feature/CDD-2285: update for linting

--- cms/dynamic_content/cards.py
+++ cms/dynamic_content/cards.py
@@ -54,8 +54,7 @@ class WHAlerts(models.TextChoices):
 
 class WeatherHealthAlertsCard(blocks.StructBlock):
     title = blocks.TextBlock(required=True, help_text=help_texts.TITLE_FIELD)
-    sub_title = blocks.TextBlock(
-        required=True, help_text=help_texts.SUB_TITLE_FIELD)
+    sub_title = blocks.TextBlock(required=True, help_text=help_texts.SUB_TITLE_FIELD)
     alert_type = blocks.ChoiceBlock(
         required=True,
         choices=WHAlerts.get_alerts,
@@ -82,9 +81,9 @@ class HeadlineNumbersRowCard(blocks.StructBlock):
 class ChartWithHeadlineAndTrendCard(blocks.StructBlock):
     title = blocks.TextBlock(required=True, help_text=help_texts.TITLE_FIELD)
     sub_title = blocks.TextBlock(
-        required=False, help_text=help_texts.OPTIONAL_CHART_SUBTITLE_FIELD)
-    body = blocks.TextBlock(
-        required=False, help_text=help_texts.OPTIONAL_BODY_FIELD)
+        required=False, help_text=help_texts.OPTIONAL_CHART_SUBTITLE_FIELD
+    )
+    body = blocks.TextBlock(required=False, help_text=help_texts.OPTIONAL_BODY_FIELD)
     tag_manager_event_id = blocks.CharBlock(
         required=False,
         help_text=help_texts.TAG_MANAGER_EVENT_ID_FIELD,
@@ -136,8 +135,7 @@ class ChartWithHeadlineAndTrendCard(blocks.StructBlock):
 
 class SimplifiedChartWithLink(blocks.StructBlock):
     title = blocks.TextBlock(required=True, help_text=help_texts.TITLE_FIELD)
-    sub_title = blocks.CharBlock(
-        required=False, help_text=help_texts.SUB_TITLE_FIELD)
+    sub_title = blocks.CharBlock(required=False, help_text=help_texts.SUB_TITLE_FIELD)
     tag_manager_event_id = blocks.CharBlock(
         required=False,
         help_text=help_texts.TAG_MANAGER_EVENT_ID_FIELD,
@@ -193,9 +191,9 @@ class SimplifiedChartWithLink(blocks.StructBlock):
 class ChartCard(blocks.StructBlock):
     title = blocks.TextBlock(required=True, help_text=help_texts.TITLE_FIELD)
     sub_title = blocks.TextBlock(
-        required=False, help_text=help_texts.OPTIONAL_CHART_SUBTITLE_FIELD)
-    body = blocks.TextBlock(
-        required=False, help_text=help_texts.OPTIONAL_BODY_FIELD)
+        required=False, help_text=help_texts.OPTIONAL_CHART_SUBTITLE_FIELD
+    )
+    body = blocks.TextBlock(required=False, help_text=help_texts.OPTIONAL_BODY_FIELD)
     tag_manager_event_id = blocks.CharBlock(
         required=False,
         help_text=help_texts.TAG_MANAGER_EVENT_ID_FIELD,
