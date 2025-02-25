59c3a17fd975413580f66100bff185055609ebdf
Joe Gasewicz (UKHSA) <joe.gasewicz@ukhsa.gov.uk>
2025-02-25 15:22:17 +0000

    CDD-2432 Implement Middleware - auth decorator

--- tests/factories/metrics/rbac_models/rbac_group_permissions.py
+++ tests/factories/metrics/rbac_models/rbac_group_permissions.py
@@ -1,5 +1,3 @@
-import contextlib
-import datetime
 from typing import List
 
 import factory
@@ -7,9 +5,6 @@ from metrics.data.models.rbac_models import (
     RBACPermission,
     RBACGroupPermission,
 )
-
-
-from django.utils import timezone
 
 
 class RBACPermissionGroupFactory(factory.django.DjangoModelFactory):
@@ -30,21 +25,3 @@ class RBACPermissionGroupFactory(factory.django.DjangoModelFactory):
         for permission in permissions:
             group.permissions.add(permission)
         return group
-
-    @classmethod
-    def _make_datetime_timezone_aware(
-        cls, datetime_obj: str | datetime.datetime | None
-    ) -> datetime.datetime:
-
-        if datetime_obj is None:
-            return datetime_obj
-
-        with contextlib.suppress(TypeError):
-            # If it is already a datetime object then suppress the resulting TypeError
-            datetime_obj = datetime.datetime.strptime(datetime_obj, "%Y-%m-%d")
-
-        try:
-            return timezone.make_aware(value=datetime_obj)
-        except ValueError:
-            # The object is already timezone aware
-            return datetime_obj
