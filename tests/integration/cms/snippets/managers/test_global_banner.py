632c7ac5b81167c603ca071d40a03d79c60600cf
Luke Towell <luke.towell@burendo.com>
2025-03-13 16:04:39 +0000

    CDD-2505: updated integration tests around the global banner manager

--- tests/integration/cms/snippets/managers/test_global_banner.py
+++ tests/integration/cms/snippets/managers/test_global_banner.py
@@ -1,4 +1,6 @@
 import pytest
+from django.utils import timezone
+import datetime
 
 from cms.snippets.models import GlobalBanner
 from tests.factories.cms.snippets.global_banner import GlobalBannerFactory
@@ -33,24 +35,48 @@ class TestGlobalBannerManager:
         Given a number of `GlobalBanner` records
             of which multiple have `is_active` set to True
             and the `banner_type` is both `Warning` and `Information`
+            and the global banners are created with different times
         When `get_active_banners()` is called
             from the `GlobalBannerManager`
         Then all active banners should be returned grouped by `banner_type`
-            with `Warning` banners above `Information`.
+            with `Warning` banners above `Information` and with the most
+            recently updated banners at the top of the banner_type group.
         """
         # Given
         # Create multiple Information/Warning global banners.
-        GlobalBannerFactory.create(is_active=True)
-        GlobalBannerFactory.create(is_active=True, banner_type="Warning")
-        GlobalBannerFactory.create(is_active=True)
-        GlobalBannerFactory.create(is_active=True, banner_type="Warning")
+        info_banner_old = GlobalBannerFactory.create(is_active=True)
+        warning_banner_old = GlobalBannerFactory.create(
+            is_active=True, banner_type="Warning"
+        )
+        info_banner_new = GlobalBannerFactory.create(is_active=True)
+        warning_banner_new = GlobalBannerFactory.create(
+            is_active=True, banner_type="Warning"
+        )
+
+        # Update the updated on banners manually as this avoid the save method of the
+        # model which will just set the updated_on value to the time that the test is ran
+        now = timezone.now()
+        GlobalBanner.objects.filter(id=info_banner_old.id).update(
+            updated_on=now - datetime.timedelta(days=3)
+        )
+        GlobalBanner.objects.filter(id=warning_banner_old.id).update(
+            updated_on=now - datetime.timedelta(days=2)
+        )
+        GlobalBanner.objects.filter(id=info_banner_new.id).update(
+            updated_on=now - datetime.timedelta(days=1)
+        )
 
         # When
         get_active_banners: GlobalBanner = GlobalBanner.objects.get_active_banners()
 
         # Then
         assert len(get_active_banners) == 4
+
         assert get_active_banners[0].banner_type == "Warning"
         assert get_active_banners[1].banner_type == "Warning"
+        # assert that the first banner in the warning array was updated more recently than the second banner.
+        assert get_active_banners[0].updated_on >= get_active_banners[1].updated_on
+
         assert get_active_banners[2].banner_type == "Information"
         assert get_active_banners[3].banner_type == "Information"
+        assert get_active_banners[2].updated_on >= get_active_banners[3].updated_on
