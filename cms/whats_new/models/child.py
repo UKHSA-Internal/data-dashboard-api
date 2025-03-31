242979d8c1f3da659c5938b617b5df197bad6caf
Luke Towell <luke.towell@burendo.com>
2025-03-31 15:31:34 +0100

    CDD-2504: Updated the whats new parent and child models

--- cms/whats_new/models/child.py
+++ cms/whats_new/models/child.py
@@ -5,6 +5,8 @@ from wagtail.fields import RichTextField
 from wagtail.search import index
 
 from cms.dashboard.models import AVAILABLE_RICH_TEXT_FEATURES, UKHSAPage
+from cms.dynamic_content import help_texts
+from cms.snippets.models.global_banner import BannerTypes
 from cms.whats_new.managers.child import WhatsNewChildEntryManager
 from cms.whats_new.serializers import BadgeSerializer
 
@@ -63,3 +65,54 @@ class WhatsNewChildEntry(UKHSAPage):
     parent_page_type = ["whats_new.WhatsNewParentPage"]
 
     objects = WhatsNewChildEntryManager()
+
+class WhatsNewChildEntryAnnouncement(Orderable):
+    page = ParentalKey(
+        WhatsNewParentPage, on_delete=models.SET_NULL, null=True, related_name="announcements"
+    )
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        help_text=help_texts.GLOBAL_BANNER_TITLE,
+    )
+    badge = models.ForeignKey(
+        "whats_new.badge",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    body = RichTextField(
+        max_length=255,
+        features=AVAILABLE_RICH_TEXT_FEATURES,
+        help_text=help_texts.GLOBAL_BANNER_BODY,
+    )
+    banner_type = models.CharField(
+        max_length=50,
+        choices=BannerTypes.choices,
+        default=BannerTypes.INFORMATION.value,
+        help_text=help_texts.GLOBAL_BANNER_TYPE,
+    )
+
+    is_active = models.BooleanField(
+        default=False,
+        help_text=help_texts.GLOBAL_BANNER_IS_ACTIVE,
+    )
+
+    # Sets which panels to show on the editing view
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("badge"),
+        FieldPanel("body"),
+        FieldPanel("banner_type"),
+        FieldPanel("is_active"),
+    ]
+
+    # Sets which fields to expose on the API
+    api_fields = [
+        APIField("title"),
+        APIField("badge"),
+        APIField("body"),
+        APIField("banner_type"),
+        APIField("is_active"),
+    ]
--- cms/whats_new/models/parent.py
+++ cms/whats_new/models/parent.py
@@ -1,13 +1,18 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from wagtail.admin.panels import FieldPanel, ObjectList, TabbedInterface
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, ObjectList, TabbedInterface, InlinePanel
 from wagtail.api import APIField
 from wagtail.search import index
+from wagtail.models import Orderable
+from wagtail.fields import RichTextField
 
 from cms.dashboard.models import (
+    AVAILABLE_RICH_TEXT_FEATURES,
     UKHSAPage,
 )
 from cms.dynamic_content import help_texts
+from cms.snippets.models.global_banner import BannerTypes
 from cms.whats_new.managers.parent import WhatsNewParentPageManager
 
 
@@ -59,3 +64,55 @@ class WhatsNewParentPage(UKHSAPage):
     subpage_types = ["whats_new.WhatsNewChildEntry", "common.CommonPage"]
 
     objects = WhatsNewParentPageManager()
+
+
+class WhatsNewParentPageAnnouncement(Orderable):
+    page = ParentalKey(
+        WhatsNewParentPage, on_delete=models.SET_NULL, null=True, related_name="announcements"
+    )
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        help_text=help_texts.GLOBAL_BANNER_TITLE,
+    )
+    badge = models.ForeignKey(
+        "whats_new.badge",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    body = RichTextField(
+        max_length=255,
+        features=AVAILABLE_RICH_TEXT_FEATURES,
+        help_text=help_texts.GLOBAL_BANNER_BODY,
+    )
+    banner_type = models.CharField(
+        max_length=50,
+        choices=BannerTypes.choices,
+        default=BannerTypes.INFORMATION.value,
+        help_text=help_texts.GLOBAL_BANNER_TYPE,
+    )
+
+    is_active = models.BooleanField(
+        default=False,
+        help_text=help_texts.GLOBAL_BANNER_IS_ACTIVE,
+    )
+
+    # Sets which panels to show on the editing view
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("badge"),
+        FieldPanel("body"),
+        FieldPanel("banner_type"),
+        FieldPanel("is_active"),
+    ]
+
+    # Sets which fields to expose on the API
+    api_fields = [
+        APIField("title"),
+        APIField("badge"),
+        APIField("body"),
+        APIField("banner_type"),
+        APIField("is_active"),
+    ]
