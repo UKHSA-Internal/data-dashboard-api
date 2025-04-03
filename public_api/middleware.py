a5bdae1abaa73d58ccba8b890ff937888e4b965a
A-Ashiq <afaan.ashiq2@gmail.com>
2025-04-03 09:44:08 +0100

    Add type hints
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- public_api/middleware.py
+++ public_api/middleware.py
@@ -1,8 +1,10 @@
 from django.utils.deprecation import MiddlewareMixin
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 
 class NoIndexNoFollowMiddleware(MiddlewareMixin):
     @classmethod
-    def process_response(cls, request, response):
+    def process_response(cls, request: Request, response: Response) -> Response:
         response["X-Robots-Tag"] = "noindex, nofollow"
         return response
