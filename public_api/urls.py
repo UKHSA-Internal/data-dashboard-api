7b1e2761b6aefd732c1c8fb94917eaf3f0a3135f
A-Ashiq <afaan.ashiq2@gmail.com>
2025-04-03 15:04:28 +0100

    Add `/robots.txt` route for auth enabled public API
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- public_api/urls.py
+++ public_api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, resolvers
+from django.views.generic import TemplateView
 
+from metrics.api.settings import AUTH_ENABLED
 from public_api.version_02.views import (
     GeographyDetailViewV2,
     GeographyListViewV2,
@@ -48,6 +50,16 @@ def construct_url_patterns_for_public_api(
     urls = []
     urls.extend(_construct_version_one_urls(prefix=prefix))
     urls.extend(_construct_version_two_urls(prefix=prefix))
+
+    if AUTH_ENABLED:
+        urls.append(
+            path(
+                "robots.txt",
+                TemplateView.as_view(
+                    template_name="robots.txt", content_type="text/plain"
+                ),
+            )
+        )
 
     return urls
 
--- tests/unit/metrics/api/test_urls_construction.py
+++ tests/unit/metrics/api/test_urls_construction.py
@@ -300,6 +300,48 @@ class TestConstructUrlpatterns:
         # Then
         assert not any(excluded_endpoint_path in str(x.pattern) for x in flattened_urls)
 
+    def test_public_api_with_auth_enabled_includes_robots_txt_url(self):
+        """
+        Given `AUTH_ENABLED` is set to True
+        And an `app_mode` of "PUBLIC_API"
+        When `construct_urlpatterns()` is called
+        Then the returned urls patterns
+            does contain `robots.txt`
+        """
+        # Given
+        app_mode = enums.AppMode.PUBLIC_API.value
+
+        # When
+        with mock.patch("public_api.urls.AUTH_ENABLED", True):
+            flattened_urls = _flatten_urls(
+                urlpatterns=construct_urlpatterns(app_mode=app_mode)
+            )
+
+        # Then
+        patterns = [str(x.pattern) for x in flattened_urls]
+        assert "robots.txt" in patterns
+
+    def test_public_api_with_auth_enabled_excludes_robots_txt_url(self):
+        """
+        Given `AUTH_ENABLED` is set to False
+        And an `app_mode` of "PUBLIC_API"
+        When `construct_urlpatterns()` is called
+        Then the returned urls patterns
+            do not contain `robots.txt`
+        """
+        # Given
+        app_mode = enums.AppMode.PUBLIC_API.value
+
+        # When
+        with mock.patch("public_api.urls.AUTH_ENABLED", False):
+            flattened_urls = _flatten_urls(
+                urlpatterns=construct_urlpatterns(app_mode=app_mode)
+            )
+
+        # Then
+        patterns = [str(x.pattern) for x in flattened_urls]
+        assert "robots.txt" not in patterns
+
     # Tests for APP_MODE = "CMS_ADMIN"
 
     def test_cms_admin_mode_returns_cms_admin_urls(self):
