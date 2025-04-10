04ac0f79a5719c0dc8d26e30a10383644f7c6ce5
A-Ashiq <afaan.ashiq2@gmail.com>
2025-04-10 15:38:00 +0100

    Reimplement input validation for `RBACPermission`
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/data/models/rbac_models/rbac_permissions.py
+++ metrics/data/models/rbac_models/rbac_permissions.py
@@ -1,35 +1,24 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Model
 
 from metrics.data.managers.rbac_models import RBACPermissionQuerySet
 from metrics.data.managers.rbac_models.rbac_permissions import RBACPermissionManager
 
 
-class AdminFormThemeError(ValidationError):
-    def __init__(self):
-        message = "You must select a theme."
+class NoModelSelectedError(ValidationError):
+    def __init__(self, category: str):
+        message = f"You must select a {category}."
         super().__init__(message)
 
 
-class AdminFormSubthemeAssocThemeError(ValidationError):
-    def __init__(self):
-        message = "You must select a valid subtheme that belongs to the selected theme."
+class SelectionInvalidWithParentSelectionError(ValidationError):
+    def __init__(self, model: type[Model], parent_model: type[Model]):
+        message = f"The selection of '{model.name}' for the {model.__class__.__name__} cannot be paired with '{parent_model.name}' for the {parent_model.__class__.__name__} ."
         super().__init__(message)
 
 
-class AdminFormTopicAssocSubthemeError(ValidationError):
-    def __init__(self):
-        message = "The selected topic's subtheme must have an associated theme."
-        super().__init__(message)
-
-
-class AdminFormSubthemeAssocTopicError(ValidationError):
-    def __init__(self):
-        message = "The selected subtheme must have an associated topic."
-        super().__init__(message)
-
-
-class AdminFormDuplicatePermissionError(ValidationError):
+class DuplicatePermissionError(ValidationError):
     def __init__(self):
         message = "A permission with these values already exists."
         super().__init__(message)
@@ -116,51 +105,109 @@ class RBACPermission(models.Model):
 
     objects = RBACPermissionManager()
 
-    def __str__(self):
-        model_str = f"name={self.name}, "
-        model_str += f"theme={self.theme.name}, "
-        model_str += f"sub_theme={self.sub_theme.name}"
-        if self.topic is not None:
-            model_str += f", topic={self.topic.name}"
-        if self.metric is not None:
-            model_str += f", metric={self.metric.name}"
-        if self.geography_type is not None:
-            model_str += f", geography_type={self.geography_type.name}"
-        if self.geography is not None:
-            model_str += f", geography={self.geography.name}"
-        if self.age is not None:
-            model_str += f", age={self.age.name}"
-        if self.stratum is not None:
-            model_str += f", stratum={self.stratum.name}"
-        return model_str
+    def __str__(self) -> str:
+        attributes = ["name=" + self.name]
+
+        for attr in [
+            "theme",
+            "sub_theme",
+            "topic",
+            "metric",
+            "geography_type",
+            "geography",
+            "age",
+            "stratum",
+        ]:
+            value = getattr(self, attr)
+            if value is not None:
+                attributes.append(f"{attr}={value.name}")
+
+        return ", ".join(attributes)
 
     def clean(self):
         super().clean()
-        self._validate_theme_selection()
-        self._validate_subtheme_selection()
-        self._validate_topic_selection()
+        if self.metric:
+            self._validate_metric_selection()
+
+        if self.topic:
+            self._validate_topic_selection()
+
+        if self.sub_theme:
+            self._validate_sub_theme_selection()
+
+        if self.geography:
+            self._validate_geography_selection()
+
         self._check_rbac_permissions_exist()
 
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
 
-    def _validate_theme_selection(self):
-        """Check theme foreign key otherwise Django raises RelatedObjectDoesNotExist"""
-        if not getattr(self, "theme", None):
-            raise AdminFormThemeError
+    def _get_theme_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="theme")
 
-    def _validate_subtheme_selection(self):
-        """Check sub_theme foreign key otherwise Django raises RelatedObjectDoesNotExist"""
-        if not getattr(self, "sub_theme", None) or self.sub_theme.theme != self.theme:
-            raise AdminFormSubthemeAssocThemeError
+    def _get_sub_theme_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="sub_theme")
 
-    def _validate_topic_selection(self):
-        if self.topic:
-            if self.topic.sub_theme.theme != self.theme:
-                raise AdminFormTopicAssocSubthemeError
-            if self.sub_theme.name != self.topic.sub_theme.name:
-                raise AdminFormSubthemeAssocTopicError
+    def _get_topic_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="topic")
+
+    def _get_metric_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="metric")
+
+    def _get_geography_type_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="geography_type")
+
+    def _get_geography_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="geography")
+
+    def _get_age_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="age")
+
+    def _get_stratum_selection(self) -> type[Model]:
+        return self._get_model_selection(model_type="stratum")
+
+    def _get_model_selection(self, model_type: str) -> type[Model]:
+        selected_model = getattr(self, model_type)
+        if selected_model is None:
+            raise NoModelSelectedError(category=model_type)
+        return selected_model
+
+    def _validate_sub_theme_selection(self) -> None:
+        theme = self._get_theme_selection()
+        sub_theme = self._get_sub_theme_selection()
+
+        if sub_theme.theme != theme:
+            raise SelectionInvalidWithParentSelectionError(
+                model=sub_theme, parent_model=theme
+            )
+
+    def _validate_topic_selection(self) -> None:
+        sub_theme = self._get_sub_theme_selection()
+        topic = self._get_topic_selection()
+
+        if topic.sub_theme != sub_theme:
+            raise SelectionInvalidWithParentSelectionError(
+                model=topic, parent_model=sub_theme
+            )
+
+    def _validate_metric_selection(self) -> None:
+        topic = self._get_topic_selection()
+        metric = self._get_metric_selection()
+
+        if metric.topic != topic:
+            raise SelectionInvalidWithParentSelectionError(
+                model=metric, parent_model=topic
+            )
+
+    def _validate_geography_selection(self) -> None:
+        geography_type = self._get_geography_type_selection()
+        geography = self._get_geography_selection()
+        if geography.geography_type != geography_type:
+            raise SelectionInvalidWithParentSelectionError(
+                model=geography, parent_model=geography_type
+            )
 
     def _get_existing_permissions(self) -> RBACPermissionQuerySet:
         """Validates that there are no duplicate permissions with the same fields."""
@@ -169,4 +216,4 @@ class RBACPermission(models.Model):
     def _check_rbac_permissions_exist(self):
         rbac_permissions = self._get_existing_permissions()
         if rbac_permissions.exists():
-            raise AdminFormDuplicatePermissionError
+            raise DuplicatePermissionError
---
+++ tests/unit/metrics/data/models/rbac_models/test_rbac_permissions.py
@@ -0,0 +1,272 @@
+from unittest import mock
+
+import pytest
+
+from metrics.data.models import RBACPermission
+from metrics.data.models.rbac_models.rbac_permissions import (
+    NoModelSelectedError,
+    SelectionInvalidWithParentSelectionError,
+)
+from tests.fakes.factories.metrics.geography_factory import FakeGeographyFactory
+from tests.fakes.factories.metrics.metric_factory import FakeMetricFactory
+from tests.fakes.factories.metrics.rbac_models.rbac_permission import (
+    FakeRBACPermissionFactory,
+)
+from tests.fakes.factories.metrics.sub_theme_factory import FakeSubThemeFactory
+from tests.fakes.factories.metrics.theme_factory import FakeThemeFactory
+from tests.fakes.factories.metrics.topic_factory import FakeTopicFactory
+
+
+DATA_PARAMETERS = {
+    "theme": "infectious_disease",
+    "sub_theme": "respiratory",
+    "topic": "COVID-19",
+    "metric": "COVID-19_cases_casesByDay",
+    "geography": "England",
+    "geography_type": "Nation",
+    "sex": "all",
+    "age": "all",
+    "stratum": "default",
+}
+
+
+class TestRBACPermission:
+    @pytest.fixture(autouse=True, scope="class")
+    def stubbed_rbac_duplicate_permission_check(self):
+        with mock.patch.object(
+            RBACPermission, "_check_rbac_permissions_exist"
+        ) as mocked:
+            yield mocked
+
+    @pytest.mark.parametrize(
+        "included_fields",
+        (
+            {
+                "theme",
+            },
+            {
+                "theme",
+                "sub_theme",
+            },
+            {
+                "theme",
+                "sub_theme",
+                "topic",
+            },
+            {
+                "theme",
+                "sub_theme",
+                "topic",
+                "metric",
+            },
+            {
+                "geography_type",
+            },
+            {
+                "geography_type",
+                "geography",
+            },
+            {
+                "theme",
+                "sub_theme",
+                "topic",
+                "metric",
+                "geography",
+                "geography_type",
+            },
+            {
+                "theme",
+                "sub_theme",
+                "topic",
+                "geography",
+                "geography_type",
+                "age",
+            },
+            {
+                "theme",
+                "sub_theme",
+                "topic",
+                "geography",
+                "geography_type",
+            },
+            {
+                "stratum",
+            },
+        ),
+    )
+    def test_selection_validation_passes_successfully(self, included_fields: set[str]):
+        """
+        Given a set of valid selections
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then the check is validated successfully
+        """
+        # Given
+        permission_values = {
+            k: v for k, v in DATA_PARAMETERS.items() if k in included_fields
+        }
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            **permission_values
+        )
+
+        # When / Then
+        rbac_permission.clean()
+
+    def test_sub_theme_selection_is_invalidated_for_different_theme(self):
+        """
+        Given a selected sub theme for a different theme
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then a `SelectionInvalidWithParentSelectionError` is raised
+        """
+        # Given
+        fake_theme = FakeThemeFactory.build_example_theme(name="extreme_event")
+        fake_sub_theme = FakeSubThemeFactory.build_example_sub_theme(
+            name="weather_alert", theme=fake_theme
+        )
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            theme="infectious_disease",
+        )
+        rbac_permission.sub_theme = fake_sub_theme
+
+        # When / Then
+        with pytest.raises(SelectionInvalidWithParentSelectionError):
+            rbac_permission.clean()
+
+    def test_topic_selection_is_invalidated_for_different_sub_theme(self):
+        """
+        Given a selected topic for a different sub theme
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then a `SelectionInvalidWithParentSelectionError` is raised
+        """
+        # Given
+        fake_theme = FakeThemeFactory.build_example_theme(name="extreme_event")
+        fake_sub_theme = FakeSubThemeFactory.build_example_sub_theme(
+            name="weather_alert", theme=fake_theme
+        )
+        fake_topic = FakeTopicFactory.build_example_topic(
+            name="COVID-19", sub_theme=fake_sub_theme
+        )
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            theme="infectious_disease",
+            sub_theme="respiratory",
+        )
+        rbac_permission.topic = fake_topic
+
+        # When / Then
+        with pytest.raises(SelectionInvalidWithParentSelectionError):
+            rbac_permission.clean()
+
+    def test_topic_selection_is_invalidated_for_no_sub_theme(self):
+        """
+        Given a selected topic without a selected sub theme
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then a `NoModelSelectedError` is raised
+        """
+        # Given
+        fake_theme = FakeThemeFactory.build_example_theme(name="extreme_event")
+        fake_sub_theme = FakeSubThemeFactory.build_example_sub_theme(
+            name="respiratory", theme=fake_theme
+        )
+        fake_topic = FakeTopicFactory.build_example_topic(
+            name="COVID-19", sub_theme=fake_sub_theme
+        )
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            theme="infectious_disease",
+        )
+        rbac_permission.topic = fake_topic
+
+        # When / Then
+        with pytest.raises(NoModelSelectedError):
+            rbac_permission.clean()
+
+    def test_metric_selection_is_invalidated_for_different_topic(self):
+        """
+        Given a selected metric for a different topic
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then a `SelectionInvalidWithParentSelectionError` is raised
+        """
+        # Given
+        fake_metric = FakeMetricFactory.build_example_metric(
+            theme_name="infectious_disease",
+            sub_theme_name="respiratory",
+            topic_name="hMPV",
+            metric_name="hMPV_testing_positivityByWeek",
+        )
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            theme="infectious_disease", sub_theme="respiratory", topic="COVID-19"
+        )
+        rbac_permission.metric = fake_metric
+
+        # When / Then
+        with pytest.raises(SelectionInvalidWithParentSelectionError):
+            rbac_permission.clean()
+
+    def test_metric_selection_is_invalidated_for_no_topic(self):
+        """
+        Given a selected metric without a selected topic
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then a `NoModelSelectedError` is raised
+        """
+        # Given
+        fake_metric = FakeMetricFactory.build_example_metric(
+            theme_name="infectious_disease",
+            sub_theme_name="respiratory",
+            topic_name="hMPV",
+            metric_name="hMPV_testing_positivityByWeek",
+        )
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            theme="infectious_disease",
+            sub_theme="respiratory",
+        )
+        rbac_permission.metric = fake_metric
+
+        # When / Then
+        with pytest.raises(NoModelSelectedError):
+            rbac_permission.clean()
+
+    def test_geography_selection_is_invalidated_for_different_geography_type(self):
+        """
+        Given
+        When
+        Then
+        """
+        # Given
+        fake_geography = FakeGeographyFactory.build_example(
+            geography_name="Bolsover",
+            geography_type_name="Lower Tier Local Authority",
+        )
+
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission(
+            geography_type="Nation",
+            geography="England",
+        )
+        rbac_permission.geography = fake_geography
+
+        # When / Then
+        with pytest.raises(SelectionInvalidWithParentSelectionError):
+            rbac_permission.clean()
+
+    def test_geography_selection_is_invalidated_for_no_geography_type(self):
+        """
+        Given a selected geography without a selected geography type
+        When `clean()` is called from an instance
+            of `RBACPermission`
+        Then a `NoModelSelectedError` is raised
+        """
+        # Given
+        fake_geography = FakeGeographyFactory.build_example(
+            geography_name="England",
+            geography_type_name="Nation",
+        )
+
+        rbac_permission = FakeRBACPermissionFactory.build_rbac_permission()
+        rbac_permission.geography = fake_geography
+
+        # When / Then
+        with pytest.raises(NoModelSelectedError):
+            rbac_permission.clean()
