1259a1f128975ee1b750a4453a32762e93850de1
A-Ashiq <afaan.ashiq2@gmail.com>
2025-04-10 15:54:47 +0100

    Formatting
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- tests/unit/metrics/data/models/rbac_models/test_rbac_permissions.py
+++ tests/unit/metrics/data/models/rbac_models/test_rbac_permissions.py
@@ -95,7 +95,7 @@ class TestRBACPermission:
             },
             {
                 "age",
-            }
+            },
         ),
     )
     def test_selection_validation_passes_successfully(self, included_fields: set[str]):
