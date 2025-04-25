180d47f266e1090f91e36a8fb9ab939e0950a8c2
A-Ashiq <afaan.ashiq2@gmail.com>
2025-04-25 11:42:16 +0100

    Add methods to `TrendsParameters` model to export dict for main & percentage metric queries
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/domain/models/trends.py
+++ metrics/domain/models/trends.py
@@ -7,3 +7,11 @@ class TrendsParameters(HeadlineParameters):
     @property
     def percentage_metric_name(self) -> str:
         return self.percentage_metric
+
+    def to_dict_for_main_metric_query(self):
+        return super().to_dict_for_query()
+
+    def to_dict_for_percentage_metric_query(self):
+        params = super().to_dict_for_query()
+        params["metric_name"] = self.percentage_metric
+        return params
