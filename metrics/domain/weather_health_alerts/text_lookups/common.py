15df677e2dc5859780d00f332bc18222947d384a
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2025-05-08 16:14:01 +0100

    CDD-2577: Update heat alert impact text

--- metrics/domain/weather_health_alerts/text_lookups/common.py
+++ metrics/domain/weather_health_alerts/text_lookups/common.py
@@ -3,7 +3,8 @@ LEVELS_1_TO_4_TEXT = """
 """
 LEVEL_5_TEXT = """
 <p>There is potential for minor impacts across health and social care services, including:</p>
-<ul><li>increased use of health care services by vulnerable people</li>
-<li>greater risk to life of vulnerable people</li></ul>
-<p>But these are not expected.</p>
+<ul><li>increased use of healthcare services by vulnerable people</li>
+<li>greater risk to life of vulnerable people</li>
+<li>increased potential for indoor environments to become very warm</ul>
+<p>but these are not expected.</p>
 """
