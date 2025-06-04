00a9c9909d184bf609ab154d29acc008cd1b80eb
A-Ashiq <afaan.ashiq2@gmail.com>
2025-06-04 11:46:46 +0100

    Reconfigure larger db connection pool for private API workloads
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/api/settings/private_api.py
+++ metrics/api/settings/private_api.py
@@ -15,3 +15,29 @@ else:
             "KEY_PREFIX": "ukhsa",
         }
     }
+
+
+DATABASES = {
+    "default": {
+        "TIME_ZONE": "Europe/London",
+        "ENGINE": "dj_db_conn_pool.backends.postgresql",
+        "NAME": config.POSTGRES_DB,
+        "USER": config.POSTGRES_USER,
+        "PASSWORD": config.POSTGRES_PASSWORD,
+        "HOST": config.POSTGRES_HOST,
+        "PORT": config.POSTGRES_PORT,
+        "POOL_OPTIONS": {
+            "POOL_SIZE": 20,
+            # Number of connections to be persisted at all times
+            "MAX_OVERFLOW": 15,
+            # Additional connections to be created at peak loads
+            "RECYCLE": 8 * 60 * 60,
+            # Time to close and replace connections
+            "TIMEOUT": 60,
+            # Period of time to wait for a connection to become available
+            # during peak loads when all overflow slots are occupied
+            "RESET_ON_RETURN": "rollback",
+            # Reset on every connection return by rollback
+        },
+    }
+}
