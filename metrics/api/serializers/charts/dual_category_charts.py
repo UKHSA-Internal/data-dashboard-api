014690e0bce6e5dfa5ef23645592db6e8d26e641
A-Ashiq <afaan.ashiq2@gmail.com>
2025-06-13 11:20:23 +0100

    Add `DualCategoryChartSegmentSerializer` for request serializiation
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

---
+++ metrics/api/serializers/charts/dual_category_charts.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from metrics.domain.charts.colour_scheme import RGBAChartLineColours
+
+
+class DualCategoryChartSegmentSerializer(serializers.Serializer):
+    primary_field_values = serializers.ListField(
+        child=serializers.CharField(),
+        help_text="List of primary field values for this segment",
+        required=True,
+        allow_empty=False,
+    )
+    secondary_field_value = serializers.CharField(
+        help_text="Secondary field value for this segment",
+        required=True,
+    )
+    color = serializers.ChoiceField(
+        choices=RGBAChartLineColours.choices(),
+        help_text="Color for this segment",
+        required=True,
+    )
+    label = serializers.CharField(
+        help_text="Label for this segment",
+        required=False,
+        allow_blank=True,
+    )
