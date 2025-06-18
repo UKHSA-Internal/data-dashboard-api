8bedf99066c408c6f52291ba241269119a2a672f
A-Ashiq <afaan.ashiq2@gmail.com>
2025-06-18 13:45:01 +0100

    Extend `DualCategoryChartRequestParams` from `BaseChartRequestParams` to gain common chart request param fields
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/domain/models/charts/dual_category_charts.py
+++ metrics/domain/models/charts/dual_category_charts.py
@@ -1,10 +1,6 @@
-from collections.abc import Iterable
-from decimal import Decimal
-from typing import Literal
-
 from pydantic import BaseModel
-from rest_framework.request import Request
 
+from metrics.api.views.charts.common import BaseChartRequestParams
 from metrics.domain.models.charts.segments import SegmentParameters
 
 
@@ -22,25 +18,8 @@ class StaticFields(BaseModel):
     date_to: str
 
 
-class DualCategoryChartRequestParams(BaseModel):
+class DualCategoryChartRequestParams(BaseChartRequestParams):
     chart_type: str
     secondary_category: str
     static_fields: StaticFields
     segments: list[SegmentParameters]
-    file_format: Literal["png", "svg", "jpg", "jpeg"]
-    chart_width: int
-    chart_height: int
-    x_axis: str
-    x_axis_title: str
-    y_axis: str
-    y_axis_title: str
-    y_axis_minimum_value: Decimal | int = 0
-    y_axis_maximum_value: Decimal | int | None = None
-    request: Request | None = None
-
-    class Config:
-        arbitrary_types_allowed = True
-
-    @property
-    def rbac_permissions(self) -> Iterable["RBACPermission"]:
-        return getattr(self.request, "rbac_permissions", [])
