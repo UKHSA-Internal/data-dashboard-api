093390bdc1dbdf8924c07c65d2bb24e0832f004f
A-Ashiq <afaan.ashiq2@gmail.com>
2025-06-23 15:55:43 +0100

    Revert change temporarily to keep git diff limited
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/data/managers/core_models/headline.py
+++ metrics/data/managers/core_models/headline.py
@@ -200,10 +200,10 @@ class CoreHeadlineQuerySet(models.QuerySet):
         )
         queryset = self._filter_for_any_optional_fields(
             queryset=queryset,
-            geography_type_name=geography_type,
-            geography_name=geography,
+            geography_type_name=geography_type_name,
+            geography_name=geography_name,
             geography_code=geography_code,
-            stratum_name=stratum,
+            stratum_name=stratum_name,
             age=age,
             sex=sex,
         )
