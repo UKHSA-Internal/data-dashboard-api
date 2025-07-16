d987282bf53a3a97c647ccf18768d4222befc6d8
A-Ashiq <afaan.ashiq2@gmail.com>
2025-07-16 11:49:34 +0100

    Add tests for happy path when global filter card is available and a filter linked component is being used
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- tests/unit/cms/dynamic_content/test_content_cards.py
+++ tests/unit/cms/dynamic_content/test_content_cards.py
@@ -1,12 +1,8 @@
 import pytest
 from unittest import mock
 from django.core.exceptions import ValidationError
-from wagtail.blocks import StreamValue
 
-from cms.dynamic_content.global_filter.card import GlobalFilterCard
 from cms.dynamic_content.sections import ContentCards
-from cms.topic.models import TopicPage
-from tests.fakes.factories.cms.topic_page_factory import FakeTopicPage
 
 
 class TestContentCards:
@@ -14,8 +10,8 @@ class TestContentCards:
     @mock.patch("wagtail.blocks.StreamBlock.clean")
     def test_clean_delegates_calls_correctly(
         self,
+        spy_parent_clean_method: mock.MagicMock,
         mocked_validate_dependant_blocks: mock.MagicMock,
-        mocked_clean: mock.MagicMock,
     ):
         """
         Given an instance of `ContentCards`
@@ -32,10 +28,11 @@ class TestContentCards:
         )
 
         # When
-        content_cards.clean(value=mocked_blocks)
+        returned_value = content_cards.clean(value=mocked_blocks)
 
         # Then
         mocked_validate_dependant_blocks.assert_called_once()
+        assert returned_value == spy_parent_clean_method.return_value
 
     @pytest.mark.parametrize(
         "filter_linked_block_type",
@@ -69,3 +66,30 @@ class TestContentCards:
             f"The '{readable_name}' is only available when using 'global filter card'."
             in str(validation_error.value)
         )
+
+    @pytest.mark.parametrize(
+        "filter_linked_block_type",
+        (
+            "filter_linked_map",
+            "filter_linked_sub_plot_chart_template",
+            "filter_linked_time_series_chart_template",
+        ),
+    )
+    def test_no_error_is_raised_when_filter_linked_component_used_with_global_filter(
+        self, filter_linked_block_type: str
+    ):
+        """
+        Given an instance of `ContentCards`
+        When `_validate_dependant_blocks` is called,
+            with a valid combination of blocks
+        Then no error is raised.
+        """
+        # Given
+        content_cards = ContentCards()
+        mocked_blocks = [
+            mock.MagicMock(block_type="global_filter_card"),
+            mock.MagicMock(block_type=filter_linked_block_type),
+        ]
+
+        # When / Then
+        content_cards._validate_dependant_blocks(value=mocked_blocks)
