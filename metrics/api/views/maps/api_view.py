f14f4feb2b3018df86938e3575f12374e0afb40c
A-Ashiq <afaan.ashiq2@gmail.com>
2025-07-24 16:29:55 +0100

    Remove spurious print statement
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/api/views/maps/api_view.py
+++ metrics/api/views/maps/api_view.py
@@ -32,7 +32,6 @@ class MapsView(APIView):
     )
     @cache_response(ns2=True)
     def post(cls, request):
-        print('hitting endpoint')
         serializer = MapsRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
