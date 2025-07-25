9cbf3dff93b8c5668f7770dcc449ddfb734e0bcb
A-Ashiq <afaan.ashiq2@gmail.com>
2025-07-25 16:30:46 +0100

    Reimplement `clear_non_reserved_keys()` as `delete_many` at client level since the keys are constructed by the caller, `CacheManagement`
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- caching/private_api/client.py
+++ caching/private_api/client.py
@@ -136,28 +136,16 @@ class InMemoryCacheClient(CacheClient):
         """
         self._cache.clear()
 
-    def clear_non_reserved_keys(self, *, reserved_namespace_key_prefix: str) -> None:
+    def delete_many(self, *, keys: list) -> None:
         """Deletes all keys in the cache which are not within the reserved namespace
 
         Args:
-            reserved_namespace_key_prefix: The prefix associated
-                with reserved namespace entries in the cache.
-                Any keys prefixed with this will not be cleared.
-                All others will be cleared by this method invocation.
-
-        Notes:
-            This allows us to keep hold of
-            expensive, infrequently changing data in the cache
-            like maps data, whilst still allowing the
-            cheaper more frequently changing data types like
-            tables and charts to be cleared.
+            keys: The cache keys to delete
+                within a bulk delete operation
 
         Returns:
             None
 
         """
-        non_reserved_keys = [
-            key for key in self._cache if reserved_namespace_key_prefix not in key
-        ]
-        for key in non_reserved_keys:
+        for key in keys:
             self._cache.pop(key)
