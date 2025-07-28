2312d2aa61a2999ba29fe0dbc19843b24a5bb10a
A-Ashiq <afaan.ashiq2@gmail.com>
2025-07-28 11:16:36 +0100

    Replace hardcoded cache key prefix with value from django app settings
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- caching/private_api/client.py
+++ caching/private_api/client.py
@@ -58,8 +58,10 @@ class CacheClient:
             List of bytes where each byte represents a single key
 
         """
+        key_prefix: str = settings.CACHES["default"]["KEY_PREFIX"]
         low_level_client = self._cache._cache.get_client()
-        return low_level_client.keys("*ukhsa*")
+        pattern = f"*{key_prefix}*"
+        return low_level_client.keys(pattern)
 
     def delete_many(self, keys: list[str]) -> None:
         """Deletes all the provided keys in the cache
