a0fa346ae20127411fb9971e8b847a6ac14af10a
A-Ashiq <afaan.ashiq2@gmail.com>
2025-07-29 21:07:45 +0100

    Rename `create_crawler_to_force_write_in_reserved_namespace()` -> `create_crawler_to_force_write_in_reserved_staging_namespace()`
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- tests/unit/caching/private_api/crawler/private_api_crawler/test_class_methods.py
+++ tests/unit/caching/private_api/crawler/private_api_crawler/test_class_methods.py
@@ -20,16 +20,16 @@ class TestPrivateAPICrawlerCreate:
         assert crawler._internal_api_client.force_refresh
         assert not crawler._internal_api_client.reserved_namespace
 
-    def test_create_crawler_to_force_write_in_reserved_namespace(self):
+    def test_create_crawler_to_force_write_in_reserved_staging_namespace(self):
         """
         Given no pre-existing `InternalAPIClient`
-        When the `create_crawler_to_force_write_in_reserved_namespace`
+        When the `create_crawler_to_force_write_in_reserved_staging_namespace`
             class method is called from the `PrivateAPICrawler` class
         Then the correct object is returned
         """
         # Given / When
         crawler = (
-            PrivateAPICrawler.create_crawler_to_force_write_in_reserved_namespace()
+            PrivateAPICrawler.create_crawler_to_force_write_in_reserved_staging_namespace()
         )
 
         # Then
