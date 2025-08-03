98ed9b0f5ada2a905739e75bb5924f60b7c676ac
A-Ashiq <afaan.ashiq2@gmail.com>
2025-08-03 10:55:17 +0100

    Add type hints and docstrings around `get_upstream_relationships_for_geography()`
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/data/in_memory_models/geography_relationships/handlers.py
+++ metrics/data/in_memory_models/geography_relationships/handlers.py
@@ -19,6 +19,9 @@ from metrics.data.in_memory_models.geography_relationships.utla_to_region import
 class RelationshipsForGeographyTypeNotSupportedError(Exception): ...
 
 
+GEOGRAPHY_RELATIONSHIP_TYPE = dict[str, str | None]
+
+
 def get_united_kingdom():
     return {
         "geography_type": GeographyType.UNITED_KINGDOM.value,
@@ -27,7 +30,7 @@ def get_united_kingdom():
     }
 
 
-def get_relevant_nation(*, geography_code: str):
+def get_relevant_nation(*, geography_code: str) -> GEOGRAPHY_RELATIONSHIP_TYPE:
     try:
         nation_geography_name = REGION_TO_NATION_LOOKUP[geography_code]
         nation_geography_code = NATION_LOOKUP[nation_geography_name]
@@ -42,7 +45,7 @@ def get_relevant_nation(*, geography_code: str):
     }
 
 
-def get_relevant_region(*, geography_code: str) -> dict[str, str]:
+def get_relevant_region(*, geography_code: str) -> GEOGRAPHY_RELATIONSHIP_TYPE:
     try:
         region_geography_name = UTLA_TO_REGION_LOOKUP[geography_code]
         region_geography_code = REGION_LOOKUP[region_geography_name]
@@ -57,11 +60,15 @@ def get_relevant_region(*, geography_code: str) -> dict[str, str]:
     }
 
 
-def _get_upstream_relationships_for_region(*, geography_code: str):
+def _get_upstream_relationships_for_region(
+    *, geography_code: str
+) -> list[GEOGRAPHY_RELATIONSHIP_TYPE]:
     return [get_united_kingdom(), get_relevant_nation(geography_code=geography_code)]
 
 
-def _get_upstream_relationships_for_utla(*, geography_code: str):
+def _get_upstream_relationships_for_utla(
+    *, geography_code: str
+) -> list[GEOGRAPHY_RELATIONSHIP_TYPE]:
     relevant_region: dict[str, str] = get_relevant_region(geography_code=geography_code)
     relevant_nation: dict[str, str] = get_relevant_nation(
         geography_code=relevant_region["geography_code"]
@@ -71,7 +78,28 @@ def _get_upstream_relationships_for_utla(*, geography_code: str):
 
 def get_upstream_relationships_for_geography(
     *, geography_code: str, geography_type: str
-):
+) -> list[GEOGRAPHY_RELATIONSHIP_TYPE] | None:
+    """Gets the upstream/parent geographies which are related to the given geography
+
+    Notes:
+        For a given geography this will get the geographies
+        in which this given geography sits within.
+        For example, the geography code associated with `Hackney`
+        will sit within the `Region` of `London`, which sits
+        within the `Nation` of `England and consequently
+        the `United Kingdom`.
+        As such the upstream relationships for `Hackney` would be:
+        - Region = `London`
+        - Nation = `England`
+        - United Kingdom = United `Kingdom`
+
+    If the geography type is not supported, then `None` will be returned
+
+    Returns:
+        A list of dicts, where each dict represents a relationship.
+        Or None if the geography type is not supported.
+
+    """
 
     if geography_type == GeographyType.NATION.value:
         return [get_united_kingdom()]
