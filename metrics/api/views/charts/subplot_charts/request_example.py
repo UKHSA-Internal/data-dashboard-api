741321da0b13b8855fb459ec8c38096ebe82c04c
A-Ashiq <afaan.ashiq2@gmail.com>
2025-08-18 10:17:01 +0100

    Add `target_threshold` & `target_threshold_label` to example request payload to subplots charts API
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/api/views/charts/subplot_charts/request_example.py
+++ metrics/api/views/charts/subplot_charts/request_example.py
@@ -6,6 +6,8 @@ REQUEST_PAYLOAD_EXAMPLE = {
     "y_axis_title": "vaccination type",
     "y_axis_minimum_value": None,
     "y_axis_maximum_value": None,
+    "target_threshold": 95,
+    "target_threshold_label": "95% target",
     "chart_parameters": {
         "x_axis": "geography",
         "y_axis": "metric",
