2eb8c311b65b7572a1ed71941092e12354d54f5b
A-Ashiq <afaan.ashiq2@gmail.com>
2025-08-20 17:00:32 +0100

    Update sub plots bar chart request example with real sample
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/api/views/charts/subplot_charts/request_example.py
+++ metrics/api/views/charts/subplot_charts/request_example.py
@@ -2,8 +2,8 @@ REQUEST_PAYLOAD_EXAMPLE = {
     "file_format": "svg",
     "chart_height": 300,
     "chart_width": 900,
-    "x_axis_title": "coverage %",
-    "y_axis_title": "vaccination type",
+    "x_axis_title": "Coverage %",
+    "y_axis_title": "Vaccination type",
     "y_axis_minimum_value": None,
     "y_axis_maximum_value": None,
     "target_threshold": 95,
@@ -17,70 +17,120 @@ REQUEST_PAYLOAD_EXAMPLE = {
         "date_to": "2021-12-31",
         "age": "all",
         "sex": "all",
-        "stratum": "test",
+        "stratum": "default"
     },
     "subplots": [
         {
-            "subplot_title": "MMR1 (UTLA)",
+            "subplot_title": "6-in-1 (12 months)",
             "subplot_parameters": {
-                "topic": "MMR1",
-                "metric": "MMR1_coverage_coverageByYear",
-                "stratum": "24m",
+                "topic": "6-in-1",
+                "metric": "6-in-1_coverage_coverageByYear",
+                "stratum": "12m"
             },
             "plots": [
                 {
-                    "label": "Country",
+                    "label": "England",
+                    "geography": "England",
+                    "geography_type": "Nation",
+                    "line_colour": "COLOUR_1_DARK_BLUE"
+                },
+                {
+                    "label": "North East",
+                    "geography": "North East",
+                    "geography_type": "Region",
+                    "line_colour": "COLOUR_2_TURQUOISE"
+                },
+                {
+                    "label": "Darlington",
                     "geography": "Darlington",
                     "geography_type": "Upper Tier Local Authority",
-                    "line_colour": "COLOUR_1_DARK_BLUE",
-                    "stratum": "24m",
-                },
-                {
-                    "label": "Region",
-                    "geography": "Hartlepool",
-                    "geography_type": "Upper Tier Local Authority",
-                    "line_colour": "COLOUR_2_TURQUOISE",
-                    "stratum": "24m",
-                },
-                {
-                    "label": "Local Authority",
-                    "geography": "Stockton-on-Tees",
-                    "geography_type": "Upper Tier Local Authority",
-                    "line_colour": "COLOUR_3_DARK_PINK",
-                    "stratum": "24m",
-                },
-            ],
+                    "line_colour": "COLOUR_3_DARK_PINK"
+                }
+            ]
         },
         {
-            "subplot_title": "MMR1 (Region)",
+            "subplot_title": "6-in-1 (24 months)",
             "subplot_parameters": {
-                "topic": "MMR1",
-                "metric": "MMR1_coverage_coverageByYear",
-                "stratum": "24m",
+                "topic": "6-in-1",
+                "metric": "6-in-1_coverage_coverageByYear",
+                "stratum": "24m"
             },
             "plots": [
                 {
-                    "label": "Country",
-                    "geography": "North West",
-                    "geography_type": "Region",
-                    "line_colour": "COLOUR_1_DARK_BLUE",
-                    "stratum": "24m",
+                    "label": "England",
+                    "geography": "England",
+                    "geography_type": "Nation",
+                    "line_colour": "COLOUR_1_DARK_BLUE"
                 },
                 {
-                    "label": "Region",
-                    "geography": "West Midlands",
+                    "label": "North East",
+                    "geography": "North East",
                     "geography_type": "Region",
-                    "line_colour": "COLOUR_2_TURQUOISE",
-                    "stratum": "24m",
+                    "line_colour": "COLOUR_2_TURQUOISE"
                 },
                 {
-                    "label": "Local Authority",
-                    "geography": "London",
-                    "geography_type": "Region",
-                    "line_colour": "COLOUR_3_DARK_PINK",
-                    "stratum": "24m",
-                },
-            ],
+                    "label": "Darlington",
+                    "geography": "Darlington",
+                    "geography_type": "Upper Tier Local Authority",
+                    "line_colour": "COLOUR_3_DARK_PINK"
+                }
+            ]
         },
-    ],
+        {
+            "subplot_title": "MMR1 (24 months)",
+            "subplot_parameters": {
+                "topic": "MMR1",
+                "metric": "MMR1_coverage_coverageByYear",
+                "stratum": "24m"
+            },
+            "plots": [
+                {
+                    "label": "England",
+                    "geography": "England",
+                    "geography_type": "Nation",
+                    "line_colour": "COLOUR_1_DARK_BLUE"
+                },
+                {
+                    "label": "North East",
+                    "geography": "North East",
+                    "geography_type": "Region",
+                    "line_colour": "COLOUR_2_TURQUOISE"
+                },
+                {
+                    "label": "Darlington",
+                    "geography": "Darlington",
+                    "geography_type": "Upper Tier Local Authority",
+                    "line_colour": "COLOUR_3_DARK_PINK"
+                }
+            ]
+        },
+        {
+            "subplot_title": "MMR1 (5 years)",
+            "subplot_parameters": {
+                "topic": "MMR1",
+                "metric": "MMR1_coverage_coverageByYear",
+                "stratum": "5y"
+            },
+            "plots": [
+                {
+                    "label": "England",
+                    "geography": "England",
+                    "geography_type": "Nation",
+                    "line_colour": "COLOUR_1_DARK_BLUE"
+                },
+                {
+                    "label": "North East",
+                    "geography": "North East",
+                    "geography_type": "Region",
+                    "line_colour": "COLOUR_2_TURQUOISE"
+                },
+                {
+                    "label": "Darlington",
+                    "geography": "Darlington",
+                    "geography_type": "Upper Tier Local Authority",
+                    "line_colour": "COLOUR_3_DARK_PINK"
+                }
+            ]
+        }
+    ]
 }
