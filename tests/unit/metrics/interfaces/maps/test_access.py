f3cace68493ee3dcfa256640a99cd50f5a2d028e
A-Ashiq <afaan.ashiq2@gmail.com>
2025-08-27 18:21:51 +0100

    Add test for using main geography
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- tests/unit/metrics/interfaces/maps/test_access.py
+++ tests/unit/metrics/interfaces/maps/test_access.py
@@ -17,7 +17,7 @@ from metrics.interfaces.maps.access import (
 
 
 class TestMapsInterface:
-    def test_get_related_geography_for_accompanying_point_uses_fallback_geography_from_main_parameters_(
+    def test_get_related_geography_for_accompanying_point_uses_fallback_geography_from_main_parameters(
         self,
     ):
         """
@@ -73,6 +73,64 @@ class TestMapsInterface:
 
         # Then
         assert related_geography == main_geography
+
+    def test_get_related_geography_for_accompanying_point_uses_main_geography_when_not_selected(
+        self,
+    ):
+        """
+        Given a `MapsParameters` model
+            which does define the same geography_type
+            on an accompanying point as the main parameter
+        When `_get_related_geography_for_accompanying_point()` is called
+            from an instance of the `MapsInterface`
+        Then the geography associated with the main parameters
+            is used as the fallback geography.
+        """
+        # Given
+        accompanying_point = MapAccompanyingPoint(
+            label_prefix="abc",
+            label_suffix="",
+            parameters=MapAccompanyingPointOptionalParameters(
+                theme=None,
+                sub_theme=None,
+                topic=None,
+                metric="6-in-1_coverage_oneYearChange",
+                age=None,
+                sex=None,
+                stratum=None,
+                geography_type="Upper Tier Local Authority",
+                geography=None,
+            ),
+        )
+        maps_parameters = MapsParameters(
+            date_from="2020-01-01",
+            date_to="2020-12-31",
+            parameters=MapMainParameters(
+                theme="immunisation",
+                sub_theme="childhood-vaccines",
+                topic="6-in-1",
+                metric="6-in-1_coverage_coverageByYear",
+                stratum="12m",
+                age="all",
+                sex="all",
+                geography_type="Upper Tier Local Authority",
+                geographies=[],
+            ),
+            accompanying_points=[accompanying_point],
+        )
+        maps_interface = MapsInterface(maps_parameters=maps_parameters)
+        main_geography = "Leeds"
+
+        # When
+        selected_geography: str = (
+            maps_interface._get_related_geography_for_accompanying_point(
+                accompanying_point=accompanying_point,
+                main_geography=main_geography,
+            )
+        )
+
+        # Then
+        assert selected_geography == main_geography
 
     def test_fetch_related_geography_by_type_raises_error(self):
         """
