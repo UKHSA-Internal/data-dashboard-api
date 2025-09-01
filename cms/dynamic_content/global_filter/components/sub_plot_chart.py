1fd1e394c266e2afb760d2b6aa704035c04a4250
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-01 11:41:46 +0100

    Add `about` CMS field to filter linked chart templates
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- cms/dynamic_content/global_filter/components/sub_plot_chart.py
+++ cms/dynamic_content/global_filter/components/sub_plot_chart.py
@@ -1,5 +1,6 @@
 from wagtail import blocks
 
+from cms.dashboard.models import AVAILABLE_RICH_TEXT_FEATURES
 from cms.dynamic_content import help_texts
 
 from .common import FilterLinkedComponent
@@ -17,6 +18,12 @@ class FilterLinkedSubPlotChartTemplate(FilterLinkedComponent):
     target_threshold_label = blocks.CharBlock(
         required=False,
         help_text=help_texts.FILTER_LINKED_SUB_PLOT_CHART_TARGET_THRESHOLD_LABEL,
+    )
+    about = blocks.RichTextBlock(
+        required=False,
+        default="",
+        features=AVAILABLE_RICH_TEXT_FEATURES,
+        help_text=help_texts.OPTIONAL_CHART_ABOUT_FIELD,
     )
 
     class Meta:
--- cms/dynamic_content/global_filter/components/time_series_chart.py
+++ cms/dynamic_content/global_filter/components/time_series_chart.py
@@ -1,5 +1,6 @@
 from wagtail import blocks
 
+from cms.dashboard.models import AVAILABLE_RICH_TEXT_FEATURES
 from cms.dynamic_content import help_texts
 
 from .common import FilterLinkedComponent
@@ -9,6 +10,12 @@ class FilterLinkedTimeSeriesChartTemplate(FilterLinkedComponent):
     legend_title = blocks.CharBlock(
         required=True,
         help_text=help_texts.FILTER_LINKED_TIME_SERIES_CHART_LEGEND_TITLE,
+    )
+    about = blocks.RichTextBlock(
+        required=False,
+        default="",
+        features=AVAILABLE_RICH_TEXT_FEATURES,
+        help_text=help_texts.OPTIONAL_CHART_ABOUT_FIELD,
     )
 
     class Meta:
