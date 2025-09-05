c7a815e933d29eaddb329b2e1b85cf4cb1d7c978
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-05 17:10:22 +0100

    Add `SubplotTablesInterface` class to handle orchestration logic -> tables interface handler
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- metrics/interfaces/tables/subplot_tables/access.py
+++ metrics/interfaces/tables/subplot_tables/access.py
@@ -7,25 +7,54 @@ from metrics.interfaces.plots.access import (
 from metrics.interfaces.tables.access import generate_table_for_full_plots
 
 
+class SubplotTablesInterface:
+    def __init__(
+        self,
+        *,
+        request_params: SubplotChartRequestParameters,
+    ):
+        self.request_params = request_params
+
+    def generate_full_plots_for_table(self) -> list[dict]:
+        request_params_per_group: list[ChartRequestParams] = (
+            self.request_params.output_payload_for_tables()
+        )
+
+        result: list[dict] = []
+
+        for request_params in request_params_per_group:
+            try:
+                tabular_data_for_sub_plot: list[dict[str, str]] = (
+                    generate_table_for_full_plots(request_params=request_params)
+                )
+            except (InvalidPlotParametersError, DataNotFoundForAnyPlotError):
+                continue
+            result += tabular_data_for_sub_plot
+
+        if not result:
+            raise DataNotFoundForAnyPlotError
+
+        return result
+
+
 def generate_subplot_table(
     *, subplot_chart_parameters: SubplotChartRequestParameters
 ) -> list[dict]:
-    request_params_per_group: list[ChartRequestParams] = (
-        subplot_chart_parameters.output_payload_for_tables()
+    """Validates and creates tabular output for the given `subplot_chart_parameters
+
+    Args:
+        subplot_chart_parameters: The requested subplot table
+            parameters encapsulated as a model
+
+    Returns:
+        The requested data in tabular format
+
+    Raises:
+        `DataNotFoundForAnyPlotError`: If no subplots
+            returned any data from the underlying queries
+    """
+
+    subplot_tables_interface = SubplotTablesInterface(
+        request_params=subplot_chart_parameters,
     )
-
-    result: list[dict] = []
-
-    for request_params in request_params_per_group:
-        try:
-            tabular_data_for_sub_plot: list[dict[str, str]] = (
-                generate_table_for_full_plots(request_params=request_params)
-            )
-        except (InvalidPlotParametersError, DataNotFoundForAnyPlotError):
-            continue
-        result += tabular_data_for_sub_plot
-
-    if not result:
-        raise DataNotFoundForAnyPlotError
-
-    return result
+    return subplot_tables_interface.generate_full_plots_for_table()
