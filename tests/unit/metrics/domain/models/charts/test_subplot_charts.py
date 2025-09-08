dc85c21e90a63658ef26724bf1b1b98b15ad8310
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-08 12:07:48 +0100

    Add unit test for missing plot within subplot
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- tests/unit/metrics/domain/models/charts/test_subplot_charts.py
+++ tests/unit/metrics/domain/models/charts/test_subplot_charts.py
@@ -69,3 +69,50 @@ class TestSubplotChartRequestParameters:
             for plot in subplot.plots:
                 assert plot.geography == geography
                 assert plot.geography_type == geography_type
+
+    def test_output_payload_for_tables_handles_missing_plot_in_subplot(self):
+        """
+        Given a payload where one subplot is missing a plot
+            for a particular geography
+        When `output_payload_for_tables()` is called from
+            an instance of `SubplotChartRequestParameters`
+        Then the corresponding output `ChartRequestParams`
+            model omits the missing plot
+        """
+        # Given
+        payload = copy.deepcopy(REQUEST_PAYLOAD_EXAMPLE)
+        # Remove the England plot from the 3rd subplot (MMR1 24 months)
+        # so for there is no matching plot for "England" in that subplot
+        third_subplot = payload["subplots"][2]
+        third_subplot["plots"] = [
+            plot for plot in third_subplot["plots"] if plot["geography"] != "England"
+        ]
+
+        serializer = SubplotChartRequestSerializer(data=payload)
+        serializer.is_valid(raise_exception=True)
+        params: SubplotChartRequestParameters = serializer.to_models(request=None)
+
+        # When
+        output: list[ChartRequestParams] = params.output_payload_for_tables()
+
+        # Then
+        # We still expect 3 groups i.e. England, North East, Darlington
+        assert len(output) == 3
+
+        england_group = output[0]
+        # Originally there were 4 plots in each subplot
+        # But for the "England" subplot there will now only be 3 plots
+        assert len(england_group.plots) == 3
+        expected_labels_for_england_subplot = [
+            "6-in-1 (12 months)",
+            "6-in-1 (24 months)",
+            # "MMR1 (24 months)" is missing for England in this case
+            "MMR1 (5 years)",
+        ]
+        assert [
+            plot.label for plot in england_group.plots
+        ] == expected_labels_for_england_subplot
+
+        # The other groups (North East & Darlington) still have all 4 plots
+        assert len(output[1].plots) == 4
+        assert len(output[2].plots) == 4
