ed2feb52fab7012a0b0d3e160afc9415d89cf5c9
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-08 15:56:33 +0100

    Add optional `about` field to filter linked map CMS component
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- cms/dynamic_content/global_filter/components/map.py
+++ cms/dynamic_content/global_filter/components/map.py
@@ -1,6 +1,18 @@
+from wagtail import blocks
+
+from cms.dashboard.models import AVAILABLE_RICH_TEXT_FEATURES
+from cms.dynamic_content import help_texts
+
 from .common import FilterLinkedComponent
 
 
 class FilterLinkedMap(FilterLinkedComponent):
+    about = blocks.RichTextBlock(
+        required=False,
+        default="",
+        features=AVAILABLE_RICH_TEXT_FEATURES,
+        help_text=help_texts.OPTIONAL_MAP_ABOUT_FIELD,
+    )
+
     class Meta:
         icon = "site"
