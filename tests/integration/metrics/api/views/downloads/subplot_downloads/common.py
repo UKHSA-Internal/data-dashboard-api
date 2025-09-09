73470b88c93ddaddfaf09b85f6e79256c5ae8658
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-09 15:27:19 +0100

    Add integration test for headline CSV sub plot download
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

--- tests/integration/metrics/api/views/downloads/subplot_downloads/common.py
+++ tests/integration/metrics/api/views/downloads/subplot_downloads/common.py
@@ -1,12 +1,13 @@
+from tests.factories.metrics.headline import CoreHeadlineFactory
 from tests.factories.metrics.time_series import CoreTimeSeriesFactory
 
 
-def create_example_core_timeseries()  -> None:
+def create_example_core_timeseries() -> None:
     CoreTimeSeriesFactory.create_record(
         theme_name="immunisation",
         sub_theme_name="childhood-vaccines",
         topic_name="6-in-1",
-        metric_name=f"6-in-1_coverage_coverageByYear",
+        metric_name="6-in-1_coverage_coverageByYear",
         stratum_name="12m",
         date="2021-03-31",
         geography_name="Darlington",
@@ -17,7 +18,7 @@ def create_example_core_timeseries()  -> None:
         theme_name="immunisation",
         sub_theme_name="childhood-vaccines",
         topic_name="6-in-1",
-        metric_name=f"6-in-1_coverage_coverageByYear",
+        metric_name="6-in-1_coverage_coverageByYear",
         stratum_name="12m",
         date="2021-03-31",
         geography_name="North East",
@@ -28,7 +29,7 @@ def create_example_core_timeseries()  -> None:
         theme_name="immunisation",
         sub_theme_name="childhood-vaccines",
         topic_name="6-in-1",
-        metric_name=f"6-in-1_coverage_coverageByYear",
+        metric_name="6-in-1_coverage_coverageByYear",
         stratum_name="12m",
         date="2021-03-31",
         geography_name="England",
@@ -40,7 +41,7 @@ def create_example_core_timeseries()  -> None:
         theme_name="immunisation",
         sub_theme_name="childhood-vaccines",
         topic_name="6-in-1",
-        metric_name=f"6-in-1_coverage_coverageByYear",
+        metric_name="6-in-1_coverage_coverageByYear",
         stratum_name="24m",
         date="2021-03-31",
         geography_name="Darlington",
@@ -51,7 +52,7 @@ def create_example_core_timeseries()  -> None:
         theme_name="immunisation",
         sub_theme_name="childhood-vaccines",
         topic_name="6-in-1",
-        metric_name=f"6-in-1_coverage_coverageByYear",
+        metric_name="6-in-1_coverage_coverageByYear",
         stratum_name="24m",
         date="2021-03-31",
         geography_name="North East",
@@ -62,7 +63,7 @@ def create_example_core_timeseries()  -> None:
         theme_name="immunisation",
         sub_theme_name="childhood-vaccines",
         topic_name="6-in-1",
-        metric_name=f"6-in-1_coverage_coverageByYear",
+        metric_name="6-in-1_coverage_coverageByYear",
         stratum_name="24m",
         date="2021-03-31",
         geography_name="England",
@@ -93,3 +94,42 @@ def create_example_core_timeseries()  -> None:
         metric_value=84,
     )
     # There is intentionally no corresponding record for England / MMR1 / 24m
+
+
+def create_example_core_headlines() -> None:
+    CoreHeadlineFactory.create_record(
+        theme="infectious_disease",
+        sub_theme="respiratory",
+        topic="COVID-19",
+        metric="COVID-19_headline_tests_7DayTotal",
+        stratum="default",
+        period_start="2021-03-31",
+        period_end="2021-03-31",
+        geography="Darlington",
+        geography_type="Upper Tier Local Authority",
+        metric_value=86,
+    )
+    CoreHeadlineFactory.create_record(
+        theme="infectious_disease",
+        sub_theme="respiratory",
+        topic="COVID-19",
+        metric="COVID-19_headline_tests_7DayTotal",
+        stratum="default",
+        period_start="2021-03-31",
+        period_end="2021-03-31",
+        geography="North East",
+        geography_type="Region",
+        metric_value=85,
+    )
+    CoreHeadlineFactory.create_record(
+        theme="infectious_disease",
+        sub_theme="respiratory",
+        topic="COVID-19",
+        metric="COVID-19_headline_tests_7DayTotal",
+        stratum="default",
+        period_start="2021-03-31",
+        period_end="2021-03-31",
+        geography="England",
+        geography_type="Nation",
+        metric_value=90,
+    )
--- tests/integration/metrics/api/views/downloads/subplot_downloads/test_csv.py
+++ tests/integration/metrics/api/views/downloads/subplot_downloads/test_csv.py
@@ -11,6 +11,7 @@ from metrics.api.views.downloads.subplot_downloads.request_example import (
 )
 from tests.integration.metrics.api.views.downloads.subplot_downloads.common import (
     create_example_core_timeseries,
+    create_example_core_headlines,
 )
 
 
@@ -20,7 +21,7 @@ class TestDownloadsSubplotView:
         return "/api/downloads/subplot/v1/"
 
     @pytest.mark.django_db
-    def test_returns_correct_data_for_csv_download(self):
+    def test_csv_download_for_timeseries_data(self):
         """
         Given a valid payload to create a JSON subplot download
         When a POST request is made to `/api/downloads/subplot/v1/`
@@ -190,6 +191,159 @@ class TestDownloadsSubplotView:
                 "year": "2023",
                 "date": "2021-03-31",
                 "metric_value": "93.0000",
+                "in_reporting_delay_period": "False",
+            },
+        ]
+
+        assert len(rows) == len(expected_rows)
+
+    @pytest.mark.django_db
+    def test_csv_download_for_headline_data(self):
+        """
+        Given a valid payload to create a JSON subplot download
+        When a POST request is made to `/api/downloads/subplot/v1/`
+        Then the correct data is returned in the response
+        """
+        # Given
+        client = APIClient()
+        valid_payload = {
+            "file_format": "csv",
+            "chart_height": 300,
+            "chart_width": 900,
+            "chart_parameters": {
+                "x_axis": "geography",
+                "y_axis": "metric",
+                "theme": "infectious_disease",
+                "sub_theme": "respiratory",
+                "date_from": "2021-01-31",
+                "date_to": "2021-12-31",
+                "age": "all",
+                "sex": "all",
+                "stratum": "default",
+                "metric_value_ranges": [
+                    {"start": 0, "end": 80},
+                    {"start": 80.0001, "end": 85},
+                    {"start": 85.0001, "end": 90},
+                    {"start": 90.0001, "end": 95},
+                    {"start": 95.0001, "end": 100},
+                ],
+            },
+            "subplots": [
+                {
+                    "subplot_title": "COVID-19 tests",
+                    "subplot_parameters": {
+                        "topic": "COVID-19",
+                        "metric": "COVID-19_headline_tests_7DayTotal",
+                        "stratum": "default"
+                    },
+                    "plots": [
+                        {
+                            "label": "England",
+                            "geography": "England",
+                            "geography_type": "Nation",
+                            "line_colour": "COLOUR_1_DARK_BLUE",
+                        },
+                        {
+                            "label": "North East",
+                            "geography": "North East",
+                            "geography_type": "Region",
+                            "line_colour": "COLOUR_2_TURQUOISE",
+                        },
+                        {
+                            "label": "Darlington",
+                            "geography": "Darlington",
+                            "geography_type": "Upper Tier Local Authority",
+                            "line_colour": "COLOUR_3_DARK_PINK",
+                        },
+                    ],
+                },
+            ],
+        }
+        create_example_core_headlines()
+
+        # When
+        response: Response = client.post(
+            path=self.path, data=valid_payload, format="json"
+        )
+
+        # Then
+        csv_reader = csv.DictReader(io.StringIO(response.text))
+        rows = list(csv_reader)
+
+        expected_rows = [
+            {
+                "theme": "infectious_disease",
+                "sub_theme": "respiratory",
+                "topic": "COVID-19",
+                "geography_type": "Nation",
+                "geography": "England",
+                "metric": "COVID-19_headline_tests_7DayTotal",
+                "sex": "all",
+                "age": "all",
+                "stratum": "default",
+                "year": "2023",
+                "date": "2021-03-31",
+                "metric_value": "88.0000",
+                "in_reporting_delay_period": "False",
+            },
+            {
+                "theme": "",
+                "sub_theme": "",
+                "topic": "",
+                "geography_type": "",
+                "geography": "",
+                "metric": "",
+                "sex": "",
+                "age": "",
+                "stratum": "",
+                "year": "",
+                "date": "",
+                "metric_value": "",
+                "in_reporting_delay_period": "",
+            },
+            {
+                "theme": "infectious_disease",
+                "sub_theme": "respiratory",
+                "topic": "COVID-19",
+                "geography_type": "Region",
+                "geography": "North East",
+                "metric": "COVID-19_headline_tests_7DayTotal",
+                "sex": "all",
+                "age": "all",
+                "stratum": "default",
+                "year": "2023",
+                "date": "2021-03-31",
+                "metric_value": "85.0000",
+                "in_reporting_delay_period": "False",
+            },
+            {
+                "theme": "",
+                "sub_theme": "",
+                "topic": "",
+                "geography_type": "",
+                "geography": "",
+                "metric": "",
+                "sex": "",
+                "age": "",
+                "stratum": "",
+                "year": "",
+                "date": "",
+                "metric_value": "",
+                "in_reporting_delay_period": "",
+            },
+            {
+                "theme": "infectious_disease",
+                "sub_theme": "respiratory",
+                "topic": "COVID-19",
+                "geography_type": "Upper Tier Local Authority",
+                "geography": "Darlington",
+                "metric": "COVID-19_headline_tests_7DayTotal",
+                "sex": "all",
+                "age": "all",
+                "stratum": "default",
+                "year": "2023",
+                "date": "2021-03-31",
+                "metric_value": "86.0000",
                 "in_reporting_delay_period": "False",
             },
         ]
