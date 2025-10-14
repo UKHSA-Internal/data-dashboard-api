75697c25c1291599d69e95cc01f0ee3627c1403a
Josh Humphries <josh.humphries@ukhsa.gov.uk>
2025-10-14 12:20:38 +0100

    fix: use the validation metrics interface not the ingestion metrics inteface
    This was a leftover from the previous refactoring which I missed.

--- validation/metrics_interface/interface.py
+++ validation/metrics_interface/interface.py
@@ -1,3 +1,4 @@
+from metrics.data.enums import TimePeriod
 from metrics.domain.common.utils import DataSourceFileType
 
 
@@ -5,3 +6,7 @@ class MetricsAPIInterface:
     @staticmethod
     def get_datasource_enum() -> DataSourceFileType:
         return DataSourceFileType
+
+    @staticmethod
+    def get_time_period_enum() -> TimePeriod:
+        return TimePeriod
