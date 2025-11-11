09a460197e789343c20bb9cb589516712cc219bd
A-Ashiq <afaan.ashiq2@gmail.com>
2025-11-11 12:21:16 +0000

    Add `get_chrome` management command to wrap around `kaleido.get_chrome_sync()`
    Signed-off-by: A-Ashiq <afaan.ashiq2@gmail.com>

---
+++ metrics/interfaces/management/commands/get_chrome.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+from kaleido import get_chrome_sync
+
+
+class Command(BaseCommand):
+    @classmethod
+    def handle(cls, *args, **options):
+        get_chrome_sync(verbose=True)
