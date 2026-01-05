75c386fd045c6e9507191f08a11c666660476311
Aidan Skinner <aidan@skinner.me.uk>
2026-01-05 11:58:42 +0000

    BE: Check confidence intervals values in order to determine if to hide/show upper and lower confidence values
    Plots gain confidence_interval flag to determine if they should be displayed and
    a confidence_colour property to specify the colour they should be drawn in.

--- metrics/interfaces/plots/access.py
+++ metrics/interfaces/plots/access.py
@@ -153,11 +153,9 @@ class PlotsInterface:
             plot_params["theme"] = topic.sub_theme.theme.name
             plot_params["sub_theme"] = topic.sub_theme.name
 
-        if (
-            # Sometimes this is a Subplots request which doesn't support these
-            hasattr(self.chart_request_params, "confidence_intervals")
-            and self.chart_request_params.confidence_intervals
-        ):
+        # Sometimes this is a Subplots request which doesn't support confidence intervals
+        confidence_intervals = getattr(self.chart_request_params, "confidence_intervals", False)
+        if confidence_intervals:
             plot_params["fields_to_export"].append("upper_confidence")
             plot_params["fields_to_export"].append("lower_confidence")
 
@@ -220,10 +218,8 @@ class PlotsInterface:
         plot_parameters.x_axis = self.chart_request_params.x_axis
         plot_parameters.y_axis = self.chart_request_params.y_axis
 
-        if (
-            # Sometimes this is a Subplots request which doesn't support these
-            hasattr(self.chart_request_params, "confidence_colour")
-        ):
+        confidence_colour = getattr(self.chart_request_params, "confidence_colour", None)
+        if confidence_colour:
             plot_parameters.confidence_colour = (
                 self.chart_request_params.confidence_colour
             )
