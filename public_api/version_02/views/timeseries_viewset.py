c3ba28e6757d1fe3b2ff36c712277e41c717fbcc
Taiwo Kareem <taiwo.kareem36@gmail.com>
2026-02-11 11:39:29 +0000

    SPIKE: Public API wildcard performance investigation

--- public_api/version_02/views/timeseries_viewset.py
+++ public_api/version_02/views/timeseries_viewset.py
@@ -1,3 +1,4 @@
+import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework import pagination, viewsets
@@ -101,3 +102,89 @@ class APITimeSeriesViewSetV2(viewsets.ReadOnlyModelViewSet):
             metric=self.kwargs["metric"],
             restrict_to_public=True,
         )
+
+
+class APITimeSeriesFilterSetv3(django_filters.FilterSet):
+    geography = django_filters.CharFilter(field_name="geography", lookup_expr="iexact")
+    geography_type = django_filters.CharFilter(
+        field_name="geography_type", lookup_expr="iexact"
+    )
+
+    class Meta:
+        model = MetricsPublicAPIInterface.get_api_timeseries_model()
+        fields = [
+            "theme",
+            "sub_theme",
+            "topic",
+            "geography_type",
+            "geography",
+            "metric",
+            "stratum",
+            "sex",
+            "age",
+            "year",
+            "epiweek",
+            "date",
+            "in_reporting_delay_period",
+        ]
+
+
+@extend_schema(tags=[PUBLIC_API_TAG])
+class APITimeSeriesViewSetV3(viewsets.ReadOnlyModelViewSet):
+    """
+    This endpoint will provide the full timeseries of a slice of data.
+
+    There are only optional query parameters:
+
+    Note that by default, results are paginated by a page size of 100
+
+    This page size can be changed using the *page_size* parameter.
+    The maximum supported page size is **365**.
+
+    ---
+
+    The optional query parameters are as follows in order from first to last:
+
+    - `theme` - The largest topical subgroup e.g. **infectious_disease**
+
+    - `sub_theme` - A topical subgroup e.g. **respiratory**
+
+    - `topic` - The name of the topic e.g. **COVID-19**
+
+    - `geography_type` - The type of the geography type e.g. **Nation**
+
+    - `geography` - The name of the geography associated with metric  e.g. **London**
+
+    - `metric` - The name of the metric being queried for e.g. **COVID-19_deaths_ONSByDay**
+
+    - `stratum` - Smallest subgroup a metric can be broken down into e.g. ethnicity, testing pillar
+
+    - `age` - Smallest subgroup a metric can be broken down into e.g. **15_44** for the age group of 15-44 years
+
+    - `sex` - Patient gender e.g. **f** for Female or **all** for all genders
+
+    - `year` - Epi year of the metrics value (important for annual metrics) e.g. **2020**
+
+    - `month` - Epi month of the metric value (important for monthly metrics) e.g. **12**
+
+    - `epiweek` - Epi week of the metric value (important for weekly metrics) e.g. **30**
+
+    - `date` - The date which this metric value was recorded in the format **YYYY-MM-DD** e.g. **2020-07-20**
+
+    - `in_reporting_delay_period` - A boolean indicating whether the data point is considered to be subject
+    to retrospective updates.
+
+    """
+
+    permission_classes = []
+    name = "API Time Series Version 3"
+    queryset = (
+        MetricsPublicAPIInterface.get_api_timeseries_model()
+        .objects.all()
+        .order_by("date")
+    )
+    serializer_class = APITimeSeriesListSerializerv2
+    pagination_class = APITimeSeriesPaginationv2
+    page_size = 100
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = APITimeSeriesFilterSetv3
