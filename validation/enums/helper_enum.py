f5525ee9b97ac51de017fb31e396bff4b3953bb7
Kathryn Dale <kathryn.dale@burendo.com>
2026-02-24 12:04:08 +0000

    Downstream geography lookup function

--- validation/enums/helper_enum.py
+++ validation/enums/helper_enum.py
@@ -3,3 +3,5 @@ from enum import Enum
 class BaseEnum(Enum):
     def return_list(self):
         return [e.value for e in self.value]
+    def return_name_list(self):
+        return [e.name for e in self.value]
