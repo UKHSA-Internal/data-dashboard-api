ec5e2f3c575ebc18a024173e75ee7004e0f37fa1
Kathryn Dale <kathryn.dale@burendo.com>
2026-02-24 12:00:44 +0000

    Update geography enums

--- validation/enums/__init__.py
+++ validation/enums/__init__.py
@@ -1,7 +1,7 @@
 from .geographies_enums import GeographyType
-from .ukhsa_region_enums import UKHSARegionUTLAs
-from .utla_ltla_enums import UTLAtoLTLA
-from .gov_office_region_enums import GovOfficeRegionUTLAs
+from .ukhsa_region_enums import UKHSARegionUTLAs, UKHSARegion
+from .utla_ltla_enums import UTLAtoLTLA, UTLAs, LTLAs
+from .gov_office_region_enums import GovOfficeRegionUTLAs, GovOfficeRegion
 from .helper_enum import BaseEnum
 from .theme_and_topic_enums import (
     ParentTheme,
