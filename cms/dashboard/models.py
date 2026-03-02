819961c7cd2edc731c07793b29ae97af381685a7
Kathryn Dale <kathryn.dale@burendo.com>
2026-03-02 16:31:44 +0000

    linting

--- cms/dashboard/models.py
+++ cms/dashboard/models.py
@@ -39,7 +39,6 @@ class DataClassificationLevels(models.TextChoices):
     SECRET = "secret"  # nosec #noqa: S105
     TOP_SECRET = "top_secret"  # nosec #noqa: S105
 
-
 class UKHSAPage(Page):
     """Abstract base class for all page types
 
