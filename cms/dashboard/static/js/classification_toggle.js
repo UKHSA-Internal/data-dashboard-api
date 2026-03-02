1a85fdeec426b7457f6941ded1a065a1726bbb9c
Kathryn Dale <kathryn.dale@burendo.com>
2026-03-02 16:33:43 +0000

    add comment

--- cms/dashboard/static/js/classification_toggle.js
+++ cms/dashboard/static/js/classification_toggle.js
@@ -1,23 +1,28 @@
+;(function () {
+  function toggleClassification() {
+    /*
+        When the is_public box is checked, this will clear any selected page_classification, 
+        and disable the field. If the is_public box is then unchecked, it will re-enable the field
+         */
+    const isPublicCheckbox = document.querySelector('input[name="is_public"]')
+    const classificationField = document.querySelector(
+      'select[name="page_classification"]',
+    )
 
-(function() {
-    function toggleClassification() {
-        const isPublicCheckbox = document.querySelector('input[name="is_public"]');
-        const classificationField = document.querySelector('select[name="page_classification"]');
+    if (!isPublicCheckbox || !classificationField) return
 
-        if (!isPublicCheckbox || !classificationField) return;
-
-        if (isPublicCheckbox.checked) {
-            classificationField.value = "";
-            classificationField.disabled = true;
-        } else {
-            classificationField.disabled = false;
-        }
+    if (isPublicCheckbox.checked) {
+      classificationField.value = ""
+      classificationField.disabled = true
+    } else {
+      classificationField.disabled = false
     }
+  }
 
-    document.addEventListener("DOMContentLoaded", toggleClassification);
-    document.addEventListener("change", function(e) {
-        if (e.target.name === "is_public") {
-            toggleClassification();
-        }
-    });
-})();
+  document.addEventListener("DOMContentLoaded", toggleClassification)
+  document.addEventListener("change", function (e) {
+    if (e.target.name === "is_public") {
+      toggleClassification()
+    }
+  })
+})()
