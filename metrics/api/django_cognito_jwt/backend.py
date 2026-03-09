5bf602d9f4814ac472c861f62266ef284ecff6d1
Matt Reynolds <18287679+mattjreynolds@users.noreply.github.com>
2026-03-09 09:16:49 +0000

    Resolve linting issues with jwt

--- metrics/api/django_cognito_jwt/backend.py
+++ metrics/api/django_cognito_jwt/backend.py
@@ -10,6 +10,7 @@ from rest_framework.authentication import BaseAuthentication, get_authorization_
 from .validator import TokenError, TokenValidator
 
 logger = logging.getLogger(__name__)
+VALID_AUTH_HEADER_LENGTH = 2
 
 
 class JSONWebTokenAuthentication(BaseAuthentication):
@@ -26,17 +27,19 @@ class JSONWebTokenAuthentication(BaseAuthentication):
             token_validator = self.get_token_validator(request)
             jwt_payload = token_validator.validate(jwt_token)
         except TokenError:
-            raise exceptions.AuthenticationFailed()
+            raise exceptions.AuthenticationFailed from None
 
-        USER_MODEL = self.get_user_model()
-        user = USER_MODEL.objects.get_or_create_for_cognito(jwt_payload)
+        user_model = self.get_user_model()
+        user = user_model.objects.get_or_create_for_cognito(jwt_payload)
         return (user, jwt_token)
 
-    def get_user_model(self):
+    @staticmethod
+    def get_user_model():
         user_model = getattr(settings, "COGNITO_USER_MODEL", settings.AUTH_USER_MODEL)
         return django_apps.get_model(user_model, require_ready=False)
 
-    def get_jwt_token(self, request):
+    @staticmethod
+    def get_jwt_token(request):
         auth = get_authorization_header(request).split()
         if not auth or force_str(auth[0].lower()) != "bearer":
             return None
@@ -44,7 +47,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
         if len(auth) == 1:
             msg = _("Invalid Authorization header. No credentials provided.")
             raise exceptions.AuthenticationFailed(msg)
-        elif len(auth) > 2:
+        if len(auth) > VALID_AUTH_HEADER_LENGTH:
             msg = _(
                 "Invalid Authorization header. Credentials string "
                 "should not contain spaces."
@@ -53,14 +56,16 @@ class JSONWebTokenAuthentication(BaseAuthentication):
 
         return auth[1]
 
-    def get_token_validator(self, request):
+    @staticmethod
+    def get_token_validator(request):
         return TokenValidator(
             settings.COGNITO_AWS_REGION,
             settings.COGNITO_USER_POOL,
             settings.COGNITO_AUDIENCE,
         )
 
-    def authenticate_header(self, request):
+    @staticmethod
+    def authenticate_header(request):
         """
         Method required by the DRF in order to return 401 responses for authentication failures, instead of 403.
         More details in https://www.django-rest-framework.org/api-guide/authentication/#custom-authentication.
