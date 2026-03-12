263420bc1534f33826898d75503358a4b215d16a
Kathryn Dale <kathryn.dale@burendo.com>
2026-03-12 16:13:48 +0000

    Update auth_content migration

--- auth_content/models.py
+++ auth_content/models.py
@@ -4,14 +4,16 @@ from django.db import models
 from django.db import models
 from wagtail.admin.panels import FieldPanel
 
-from validation.enums.theme_and_topic_enums import ParentTheme
+from validation.enums.theme_and_topic_enums import ChildTheme, ParentTheme
 
 
 class PermissionSet(models.Model):
     theme = models.CharField(
         choices=[(e.value, e.name.replace("_", " ").title()) for e in ParentTheme]
     )
-    sub_theme = models.CharField(max_length=255)
+    sub_theme = models.CharField(
+        choices=ChildTheme["INFECTIOUS_DISEASE"].return_tuple_list()
+    )
     topic = models.CharField(max_length=255)
     metric = models.CharField(max_length=255)
     geography_type = models.CharField(max_length=255)
