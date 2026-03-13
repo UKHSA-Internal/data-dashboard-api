663444afbe584d3b02caa6dceb42e26696fb4d10
Taiwo Kareem <taiwo.kareem36@gmail.com>
2026-03-13 11:07:20 +0000

    CMS: Update test and migration

--- cms/home/migrations/0028_landingpage_page_description_and_more.py
+++ cms/home/migrations/0028_landingpage_page_description_and_more.py
@@ -56,7 +56,7 @@ class Migration(migrations.Migration):
                 ),
                 ("title", models.CharField(max_length=255)),
                 ("url", models.URLField(max_length=400, verbose_name="URL")),
-                ("body", wagtail.fields.RichTextField(blank=True, null=True)),
+                ("body", wagtail.fields.RichTextField()),
                 (
                     "page",
                     modelcluster.fields.ParentalKey(
