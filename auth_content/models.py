2c01f79fe168cec39d78c014dc830eedc2850e82
Luke Towell <luke.towell@burendo.com>
2026-03-18 15:21:25 +0000

    Wire up geographyType model

--- auth_content/models.py
+++ auth_content/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db import models
 from wagtail.admin.panels import FieldPanel
 
+from validation.enums.geographies_enums import GeographyType
 from validation.enums.theme_and_topic_enums import ChildTheme, ParentTheme, Topic
 
 
@@ -50,6 +51,28 @@ def get_sub_theme_child_map():
     return sub_theme_mapping
 
 
+def get_geography_type_geographies_map():
+    """Returns an object of all parent to child mappings
+    e.g.
+    {
+        infectious_disease: [vaccine_preventable, respiratory ....],
+        extreme_event: [weather_alert, mortality_report...]
+        ...
+    }
+
+    """
+
+    geographies_mapping = {}
+    for geographyType in GeographyType:
+        # It has been assumed for now that validation and ingestion will catch if any parent name are not used in ChildTheme
+        geographies_mapping[geographyType.value] = [
+            geographyType.name].return_tuple_list()
+
+    print(geographies_mapping)
+    geographies_mapping = {}
+    return geographies_mapping
+
+
 class PermissionSet(models.Model):
     theme = models.CharField(
         choices=[(e.value, e.name.replace("_", " ").title())
@@ -58,7 +81,8 @@ class PermissionSet(models.Model):
     sub_theme = models.CharField(max_length=255, choices=[])
     topic = models.CharField(max_length=255, choices=[])
     metric = models.CharField(max_length=255, choices=[])
-    geography_type = models.CharField(max_length=255, choices=[])
+    geography_type = models.CharField(max_length=255, choices=[(
+        e.value, e.value.replace("_", " ")) for e in GeographyType])
     geography = models.CharField(max_length=255, choices=[])
 
     panels = [
