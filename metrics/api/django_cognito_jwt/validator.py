95ac503ebefe6e8d07a368658fb244509ec6e179
Matt Reynolds <18287679+mattjreynolds@users.noreply.github.com>
2026-03-23 20:49:24 +0000

    Ignore invalid sonar rule

--- metrics/api/django_cognito_jwt/validator.py
+++ metrics/api/django_cognito_jwt/validator.py
@@ -69,7 +69,7 @@ class TokenValidator:
         }
 
         logger.debug("JWT - %s", params)
-        token_payload = jwt.decode(token, options={"verify_signature": False})
+        token_payload = jwt.decode(token, options={"verify_signature": False})  # noqa: S5659
         logger.debug("JWT decoded - %s", token_payload)
 
         if "aud" in token_payload:
