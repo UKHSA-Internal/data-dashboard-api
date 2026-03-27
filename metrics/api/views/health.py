c67b357f52192fc07a487ff972c14e84c49afcd4
Pete Milne <petermilne@hotmail.com>
2026-03-27 11:46:08 +0000

    feat: add telemetry via health endpoint

--- metrics/api/views/health.py
+++ metrics/api/views/health.py
@@ -3,17 +3,24 @@ from http import HTTPStatus
 from django.http import HttpResponse
 from rest_framework.views import APIView
 
-from metrics.interfaces.health.probes import HealthProbeManagement
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 
 class HealthView(APIView):
     permission_classes = []
 
-    @staticmethod
-    def get(*args, **kwargs):
-        """This health probe can be used to determine whether the service is ready."""
-        return HttpResponse(status=HTTPStatus.OK.value)
+    def get(self, request, *args, **kwargs):
+        """Health probe + optional metrics"""
 
+        # 👇 your hidden metrics endpoint
+        if request.GET.get("metrics") == "1":
+            return HttpResponse(
+                generate_latest(),
+                content_type=CONTENT_TYPE_LATEST,
+                status=HTTPStatus.OK.value,
+            )
+
+        return HttpResponse(status=HTTPStatus.OK.value)
 
 class InternalHealthView(APIView):
     permission_classes = []
