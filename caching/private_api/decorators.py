d984a5bd3932ade34b54e31a89a96aba41cf1f4a
Kathryn Dale <kathryn.dale@burendo.com>
2026-04-09 11:27:57 +0100

    Update decorator to reduce duplicate calls

--- caching/private_api/decorators.py
+++ caching/private_api/decorators.py
@@ -73,10 +73,7 @@ def cache_response(
 
 
 def _check_if_valid_non_public_request(request) -> bool:
-    auth = backend.JSONWebTokenAuthentication()
-    result = auth.authenticate(request)
-
-    return result is not None
+    return request.auth is not None
 
 
 def _retrieve_response_from_cache_or_calculate(
