b10c77be31b5f398835e42c966008b34b75fa4cf
David Logie <david.logie@ukha.gov.uk>
2026-04-09 09:23:32 +0100

    CDD-3119 Beef up the SimpleMenu serializer tests.

--- tests/unit/cms/snippets/serializers/test_menu.py
+++ tests/unit/cms/snippets/serializers/test_menu.py
@@ -2,8 +2,8 @@ from cms.snippets.models.menu_builder import Menu, SimpleMenu
 from cms.snippets.serializers import (
     MenuResponseSerializer,
     MenuSerializer,
-    SimpleMenuSerializer,
     SimpleMenuResponseSerializer,
+    SimpleMenuSerializer,
 )
 from tests.fakes.managers.cms.menu_manager import FakeMenuManager, FakeSimpleMenuManager
 
@@ -71,7 +71,17 @@ class TestSimpleMenuResponseSerializer:
         Then the output `data` contains the correct fields
         """
         # Given
-        fake_body = []
+        fake_body = [
+            {
+                "type": "link",
+                "value": {
+                    "title": "Test link",
+                    "page": 14,
+                    "html_url": "https://localhost/whats-new/",
+                },
+                "id": "93509312-316b-4de8-936b-e7ac57d0aee1",
+            }
+        ]
         menu = SimpleMenu(internal_label="abc", is_active=True, body=fake_body)
 
         # When
@@ -105,7 +115,17 @@ class TestSimpleMenuSerializer:
             about the currently active menu
         """
         # Given
-        fake_body = []
+        fake_body = [
+            {
+                "type": "link",
+                "value": {
+                    "title": "Test link",
+                    "page": 14,
+                    "html_url": "https://localhost/whats-new/",
+                },
+                "id": "93509312-316b-4de8-936b-e7ac57d0aee1",
+            }
+        ]
         active_menu = SimpleMenu(internal_label="abc", is_active=True, body=fake_body)
         inactive_menu = SimpleMenu(internal_label="abc", is_active=False)
         fake_menu_manager = FakeSimpleMenuManager(menus=[active_menu, inactive_menu])
