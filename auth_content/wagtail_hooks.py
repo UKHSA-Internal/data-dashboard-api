f61cc5e7157c9a382106d166914b01965b7fa92c
Kathryn Dale <kathryn.dale@burendo.com>
2026-04-13 16:15:19 +0100

    CDD-3284 Make permission sets read only

--- auth_content/wagtail_hooks.py
+++ auth_content/wagtail_hooks.py
@@ -1,16 +1,31 @@
 from django.templatetags.static import static
 from django.utils.html import format_html
 from wagtail import hooks
-from wagtail.admin.viewsets.model import ModelViewSet, ModelViewSetGroup
+from wagtail.admin.viewsets.model import ModelPermissionPolicy, ModelViewSet, ModelViewSetGroup
 
 from auth_content.models.permission_sets import PermissionSet
 from auth_content.models.users import User
+
+
+class NoEditPermissionPolicy(ModelPermissionPolicy):
+    def user_has_permission(self, user, action):
+        if action == "change":
+            return False
+        return super().user_has_permission(user, action)
+
+    def user_has_permission_for_instance(self, user, action, instance):
+        if action == "change":
+            return False
+        return super().user_has_permission_for_instance(user, action, instance)
 
 
 class PermissionSetViewSet(ModelViewSet):
     model = PermissionSet
     menu_label = "Permission Sets"
     icon = "key"
+    permission_policy = NoEditPermissionPolicy(PermissionSet)
+    inspect_view_enabled = True
+    inspect_view_fields = ["permission_set_details"]
 
 
 class UserViewSet(ModelViewSet):
