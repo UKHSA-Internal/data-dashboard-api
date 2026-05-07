ad4e6bc20e85c69da8071e60d00b78f0d22f525a
Jean Pierre Fouche <261405437+jeanpierrefouche-ukhsa@users.noreply.github.com>
2026-05-07 14:39:59 +0100

    task/CDD-3279-BE-SPIKE-investigate-tooling-for-javascript-testing-CMS-WITH-ESM fix github runner and ESM config no longer using experimental vm!

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 /** @type {import('jest').Config} */
 export default {
   testEnvironment: 'jsdom',
-  transform: {},
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
 
   // 1. Where Jest looks for tests
   roots: [
