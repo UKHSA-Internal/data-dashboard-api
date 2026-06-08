86cc77369e68821671b054beac95f554ff128e86
Aidan Skinner <aidan@skinner.me.uk>
2026-06-08 15:37:48 +0100

    settings: set PAGE_PREVIEWS_TOKEN_TTL_SECONDS to 5 minutes
    After discussion with @jrdh we decided 5 we needed this until performance
    improves

--- metrics/api/settings/local.py
+++ metrics/api/settings/local.py
@@ -37,10 +37,6 @@ MIDDLEWARE += [
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-# Local-only override to ease frontend preview testing
-PAGE_PREVIEWS_TOKEN_TTL_SECONDS = int(
-    os.environ.get("PAGE_PREVIEWS_TOKEN_TTL_SECONDS", "86400")
-)
 PAGE_PREVIEWS_ENABLED = os.environ.get("PAGE_PREVIEWS_ENABLED", "true").lower() in {
     "1",
     "true",
