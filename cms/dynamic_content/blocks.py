3ecbd2d1773982b8881ca53a84ae1fdf2fcbde37
Megan Bower <megan.bower@burendo.com>
2026-07-06 13:53:21 +0100

    CDD-3441 Reorder import

--- cms/dynamic_content/blocks.py
+++ cms/dynamic_content/blocks.py
@@ -1,6 +1,5 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from common.auth.permissions import check_page_permissions
 from wagtail import blocks
 from wagtail.blocks import (
     CharBlock,
@@ -20,6 +19,7 @@ from cms.dynamic_content.components import (
     PercentageNumberComponent,
     TrendNumberComponent,
 )
+from common.auth.permissions import check_page_permissions
 from validation.url import validate_https_scheme
 
 MINIMUM_ROWS_NUMBER_BLOCK_COUNT: int = 1
