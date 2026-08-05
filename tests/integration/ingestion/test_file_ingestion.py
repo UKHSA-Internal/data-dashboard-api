480605df1cebd93c25008bd4b4f37bef6f6a196d
Aidan Skinner <aidan@skinner.me.uk>
2026-08-05 13:27:26 +0100

    ingestion: make file_ingestion atomic

--- tests/integration/ingestion/test_file_ingestion.py
+++ tests/integration/ingestion/test_file_ingestion.py
@@ -8,6 +8,7 @@ from ingestion.file_ingestion import data_ingester
 from ingestion.utils import type_hints
 from metrics.data.models.api_models import APITimeSeries
 from metrics.data.models.core_models import CoreHeadline, CoreTimeSeries
+from validation.data_transfer_models.base import MissingFieldError
 from validation.is_public import (
     MISSING_IS_PUBLIC_FIELD_ERROR,
 )
@@ -206,7 +207,7 @@ class TestDataIngester:
         assert APITimeSeries.objects.count() == 0
 
     @pytest.mark.django_db
-    def test_change_in_confidence_intervals_updates_headline(
+    def test_failure_creates_no_data(
         self,
         example_headline_data: type_hints.INCOMING_DATA_TYPE,
         test_filename: str,
@@ -234,3 +235,30 @@ class TestDataIngester:
         headline = CoreHeadline.objects.first()
         assert headline.lower_confidence == new_lower_confidence
         assert headline.upper_confidence == new_upper_confidence
+
+    @pytest.mark.django_db
+    def test_change_in_confidence_intervals_updates_headline(
+        self,
+        example_headline_data: type_hints.INCOMING_DATA_TYPE,
+        test_filename: str,
+    ):
+        """
+        Given incoming data
+        When `data_ingester()` is called
+        And the transaction fails
+        Then no records are created
+        """
+        # Given
+        assert APIHeadline.objects.all().count() == 0
+        assert CoreHeadline.objects.all().count() == 0
+
+        # When
+        data = example_headline_data
+        data["data"][0].pop("metric_value")
+        with pytest.raises(MissingFieldError):
+            data_ingester(data=data, filename=test_filename)
+
+        # Then
+        # Check that 0 records are created
+        assert APIHeadline.objects.all().count() == 0
+        assert CoreHeadline.objects.all().count() == 0
