745dfcea634c791df98e2494728a5d52349bd061
Matt Reynolds <18287679+mattjreynolds@users.noreply.github.com>
2026-08-10 07:51:45 +0100

    CDD-3173: Add integration test for authenticated timeseries download

--- tests/integration/metrics/api/views/downloads/test_single_category_downloads.py
+++ tests/integration/metrics/api/views/downloads/test_single_category_downloads.py
@@ -3,12 +3,14 @@ import datetime
 import io
 from collections import OrderedDict
 from http import HTTPStatus
+from unittest import mock
 
 import pytest
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 
 from metrics.data.models.core_models import CoreTimeSeries, Geography, CoreHeadline
+from tests.factories.common.auth.permissions import UserPermissionsFactory
 from tests.factories.metrics.time_series import CoreTimeSeriesFactory
 from tests.factories.metrics.headline import CoreHeadlineFactory
 
@@ -47,14 +49,17 @@ class TestDownloadsView:
         "period_end": "2024-02-02 00:00:00",
     }
 
-    def _build_valid_payload(self) -> dict[str, str | list[dict[str, str]]]:
+    def _build_valid_payload(
+        self, is_public: bool = True
+    ) -> dict[str, str | list[dict[str, str]]]:
+        metric_prefix = "OFF-SENS_" if not is_public else ""
         return {
             "file_format": "csv",
             "plots": [
                 {
                     "theme": self.core_timeseries_data["theme"],
                     "sub_theme": self.core_timeseries_data["sub_theme"],
-                    "metric": self.core_timeseries_data["metric"],
+                    "metric": metric_prefix + self.core_timeseries_data["metric"],
                     "topic": self.core_timeseries_data["topic"],
                     "stratum": self.core_timeseries_data["stratum"],
                     "age": self.core_timeseries_data["age"],
@@ -87,13 +92,16 @@ class TestDownloadsView:
             ],
         }
 
-    def _create_example_core_time_series(self) -> CoreTimeSeries:
+    def _create_example_core_time_series(
+        self, is_public: bool = True
+    ) -> CoreTimeSeries:
+        metric_prefix = "OFF-SENS_" if not is_public else ""
         return CoreTimeSeriesFactory.create_record(
             metric_value=self.core_timeseries_data["metric_value"],
             theme_name=self.core_timeseries_data["theme"],
             sub_theme_name=self.core_timeseries_data["sub_theme"],
             topic_name=self.core_timeseries_data["topic"],
-            metric_name=self.core_timeseries_data["metric"],
+            metric_name=metric_prefix + self.core_timeseries_data["metric"],
             geography_name=self.core_timeseries_data["geography"],
             geography_type_name=self.core_timeseries_data["geography_type"],
             stratum_name=self.core_timeseries_data["stratum"],
@@ -105,6 +113,7 @@ class TestDownloadsView:
             in_reporting_delay_period=self.core_timeseries_data[
                 "in_reporting_delay_period"
             ],
+            is_public=is_public,
         )
 
     def _create_examples_headline_data(self) -> CoreHeadline:
@@ -348,6 +357,94 @@ class TestDownloadsView:
             ]
         ]
         assert csv_output == expected_csv_content
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        ("authenticated_user", "expected_status"),
+        [
+            (True, HTTPStatus.OK),
+            (False, HTTPStatus.BAD_REQUEST),
+        ],
+    )
+    def test_csv_download_non_pub_timeseries_returns_correct_response(
+        self, authenticated_user, expected_status
+    ):
+        """
+        Given a valid authenticated payload to request a download
+        When the `POST /api/downloads/v2/` endpoint is hit
+        Then the response contains the expected output in csv format
+        """
+        # Given
+        client = APIClient()
+        if authenticated_user:
+            mock_user = mock.MagicMock()
+            mock_user.username = "restricted-user"
+
+            mock_user.permission_sets = UserPermissionsFactory(
+                [],
+                has_global_access=True,
+            )
+            client.force_authenticate(user=mock_user, token="token")
+
+        core_time_series = self._create_example_core_time_series(is_public=False)
+        valid_payload = self._build_valid_payload(is_public=False)
+        # core_time_series = self._create_example_non_pub_time_series()
+        # valid_payload = self._build_valid_non_pub_payload()
+
+        # When
+        response: Response = client.post(
+            path=self.path,
+            data=valid_payload,
+            format="json",
+        )
+
+        # Then
+        assert response.status_code == expected_status
+
+        if authenticated_user:
+            # Check that the headers on the response indicate csv is being returned
+            assert response.headers["Content-Type"] == "text/csv"
+
+            # Check the output itself is as expected
+            csv_file = csv.reader(io.StringIO(response.content.decode("utf-8")))
+            csv_output = list(csv_file)
+            csv_headers = csv_output.pop(0)
+
+            expected_csv_headings = [
+                "theme",
+                "sub_theme",
+                "topic",
+                "geography_type",
+                "geography",
+                "metric",
+                "sex",
+                "age",
+                "stratum",
+                "year",
+                "date",
+                "metric_value",
+                "in_reporting_delay_period",
+            ]
+            assert csv_headers == expected_csv_headings
+
+            expected_csv_content = [
+                [
+                    core_time_series.metric.topic.sub_theme.theme.name,
+                    core_time_series.metric.topic.sub_theme.name,
+                    core_time_series.metric.topic.name,
+                    core_time_series.geography.geography_type.name,
+                    core_time_series.geography.name,
+                    core_time_series.metric.name,
+                    core_time_series.sex,
+                    core_time_series.age.name,
+                    core_time_series.stratum.name,
+                    str(core_time_series.year),
+                    core_time_series.date,
+                    f"{core_time_series.metric_value:.4f}",
+                    str(core_time_series.in_reporting_delay_period),
+                ]
+            ]
+            assert csv_output == expected_csv_content
 
     @pytest.mark.django_db
     def test_returns_bad_request_response_for_invalid_data(self):
