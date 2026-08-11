c551ea12fbd7ba1e63ab0fe856505875bc88ee40
Josh Humphries <josh.humphries@ukhsa.gov.uk>
2026-08-11 22:34:47 +0100

    fix: resolve migration clash

--- cms/topic/migrations/0037_rename_topicpage_non_public_fields.py
+++ cms/topic/migrations/0037_rename_topicpage_non_public_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("topic", "0035_alter_topicpage_body_dual_category_update"),
+        ("topic", "0036_alter_topicpage_add_HeadlineChartWithDescriptionCard"),
     ]
 
     operations = [
