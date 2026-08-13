76fc46d024586e4b3d55b90e905f00e6d3103b65
Daniel Welsh <daniel.welsh@ukhsa.gov.uk>
2026-08-13 16:07:53 +0100

    Fix to the validation for metric groups based on metric name string

--- cms/dynamic_content/elements.py
+++ cms/dynamic_content/elements.py
@@ -22,6 +22,7 @@ from cms.metrics_interface.field_choices_callables import (
     get_headline_chart_types,
     get_simplified_chart_types,
 )
+from metrics.domain.common.utils import extract_metric_group_from_metric
 from validation.data_transfer_models.base import IncomingBaseDataModel
 
 DEFAULT_GEOGRAPHY = "England"
@@ -173,8 +174,10 @@ class BaseMetricsElement(blocks.StructBlock):
             StructBlockValidationError on the metric if a group was not extractable
         """
         try:
-            return value["metric"].split("_")[1]
-        except IndexError as e:
+            return MetricsAPIInterface.get_metric_group_from_metric(
+                metric=value["metric"]
+            )
+        except (KeyError, TypeError, ValueError) as e:
             raise blocks.StructBlockValidationError(
                 block_errors={
                     "metric": ValidationError("Invalid metric, could not extract group")
