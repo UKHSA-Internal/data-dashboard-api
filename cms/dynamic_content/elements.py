486813bad5438d251fd4a2f43e5f392e6f0ba72d
Megan Bower <megan.bower@burendo.com>
2026-08-28 22:00:28 +0100

    CDD-3442 Populate & filter metric dynamically with javascript hook

--- cms/dynamic_content/elements.py
+++ cms/dynamic_content/elements.py
@@ -1,6 +1,10 @@
 import pydantic
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django import forms
 from wagtail import blocks
+from wagtail.admin.telepath import register
+from wagtail.blocks.struct_block import StructBlockAdapter
+
 
 from cms.dynamic_content import help_texts
 from cms.metrics_interface import MetricsAPIInterface
@@ -12,7 +16,7 @@ from cms.metrics_interface.field_choices_callables import (
     get_all_sex_names,
     get_all_stratum_names,
     get_all_timeseries_metric_names,
-    get_all_topic_names,
+    get_all_topic_names_and_ids,
     get_all_unique_metric_names,
     get_chart_line_types,
     get_chart_types,
@@ -34,7 +38,7 @@ DEFAULT_SIMPLIFIED_CHART_TYPE = "line_single_simplified"
 class BaseMetricsElement(blocks.StructBlock):
     topic = blocks.ChoiceBlock(
         required=True,
-        choices=get_all_topic_names,
+        choices=get_all_topic_names_and_ids,
         help_text=help_texts.TOPIC_FIELD,
     )
     metric = blocks.ChoiceBlock(
@@ -195,6 +199,21 @@ class BaseMetricsElement(blocks.StructBlock):
                 }
             ) from e
 
+class BaseMetricsElementAdapter(StructBlockAdapter):
+
+    js_constructor = "cms.dynamic_content.elements.BaseMetricsElement"
+
+    @property
+    def media(self):
+        structblock_media = super().media
+        return forms.Media(
+            js=structblock_media._js + [
+                "js/metric_dropdown_manager.js",
+            ],
+            css=structblock_media._css,
+        )
+
+register(BaseMetricsElementAdapter(), BaseMetricsElement)
 
 class ChartPlotElement(BaseMetricsElement):
     metric = blocks.ChoiceBlock(
