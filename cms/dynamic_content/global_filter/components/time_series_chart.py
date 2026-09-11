a78f96fbe0c9b392640d47c3060b210f585f3f37
Rhys <rhys.howard@burendo.com>
2026-09-11 12:30:10 +0100

    CDD-3582: Added date prefix on cover chart templates

--- cms/dynamic_content/global_filter/components/time_series_chart.py
+++ cms/dynamic_content/global_filter/components/time_series_chart.py
@@ -5,8 +5,14 @@ from cms.dynamic_content import help_texts
 
 from .common import FilterLinkedComponent
 
+COVER_CHART_DATE_PREFIX_DEFAULT_TEXT = "Last updated"
 
 class FilterLinkedTimeSeriesChartTemplate(FilterLinkedComponent):
+    date_prefix = blocks.CharBlock(
+        requried=True,
+        default=COVER_CHART_DATE_PREFIX_DEFAULT_TEXT,
+        help_text=help_texts.CHART_DATE_PREFIX,
+    )
     legend_title = blocks.CharBlock(
         required=True,
         help_text=help_texts.FILTER_LINKED_TIME_SERIES_CHART_LEGEND_TITLE,
