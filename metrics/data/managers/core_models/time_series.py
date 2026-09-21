e23935b6e4f2cc9c853f32ba2fb343f47449fcc4
Manu George <manu.george@ukhsa.gov.uk>
2026-09-21 16:37:52 +0100

    Update time_series.py

--- metrics/data/managers/core_models/time_series.py
+++ metrics/data/managers/core_models/time_series.py
@@ -487,7 +487,7 @@ class CoreTimeSeriesQuerySet(models.QuerySet):
         )
 
     def get_available_geographies(
-        self, *, topic: str, theme: str, sub_theme: str, request: None
+        self, *, topic: str, theme: str, sub_theme: str
     ) -> models.QuerySet:
         """Gets all available geographies for the given `topic` which have at least 1 `CoreTimeSeries` record
 
