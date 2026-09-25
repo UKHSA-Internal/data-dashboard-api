f92838fad34dd6bc92fcb8f5b2cb88b6915bdd39
Matt Reynolds <18287679+mattjreynolds@users.noreply.github.com>
2026-09-25 13:32:42 +0100

    CDD-3452: Update test for V3

--- tests/unit/public_api/v3/serializers/test_api_time_series_request_serializer.py
+++ tests/unit/public_api/v3/serializers/test_api_time_series_request_serializer.py
@@ -1,4 +1,5 @@
 from unittest import mock
+from django.db import models
 
 import pytest
 
@@ -81,7 +82,16 @@ class TestAPITimeSeriesRequestSerializerV3:
         # Then
         assert returned_kwargs_from_request == expected_request_kwargs
 
-    def test_get_queryset_does_not_use_permissions_without_authentication(self):
+    @pytest.mark.parametrize(
+        "api_model",
+        [
+            MetricsPublicAPIInterface.get_api_timeseries_model(),
+            MetricsPublicAPIInterface.get_api_headline_model(),
+        ],
+    )
+    def test_get_queryset_does_not_use_permissions_without_authentication(
+        self, api_model: models.Model
+    ):
         # This isn't a scenario that could happen but tested anyway to prevent it
         # from creeping in
         permission_sets = {
@@ -93,25 +103,28 @@ class TestAPITimeSeriesRequestSerializerV3:
             user=mock.Mock(permission_sets=permission_sets),
             parser_context={"kwargs": {}},
         )
-        api_time_series_manager_spy = mock.Mock()
-        serializer = APITimeSeriesRequestSerializerv2(
+
+        serializer = APIRequestSerializerv3(
             context={
                 "request": mocked_request,
                 "lookup_field": "theme",
-                "api_time_series_manager": api_time_series_manager_spy,
+                "api_model": api_model,
             }
         )
 
-        with mock.patch(
-            "public_api.auth.MetricsPublicAPIInterface.is_auth_enabled",
-            return_value=True,
+        with (
+            mock.patch.object(api_model, "objects") as api_manager_spy,
+            mock.patch(
+                "public_api.auth.MetricsPublicAPIInterface.is_auth_enabled",
+                return_value=True,
+            ),
         ):
             serializer.get_queryset()
 
-        api_time_series_manager_spy.get_distinct_column_values_with_filters.assert_called_once_with(
-            lookup_field="theme",
-            permission_sets=None,
-        )
+            api_manager_spy.get_distinct_column_values_with_filters.assert_called_once_with(
+                lookup_field="theme",
+                permission_sets=None,
+            )
 
     def test_get_timeseries_dto_slice_returns_list_of_dto_objects_for_topic_lookup(
         self,
